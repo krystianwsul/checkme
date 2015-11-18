c35ac1129464e3684e0c8c856f5accdfab02db1a
krystianwsul <krystianwsul@gmail.com>
2015-11-18 19:35:06 +0100

    GroupListFragment: ListView -> RecyclerView

--- app/src/main/java/com/example/krystianwsul/organizatortest/GroupListFragment.java
+++ app/src/main/java/com/example/krystianwsul/organizatortest/GroupListFragment.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -22,7 +24,7 @@ import java.util.ArrayList;
  * Created by Krystian on 10/31/2015.
  */
 public class GroupListFragment extends Fragment {
-    private ListView mGroupList;
+    private RecyclerView mGroupList;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -33,25 +35,8 @@ public class GroupListFragment extends Fragment {
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-        mGroupList = (ListView) getView().findViewById(R.id.groups_list);
-
-        mGroupList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Group group = (Group) parent.getItemAtPosition(position);
-                Intent intent = getIntent(group, view.getContext());
-                startActivity(intent);
-            }
-
-            private Intent getIntent(Group group, Context context) {
-                if (group.singleInstance()) {
-                    Instance instance = group.getSingleSinstance();
-                    return ShowInstanceActivity.getIntent(instance, context);
-                } else {
-                    return ShowGroupActivity.getIntent(group, context);
-                }
-            }
-        });
+        mGroupList = (RecyclerView) getView().findViewById(R.id.groups_list);
+        mGroupList.setLayoutManager(new LinearLayoutManager(getContext()));
     }
 
     @Override
--- app/src/main/java/com/example/krystianwsul/organizatortest/arrayadapters/GroupAdapter.java
+++ app/src/main/java/com/example/krystianwsul/organizatortest/arrayadapters/GroupAdapter.java
@@ -1,19 +1,24 @@
 package com.example.krystianwsul.organizatortest.arrayadapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Resources;
-import android.util.Log;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
+import android.widget.AdapterView;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.ImageView;
+import android.widget.TableLayout;
 import android.widget.TextView;
 
 import com.example.krystianwsul.organizatortest.R;
+import com.example.krystianwsul.organizatortest.ShowGroupActivity;
+import com.example.krystianwsul.organizatortest.ShowInstanceActivity;
 import com.example.krystianwsul.organizatortest.domainmodel.groups.Group;
+import com.example.krystianwsul.organizatortest.domainmodel.instances.Instance;
 
 import junit.framework.Assert;
 
@@ -22,13 +27,11 @@ import java.util.ArrayList;
 /**
  * Created by Krystian on 10/23/2015.
  */
-public class GroupAdapter extends ArrayAdapter<Group> {
+public class GroupAdapter extends RecyclerView.Adapter<GroupAdapter.GroupHolder> {
     private final Context mContext;
     private final ArrayList<Group> mGroups;
 
     public GroupAdapter(Context context, ArrayList<Group> groups) {
-        super(context, -1, groups);
-
         Assert.assertTrue(context != null);
         Assert.assertTrue(groups != null);
         Assert.assertTrue(!groups.isEmpty());
@@ -38,38 +41,37 @@ public class GroupAdapter extends ArrayAdapter<Group> {
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        if (convertView == null) {
-            LayoutInflater inflater = LayoutInflater.from(mContext);
-            convertView = inflater.inflate(R.layout.show_group_row, parent, false);
+    public GroupHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        TableLayout groupRow = (TableLayout) LayoutInflater.from(parent.getContext()).inflate(R.layout.show_group_row, parent, false);
 
-            GroupHolder groupHolder = new GroupHolder();
-            groupHolder.groupRowName = (TextView) convertView.findViewById(R.id.group_row_name);
-            groupHolder.groupRowDetails = (TextView) convertView.findViewById(R.id.group_row_details);
-            groupHolder.groupRowImg = (ImageView) convertView.findViewById(R.id.group_row_img);
-            groupHolder.groupRowCheckBox = (CheckBox) convertView.findViewById(R.id.group_row_checkbox);
-            convertView.setTag(groupHolder);
-        }
+        TextView groupRowName = (TextView) groupRow.findViewById(R.id.group_row_name);
+        TextView groupRowDetails = (TextView) groupRow.findViewById(R.id.group_row_details);
+        ImageView groupRowImg = (ImageView) groupRow.findViewById(R.id.group_row_img);
+        CheckBox groupCheckBox = (CheckBox) groupRow.findViewById(R.id.group_row_checkbox);
 
-        GroupHolder groupHolder = (GroupHolder) convertView.getTag();
+        GroupHolder groupHolder = new GroupHolder(groupRow, groupRowName, groupRowDetails, groupRowImg, groupCheckBox);
+        return groupHolder;
+    }
 
+    @Override
+    public void onBindViewHolder(GroupHolder groupHolder, int position) {
         final Group group = mGroups.get(position);
 
-        groupHolder.groupRowName.setText(group.getNameText(mContext));
+        groupHolder.mGroupRowName.setText(group.getNameText(mContext));
 
-        groupHolder.groupRowDetails.setText(group.getDetailsText(mContext));
+        groupHolder.mGroupRowDetails.setText(group.getDetailsText(mContext));
 
         Resources resources = mContext.getResources();
 
         if (group.singleInstance() && group.getSingleSinstance().getChildInstances().isEmpty())
-            groupHolder.groupRowImg.setBackground(resources.getDrawable(R.drawable.ic_label_outline_black_18dp));
+            groupHolder.mGroupRowImg.setBackground(resources.getDrawable(R.drawable.ic_label_outline_black_18dp));
         else
-            groupHolder.groupRowImg.setBackground(resources.getDrawable(R.drawable.ic_list_black_18dp));
+            groupHolder.mGroupRowImg.setBackground(resources.getDrawable(R.drawable.ic_list_black_18dp));
 
         if (group.singleInstance()) {
-            groupHolder.groupRowCheckBox.setVisibility(View.VISIBLE);
+            groupHolder.mGroupRowCheckBox.setVisibility(View.VISIBLE);
 
-            groupHolder.groupRowCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            groupHolder.mGroupRowCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
                 @Override
                 public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                     Assert.assertTrue(group.singleInstance());
@@ -77,18 +79,55 @@ public class GroupAdapter extends ArrayAdapter<Group> {
                 }
             });
 
-            groupHolder.groupRowCheckBox.setChecked(group.getSingleSinstance().getDone() != null);
+            groupHolder.mGroupRowCheckBox.setChecked(group.getSingleSinstance().getDone() != null);
         } else {
-            groupHolder.groupRowCheckBox.setVisibility(View.INVISIBLE);
+            groupHolder.mGroupRowCheckBox.setVisibility(View.INVISIBLE);
         }
 
-        return convertView;
+        groupHolder.mGroupRow.setOnClickListener(new TableLayout.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = getIntent(group, view.getContext());
+                mContext.startActivity(intent);
+            }
+
+            private Intent getIntent(Group group, Context context) {
+                if (group.singleInstance()) {
+                    Instance instance = group.getSingleSinstance();
+                    return ShowInstanceActivity.getIntent(instance, context);
+                } else {
+                    return ShowGroupActivity.getIntent(group, context);
+                }
+            }
+        });
     }
 
-    private class GroupHolder {
-        public TextView groupRowName;
-        public TextView groupRowDetails;
-        public ImageView groupRowImg;
-        public CheckBox groupRowCheckBox;
+    @Override
+    public int getItemCount() {
+        return mGroups.size();
+    }
+
+    public static class GroupHolder extends RecyclerView.ViewHolder {
+        public final TableLayout mGroupRow;
+        public final TextView mGroupRowName;
+        public final TextView mGroupRowDetails;
+        public final ImageView mGroupRowImg;
+        public final CheckBox mGroupRowCheckBox;
+
+        public GroupHolder(TableLayout groupRow, TextView groupRowName, TextView groupRowDetails, ImageView groupRowImg, CheckBox groupRowCheckBox) {
+            super(groupRow);
+
+            Assert.assertTrue(groupRow != null);
+            Assert.assertTrue(groupRowName != null);
+            Assert.assertTrue(groupRowDetails != null);
+            Assert.assertTrue(groupRowImg != null);
+            Assert.assertTrue(groupRowCheckBox != null);
+
+            mGroupRow = groupRow;
+            mGroupRowName = groupRowName;
+            mGroupRowDetails = groupRowDetails;
+            mGroupRowImg = groupRowImg;
+            mGroupRowCheckBox = groupRowCheckBox;
+        }
     }
 }

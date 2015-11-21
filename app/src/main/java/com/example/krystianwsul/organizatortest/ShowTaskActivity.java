ea1a5ed7df3a515ecbf231367e491f2aaf7af278
krystianwsul <krystianwsul@gmail.com>
2015-11-21 11:32:42 +0100

    dodanie onStart() do ShowTaskActivity

--- app/src/main/java/com/example/krystianwsul/organizatortest/ShowTaskActivity.java
+++ app/src/main/java/com/example/krystianwsul/organizatortest/ShowTaskActivity.java
@@ -19,6 +19,9 @@ import junit.framework.Assert;
 import java.util.ArrayList;
 
 public class ShowTaskActivity extends AppCompatActivity {
+    private ListView mShowTaskList;
+    private Task mTask;
+
     private static final String INTENT_KEY = "taskId";
 
     public static Intent getIntent(Task task, Context context) {
@@ -36,31 +39,37 @@ public class ShowTaskActivity extends AppCompatActivity {
         Assert.assertTrue(intent.hasExtra(INTENT_KEY));
         int taskId = intent.getIntExtra(INTENT_KEY, -1);
         Assert.assertTrue(taskId != -1);
-        Task task = TaskFactory.getInstance().getTask(taskId);
-        Assert.assertTrue(task != null);
+        mTask = TaskFactory.getInstance().getTask(taskId);
+        Assert.assertTrue(mTask != null);
 
         TextView tasksHeadingLabel = (TextView) findViewById(R.id.show_task_name);
-        tasksHeadingLabel.setText(task.getName());
+        tasksHeadingLabel.setText(mTask.getName());
 
         TextView tasksRowSchedule = (TextView) findViewById(R.id.show_task_details);
-        String scheduleText = task.getScheduleText(this);
+        String scheduleText = mTask.getScheduleText(this);
         if (TextUtils.isEmpty(scheduleText))
             tasksRowSchedule.setVisibility(View.GONE);
         else
             tasksRowSchedule.setText(scheduleText);
 
-        ListView showTaskList = (ListView) findViewById(R.id.show_task_list);
-        if (task.getChildTasks().isEmpty())
-            showTaskList.setVisibility(View.GONE);
-        else
-            showTaskList.setAdapter(new TaskAdapter(this, new ArrayList(task.getChildTasks())));
+        mShowTaskList = (ListView) findViewById(R.id.show_task_list);
 
-        showTaskList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        mShowTaskList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Task childTask = (Task) parent.getItemAtPosition(position);
                 startActivity(ShowTaskActivity.getIntent(childTask, view.getContext()));
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (mTask.getChildTasks().isEmpty())
+            mShowTaskList.setVisibility(View.GONE);
+        else
+            mShowTaskList.setAdapter(new TaskAdapter(this, new ArrayList(mTask.getChildTasks())));
     }
 }

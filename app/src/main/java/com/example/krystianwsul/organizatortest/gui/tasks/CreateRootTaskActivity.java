ae3f71110628ea4c542c0d02b12a432b42b7f30f
Krystian <krystianwsul@gmail.com>
2015-12-10 19:24:10 +0100

    +SchedulePickerFragment

--- app/src/main/java/com/example/krystianwsul/organizatortest/gui/tasks/CreateRootTaskActivity.java
+++ app/src/main/java/com/example/krystianwsul/organizatortest/gui/tasks/CreateRootTaskActivity.java
@@ -3,15 +3,11 @@ package com.example.krystianwsul.organizatortest.gui.tasks;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.example.krystianwsul.organizatortest.R;
@@ -21,10 +17,6 @@ import com.example.krystianwsul.organizatortest.domainmodel.times.HourMinute;
 import junit.framework.Assert;
 
 public class CreateRootTaskActivity extends AppCompatActivity implements HourMinutePickerFragment.HourMinutePickerFragmentListener, DatePickerFragment.DatePickerFragmentListener {
-    private static final String POSITION_KEY = "position";
-
-    private Spinner mCreateTaskSpinner;
-
     public static Intent getIntent(Context context) {
         return new Intent(context, CreateRootTaskActivity.class);
     }
@@ -33,19 +25,6 @@ public class CreateRootTaskActivity extends AppCompatActivity implements HourMin
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_root_task);
-
-        int count = 1;
-        if (savedInstanceState != null) {
-            int position = savedInstanceState.getInt(POSITION_KEY, -1);
-            Assert.assertTrue(position != -1);
-            if (position > 0)
-                count = 2;
-        }
-
-        final int finalCount = count;
-
-        if (savedInstanceState == null)
-            loadFragment(0);
 
         final EditText createTaskName = (EditText) findViewById(R.id.create_root_task_name);
 
@@ -61,67 +40,14 @@ public class CreateRootTaskActivity extends AppCompatActivity implements HourMin
                     return;
                 }
 
-                ScheduleFragment scheduleFragment = (ScheduleFragment) getSupportFragmentManager().findFragmentById(R.id.create_root_task_frame);
-                Assert.assertTrue(scheduleFragment != null);
+                SchedulePickerFragment schedulePickerFragment = (SchedulePickerFragment) getSupportFragmentManager().findFragmentById(R.id.create_root_task_frame);
+                Assert.assertTrue(schedulePickerFragment != null);
 
-                scheduleFragment.createRootTask(name);
+                schedulePickerFragment.createRootTask(name);
 
                 finish();
             }
         });
-
-        mCreateTaskSpinner = (Spinner) findViewById(R.id.create_root_task_spinner);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.schedule_spinner, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        mCreateTaskSpinner.setAdapter(adapter);
-
-        mCreateTaskSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            private int mCount = finalCount;
-
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                Assert.assertTrue(position >= 0);
-                Assert.assertTrue(position < 3);
-
-                if (mCount > 0) {
-                    mCount--;
-                    return;
-                }
-
-                loadFragment(position);
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
-    }
-
-    private Fragment createFragment(int position) {
-        Assert.assertTrue(position >= 0);
-        Assert.assertTrue(position < 3);
-
-        switch (position) {
-            case 0:
-                return SingleScheduleFragment.newInstance();
-            case 1:
-                return DailyScheduleFragment.newInstance();
-            case 2:
-                return WeeklyScheduleFragment.newInstance();
-            default:
-                return null;
-        }
-    }
-
-    private void loadFragment(int position) {
-        Assert.assertTrue(position >= 0);
-        Assert.assertTrue(position < 3);
-
-        Fragment fragment = createFragment(position);
-        Assert.assertTrue(fragment != null);
-
-        getSupportFragmentManager().beginTransaction().replace(R.id.create_root_task_frame, fragment).commit();
     }
 
     @Override
@@ -142,12 +68,5 @@ public class CreateRootTaskActivity extends AppCompatActivity implements HourMin
         Assert.assertTrue(hourMinutePickerFragmentListener != null);
 
         hourMinutePickerFragmentListener.onHourMinutePickerFragmentResult(hourMinute);
-    }
-
-    @Override
-    public void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-
-        outState.putInt(POSITION_KEY, mCreateTaskSpinner.getSelectedItemPosition());
     }
 }

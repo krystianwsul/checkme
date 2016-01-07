c8ed1e902842d4f3837fd3b98b66bcfde3519f11
Krystian <krystianwsul@gmail.com>
2016-01-07 20:04:30 +0100

    użycia RecyclerView w ShowCustomTimesActivity

--- app/src/main/java/com/example/krystianwsul/organizator/gui/customtimes/ShowCustomTimesActivity.java
+++ app/src/main/java/com/example/krystianwsul/organizator/gui/customtimes/ShowCustomTimesActivity.java
@@ -1,16 +1,16 @@
 package com.example.krystianwsul.organizator.gui.customtimes;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 import android.widget.TextView;
 
 import com.example.krystianwsul.organizator.R;
@@ -22,7 +22,7 @@ import junit.framework.Assert;
 import java.util.ArrayList;
 
 public class ShowCustomTimesActivity extends AppCompatActivity {
-    private ListView mShowTimesList;
+    private RecyclerView mShowTimesList;
 
     public static Intent getIntent(Context context) {
         return new Intent(context, ShowCustomTimesActivity.class);
@@ -33,14 +33,8 @@ public class ShowCustomTimesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_custom_times);
 
-        mShowTimesList = (ListView) findViewById(R.id.show_times_list);
-        mShowTimesList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                CustomTime customTime = (CustomTime) parent.getItemAtPosition(position);
-                startActivity(ShowCustomTimeActivity.getEditIntent(customTime, ShowCustomTimesActivity.this));
-            }
-        });
+        mShowTimesList = (RecyclerView) findViewById(R.id.show_times_list);
+        mShowTimesList.setLayoutManager(new LinearLayoutManager(this));
 
         FloatingActionButton showTimesFab = (FloatingActionButton) findViewById(R.id.show_times_fab);
         showTimesFab.setOnClickListener(new View.OnClickListener() {
@@ -58,41 +52,64 @@ public class ShowCustomTimesActivity extends AppCompatActivity {
         mShowTimesList.setAdapter(new CustomTimesAdapter(this));
     }
 
-    public static class CustomTimesAdapter extends ArrayAdapter<CustomTime> {
-        private final Context mContext;
+    public static class CustomTimesAdapter extends RecyclerView.Adapter<CustomTimesAdapter.CustomTimeHolder> {
+        private final Activity mActivity;
         private final ArrayList<CustomTime> mCustomTimes;
 
-        public CustomTimesAdapter(Context context) {
-            super(context, -1, new ArrayList<>(DomainFactory.getInstance().getCustomTimeFactory().getCustomTimes()));
+        public CustomTimesAdapter(Activity activity) {
+            Assert.assertTrue(activity != null);
 
-            Assert.assertTrue(context != null);
-
-            mContext = context;
+            mActivity = activity;
             mCustomTimes = new ArrayList<>(DomainFactory.getInstance().getCustomTimeFactory().getCustomTimes());
         }
 
         @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            if (convertView == null) {
-                LayoutInflater inflater = LayoutInflater.from(mContext);
-                convertView = inflater.inflate(R.layout.show_custom_times_row, parent, false);
-
-                CustomTimeHolder customTimeHolder = new CustomTimeHolder();
-                customTimeHolder.timesRowName = (TextView) convertView.findViewById(R.id.times_row_name);
-                convertView.setTag(customTimeHolder);
-            }
-
-            CustomTimeHolder customTimeHolder = (CustomTimeHolder) convertView.getTag();
-
-            CustomTime customTime = mCustomTimes.get(position);
-
-            customTimeHolder.timesRowName.setText(customTime.getName());
-
-            return convertView;
+        public int getItemCount() {
+            return mCustomTimes.size();
         }
 
-        private class CustomTimeHolder {
-            public TextView timesRowName;
+        @Override
+        public CustomTimeHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater layoutInflater = LayoutInflater.from(mActivity);
+            View showCustomTimesRow = layoutInflater.inflate(R.layout.show_custom_times_row, parent, false);
+
+            TextView timesRowName = (TextView) showCustomTimesRow.findViewById(R.id.times_row_name);
+
+            return new CustomTimeHolder(showCustomTimesRow, timesRowName);
+        }
+
+        @Override
+        public void onBindViewHolder(final CustomTimeHolder customTimeHolder, int position) {
+            CustomTime customTime = mCustomTimes.get(position);
+            Assert.assertTrue(customTime != null);
+
+            customTimeHolder.mTimesRowName.setText(customTime.getName());
+
+            customTimeHolder.mShowCustomTimeRow.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    customTimeHolder.onClick();
+                }
+            });
+        }
+
+        public class CustomTimeHolder extends RecyclerView.ViewHolder {
+            public final View mShowCustomTimeRow;
+            public final TextView mTimesRowName;
+
+            public CustomTimeHolder(View showCustomTimesRow, TextView timesRowName) {
+                super(showCustomTimesRow);
+
+                Assert.assertTrue(timesRowName != null);
+
+                mShowCustomTimeRow = showCustomTimesRow;
+                mTimesRowName = timesRowName;
+            }
+
+            public void onClick() {
+                CustomTime customTime = mCustomTimes.get(getAdapterPosition());
+                mActivity.startActivity(ShowCustomTimeActivity.getEditIntent(customTime, mActivity));
+            }
         }
     }
 }

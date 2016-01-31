b7aea2efe7018274f9ffc9614d289601596af9c4
Krystian <krystianwsul@gmail.com>
2016-01-31 15:40:24 +0100

    ShowTaskActivity +DomainLoader

--- app/src/main/java/com/example/krystianwsul/organizator/gui/tasks/ShowTaskActivity.java
+++ app/src/main/java/com/example/krystianwsul/organizator/gui/tasks/ShowTaskActivity.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -14,19 +16,18 @@ import android.widget.TextView;
 
 import com.example.krystianwsul.organizator.R;
 import com.example.krystianwsul.organizator.domainmodel.DomainFactory;
+import com.example.krystianwsul.organizator.domainmodel.DomainLoader;
 import com.example.krystianwsul.organizator.domainmodel.Task;
 import com.example.krystianwsul.organizator.utils.time.TimeStamp;
 
 import junit.framework.Assert;
 
-public class ShowTaskActivity extends AppCompatActivity {
-    private DomainFactory mDomainFactory;
-
+public class ShowTaskActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<DomainFactory> {
     private RecyclerView mShowTaskRecycler;
     private TextView mTasksHeadingLabel;
     private TextView mTasksRowSchedule;
-
-    private Task mTask;
+    private ImageView mShowTaskEdit;
+    private FloatingActionButton mFloatingActionButton;
 
     private static final String INTENT_KEY = "taskId";
 
@@ -41,15 +42,6 @@ public class ShowTaskActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_task);
 
-        mDomainFactory = DomainFactory.getDomainFactory(this);
-
-        Intent intent = getIntent();
-        Assert.assertTrue(intent.hasExtra(INTENT_KEY));
-        int taskId = intent.getIntExtra(INTENT_KEY, -1);
-        Assert.assertTrue(taskId != -1);
-        mTask = mDomainFactory.getTaskFactory().getTask(taskId);
-        Assert.assertTrue(mTask != null);
-
         mTasksHeadingLabel = (TextView) findViewById(R.id.show_task_name);
 
         mTasksRowSchedule = (TextView) findViewById(R.id.show_task_schedule);
@@ -57,37 +49,58 @@ public class ShowTaskActivity extends AppCompatActivity {
         mShowTaskRecycler = (RecyclerView) findViewById(R.id.show_task_recycler);
         mShowTaskRecycler.setLayoutManager(new LinearLayoutManager(this));
 
-        FloatingActionButton floatingActionButton = (FloatingActionButton) findViewById(R.id.show_task_fab);
-        floatingActionButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(CreateChildTaskActivity.getCreateIntent(ShowTaskActivity.this, mTask));
-            }
-        });
+        mShowTaskEdit = (ImageView) findViewById(R.id.show_task_edit);
 
-        ImageView showTaskEdit = (ImageView) findViewById(R.id.show_task_edit);
-        showTaskEdit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (mTask.isRootTask(TimeStamp.getNow()))
-                    startActivity(CreateRootTaskActivity.getEditIntent(ShowTaskActivity.this, mTask));
-                else
-                    startActivity(CreateChildTaskActivity.getEditIntent(ShowTaskActivity.this, mTask));
-            }
-        });
+        mFloatingActionButton = (FloatingActionButton) findViewById(R.id.show_task_fab);
+
+        getSupportLoaderManager().initLoader(0, null, this);
     }
 
     @Override
-    protected void onStart() {
-        super.onStart();
+    public Loader<DomainFactory> onCreateLoader(int id, Bundle args) {
+        return new DomainLoader(this);
+    }
 
-        mShowTaskRecycler.setAdapter(new TaskAdapter(this, mDomainFactory, mTask.getChildTasks(TimeStamp.getNow())));
+    @Override
+    public void onLoadFinished(Loader<DomainFactory> loader, DomainFactory domainFactory) {
+        Intent intent = getIntent();
+        Assert.assertTrue(intent.hasExtra(INTENT_KEY));
+        int taskId = intent.getIntExtra(INTENT_KEY, -1);
+        Assert.assertTrue(taskId != -1);
+        final Task task = domainFactory.getTaskFactory().getTask(taskId);
+        Assert.assertTrue(task != null);
 
-        mTasksHeadingLabel.setText(mTask.getName());
-        String scheduleText = mTask.getScheduleText(this, TimeStamp.getNow());
+        mFloatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(CreateChildTaskActivity.getCreateIntent(ShowTaskActivity.this, task));
+            }
+        });
+
+        mShowTaskEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (task.isRootTask(TimeStamp.getNow()))
+                    startActivity(CreateRootTaskActivity.getEditIntent(ShowTaskActivity.this, task));
+                else
+                    startActivity(CreateChildTaskActivity.getEditIntent(ShowTaskActivity.this, task));
+            }
+        });
+
+        mShowTaskRecycler.setAdapter(new TaskAdapter(this, domainFactory, task.getChildTasks(TimeStamp.getNow())));
+
+        mTasksHeadingLabel.setText(task.getName());
+        String scheduleText = task.getScheduleText(this, TimeStamp.getNow());
         if (TextUtils.isEmpty(scheduleText))
             mTasksRowSchedule.setVisibility(View.GONE);
         else
             mTasksRowSchedule.setText(scheduleText);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<DomainFactory> loader) {
+        mFloatingActionButton.setOnClickListener(null);
+        mShowTaskEdit.setOnClickListener(null);
+        mShowTaskRecycler.setAdapter(null);
     }
 }

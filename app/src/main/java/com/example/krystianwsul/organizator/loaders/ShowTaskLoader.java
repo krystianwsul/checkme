7cec1e931c7c15b9cfd6358f75f76e25d80a5c96
Krystian <krystianwsul@gmail.com>
2016-03-05 09:58:46 +0100

    flicker fix: ShowTaskLoader

--- app/src/main/java/com/example/krystianwsul/organizator/loaders/ShowTaskLoader.java
+++ app/src/main/java/com/example/krystianwsul/organizator/loaders/ShowTaskLoader.java
@@ -34,6 +34,10 @@ public class ShowTaskLoader extends DomainLoader<ShowTaskLoader.Data, ShowTaskLo
             if (mData != null && dataId == mData.DataId)
                 return;
 
+            Data newData = loadInBackground();
+            if (mData.equals(newData))
+                return;
+
             onContentChanged();
         }
     }
@@ -55,6 +59,34 @@ public class ShowTaskLoader extends DomainLoader<ShowTaskLoader.Data, ShowTaskLo
             TaskId = taskId;
             ChildTaskDatas = childTaskDatas;
         }
+
+        @Override
+        public int hashCode() {
+            int hashCode = 0;
+            hashCode += (IsRootTask ? 1 : 0);
+            hashCode += Name.hashCode();
+            if (!TextUtils.isEmpty(ScheduleText))
+                hashCode += ScheduleText.hashCode();
+            hashCode += TaskId;
+            hashCode += ChildTaskDatas.hashCode();
+            return hashCode;
+        }
+
+        @Override
+        public boolean equals(Object object) {
+            if (object == null)
+                return false;
+
+            if (object == this)
+                return true;
+
+            if (!(object instanceof Data))
+                return false;
+
+            Data data = (Data) object;
+
+            return ((IsRootTask == data.IsRootTask) && Name.equals(data.Name) && ((TextUtils.isEmpty(ScheduleText) && TextUtils.isEmpty(data.ScheduleText)) || ((!TextUtils.isEmpty(ScheduleText) && !TextUtils.isEmpty(data.ScheduleText)) && ScheduleText.equals(data.ScheduleText))) && (TaskId == data.TaskId) && ChildTaskDatas.equals(data.ChildTaskDatas));
+        }
     }
 
     public static class ChildTaskData {
@@ -68,6 +100,27 @@ public class ShowTaskLoader extends DomainLoader<ShowTaskLoader.Data, ShowTaskLo
             TaskId = taskId;
             Name = name;
             HasChildTasks = hasChildTasks;
+        }
+
+        @Override
+        public int hashCode() {
+            return (TaskId + Name.hashCode() + (HasChildTasks ? 1 : 0));
+        }
+
+        @Override
+        public boolean equals(Object object) {
+            if (object == null)
+                return false;
+
+            if (object == this)
+                return true;
+
+            if (!(object instanceof ChildTaskData))
+                return false;
+
+            ChildTaskData childTaskData = (ChildTaskData) object;
+
+            return ((TaskId == childTaskData.TaskId) && Name.equals(childTaskData.Name) && (HasChildTasks == childTaskData.HasChildTasks));
         }
     }
 }

68ced79256e7c7c74981fdd12b23626c9c0571f3
Krystian <krystianwsul@gmail.com>
2016-10-05 20:11:37 +0200

    logowanie do firebase

--- app/src/main/java/com/krystianwsul/checkme/gui/MainActivity.java
+++ app/src/main/java/com/krystianwsul/checkme/gui/MainActivity.java
@@ -2,10 +2,9 @@ package com.krystianwsul.checkme.gui;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
@@ -21,6 +20,7 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.view.ActionMode;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -29,13 +29,19 @@ import android.widget.ArrayAdapter;
 import android.widget.FrameLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.crashlytics.android.Crashlytics;
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 import com.krystianwsul.checkme.R;
 import com.krystianwsul.checkme.gui.customtimes.ShowCustomTimesFragment;
 import com.krystianwsul.checkme.gui.instances.DayFragment;
@@ -101,13 +107,28 @@ public class MainActivity extends AbstractActivity implements TaskListFragment.T
 
     private GoogleApiClient mGoogleApiClient;
 
-    private SharedPreferences mSharedPreferences;
-
     private NavigationView mMainActivityNavigation;
     private TextView mNavHeaderName;
     private TextView mNavHeaderEmail;
 
-    @Override
+    private FirebaseAuth mFirebaseAuth;
+    private boolean mSignedIn = false;
+
+    private FirebaseAuth.AuthStateListener mAuthStateListener = firebaseAuth -> {
+        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+        if (firebaseUser != null) {
+            mSignedIn = true;
+
+            Log.e("asdf", "firebase logged in");
+        } else {
+            mSignedIn = false;
+
+            Log.e("asdf", "firebase logged out");
+        }
+
+        updateSignInState(firebaseUser);
+    };
+
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_select_all, menu);
         return true;
@@ -304,16 +325,10 @@ public class MainActivity extends AbstractActivity implements TaskListFragment.T
                     showTab(CUSTOM_TIMES_VISIBLE);
                     break;
                 case R.id.main_drawer_sign_in:
-                    if (mSharedPreferences.getBoolean(SIGNED_IN_KEY, false)) {
+                    if (mSignedIn) {
                         Auth.GoogleSignInApi.signOut(mGoogleApiClient);
 
-                        mSharedPreferences.edit()
-                                .putBoolean(SIGNED_IN_KEY, false)
-                                .putString(DISPLAY_NAME_KEY, null)
-                                .putString(EMAIL_KEY, null)
-                                .apply();
-
-                        updateSignInState();
+                        mFirebaseAuth.signOut();
                     } else {
                         startActivityForResult(Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient), RC_SIGN_IN);
                     }
@@ -361,6 +376,7 @@ public class MainActivity extends AbstractActivity implements TaskListFragment.T
         TickService.register(this);
 
         GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail()
                 .build();
 
@@ -369,9 +385,21 @@ public class MainActivity extends AbstractActivity implements TaskListFragment.T
                 .addApi(Auth.GOOGLE_SIGN_IN_API, googleSignInOptions)
                 .build();
 
-        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        mFirebaseAuth = FirebaseAuth.getInstance();
+    }
 
-        updateSignInState();
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
     }
 
     @Override
@@ -602,38 +630,42 @@ public class MainActivity extends AbstractActivity implements TaskListFragment.T
                 GoogleSignInAccount googleSignInAccount = googleSignInResult.getSignInAccount();
                 Assert.assertTrue(googleSignInAccount != null);
 
-                String displayName = googleSignInAccount.getDisplayName();
-                Assert.assertTrue(!TextUtils.isEmpty(displayName));
-                Assert.assertTrue(!displayName.equals("null"));
+                AuthCredential credential = GoogleAuthProvider.getCredential(googleSignInAccount.getIdToken(), null);
 
-                String email = googleSignInAccount.getEmail();
-                Assert.assertTrue(!TextUtils.isEmpty(email));
-                Assert.assertTrue(!email.equals("null"));
+                mFirebaseAuth.signInWithCredential(credential)
+                        .addOnCompleteListener(this, task -> {
+                            Log.e("asdf", "signInWithCredential:onComplete:" + task.isSuccessful());
 
-                mSharedPreferences.edit()
-                        .putBoolean(SIGNED_IN_KEY, true)
-                        .putString(DISPLAY_NAME_KEY, displayName)
-                        .putString(EMAIL_KEY, email)
-                        .apply();
+                            if (!task.isSuccessful()) {
+                                Log.e("asdf", "firebase signin error: " + task.getException());
 
-                updateSignInState();
+                                Toast.makeText(this, R.string.signInFailed, Toast.LENGTH_SHORT).show();
+
+                                Crashlytics.logException(task.getException());
+
+                                Auth.GoogleSignInApi.signOut(mGoogleApiClient);
+                            } else {
+                                Toast.makeText(this, getString(R.string.signInAs) + " " + task.getResult().getUser().getDisplayName(), Toast.LENGTH_SHORT).show();
+                            }
+                        });
+            } else {
+                String message = "google signin error: " + googleSignInResult;
+
+                Log.e("asdf", message);
+
+                Toast.makeText(this, R.string.signInFailed, Toast.LENGTH_SHORT).show();
+
+                Crashlytics.logException(new Exception(message));
             }
         }
     }
 
-    private void updateSignInState() {
-        Assert.assertTrue(mSharedPreferences != null);
-
-        boolean signedIn = mSharedPreferences.getBoolean(SIGNED_IN_KEY, false);
-
-        if (signedIn) {
-            Assert.assertTrue(mSharedPreferences.contains(DISPLAY_NAME_KEY));
-            Assert.assertTrue(mSharedPreferences.contains(EMAIL_KEY));
-
-            String displayName = mSharedPreferences.getString(DISPLAY_NAME_KEY, null);
+    private void updateSignInState(@Nullable FirebaseUser firebaseUser) {
+        if (firebaseUser != null) {
+            String displayName = firebaseUser.getDisplayName();
             Assert.assertTrue(!TextUtils.isEmpty(displayName));
 
-            String email = mSharedPreferences.getString(EMAIL_KEY, null);
+            String email = firebaseUser.getEmail();
             Assert.assertTrue(!TextUtils.isEmpty(email));
 
             mNavHeaderName.setText(displayName);
@@ -641,9 +673,6 @@ public class MainActivity extends AbstractActivity implements TaskListFragment.T
 
             mMainActivityNavigation.getMenu().findItem(R.id.main_drawer_sign_in).setTitle(R.string.signOut);
         } else {
-            Assert.assertTrue(!mSharedPreferences.contains(DISPLAY_NAME_KEY));
-            Assert.assertTrue(!mSharedPreferences.contains(EMAIL_KEY));
-
             mNavHeaderName.setText(null);
             mNavHeaderEmail.setText(null);
 

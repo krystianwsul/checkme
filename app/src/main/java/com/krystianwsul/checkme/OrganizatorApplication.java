0f9ab545f4373462d24a71753c0105675faeed60
Krystian <krystianwsul@gmail.com>
2016-11-08 18:25:31 +0100

    Firebase LogLevel: debug

--- app/src/main/java/com/krystianwsul/checkme/OrganizatorApplication.java
+++ app/src/main/java/com/krystianwsul/checkme/OrganizatorApplication.java
@@ -2,11 +2,16 @@ package com.krystianwsul.checkme;
 
 import android.app.Application;
 
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Logger;
+
 public class OrganizatorApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
 
         MyCrashlytics.initialize(this);
+
+        FirebaseDatabase.getInstance().setLogLevel(Logger.Level.DEBUG);
     }
 }

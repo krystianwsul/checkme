3ebbff6ea6fb6db2608367991da932dc7ba835b0
Krystian <krystianwsul@gmail.com>
2016-11-30 19:52:22 +0100

    +MyFirebaseMessagingService

---
+++ app/src/main/java/com/krystianwsul/checkme/MyFirebaseMessagingService.java
@@ -0,0 +1,7 @@
+package com.krystianwsul.checkme;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+}

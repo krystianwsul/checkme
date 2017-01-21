746dbdeb32358b16552e9b19ae8d419a353acb32
Krystian <krystianwsul@gmail.com>
2017-01-21 09:43:32 +0100

    +LeakCanary

--- app/src/main/java/com/krystianwsul/checkme/OrganizatorApplication.java
+++ app/src/main/java/com/krystianwsul/checkme/OrganizatorApplication.java
@@ -5,11 +5,16 @@ import android.app.Application;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Logger;
 import com.krystianwsul.checkme.firebase.DatabaseWrapper;
+import com.squareup.leakcanary.LeakCanary;
 
 public class OrganizatorApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        if (LeakCanary.isInAnalyzerProcess(this))
+            return;
+        LeakCanary.install(this);
 
         MyCrashlytics.initialize(this);
 

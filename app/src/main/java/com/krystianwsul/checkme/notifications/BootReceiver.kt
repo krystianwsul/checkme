9e3c448b7ea80b0e063af252223383d94f3fc413
Krystian Wsul <krystianwsul@gmail.com>
2017-09-14 18:06:38 +0200

    BootReceiver: kotlin

---
+++ app/src/main/java/com/krystianwsul/checkme/notifications/BootReceiver.kt
@@ -0,0 +1,14 @@
+package com.krystianwsul.checkme.notifications
+
+import android.annotation.SuppressLint
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class BootReceiver : BroadcastReceiver() {
+
+    @SuppressLint("UnsafeProtectedBroadcastReceiver")
+    override fun onReceive(context: Context, intent: Intent) {
+        TickService.startServiceRegister(context, "BootReceiver: TickService.startServiceRegister")
+    }
+}

4c500af33fbedef3452c3dc54bcd24d4de1c4d15
Krystian Wsul <krystianwsul@gmail.com>
2018-01-30 18:42:42 +0100

    InstanceNotificationDeleteService: kotlin

---
+++ app/src/main/java/com/krystianwsul/checkme/notifications/InstanceNotificationDeleteService.kt
@@ -0,0 +1,34 @@
+package com.krystianwsul.checkme.notifications
+
+import android.app.IntentService
+import android.content.Context
+import android.content.Intent
+import android.os.Parcelable
+
+import com.krystianwsul.checkme.domainmodel.DomainFactory
+import com.krystianwsul.checkme.persistencemodel.SaveService
+import com.krystianwsul.checkme.utils.InstanceKey
+import com.krystianwsul.checkme.utils.TaskKey
+
+import junit.framework.Assert
+
+class InstanceNotificationDeleteService : IntentService("InstanceNotificationDeleteService") {
+
+    companion object {
+
+        private const val INSTANCE_KEY = "instanceKey"
+
+        fun getIntent(context: Context, instanceKey: InstanceKey) = Intent(context, InstanceNotificationDeleteService::class.java).apply {
+            if (instanceKey.type == TaskKey.Type.REMOTE && instanceKey.mScheduleKey.ScheduleTimePair.mCustomTimeKey != null)
+                Assert.assertTrue(instanceKey.mScheduleKey.ScheduleTimePair.mCustomTimeKey.type == TaskKey.Type.REMOTE)
+
+            putExtra(INSTANCE_KEY, instanceKey as Parcelable)
+        }
+    }
+
+    override fun onHandleIntent(intent: Intent?) {
+        val instanceKey = intent!!.getParcelableExtra<InstanceKey>(INSTANCE_KEY)!!
+
+        DomainFactory.getDomainFactory().setInstanceNotified(this, 0, SaveService.Source.SERVICE, instanceKey)
+    }
+}

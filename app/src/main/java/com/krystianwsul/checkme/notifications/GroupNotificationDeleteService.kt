6e5d1ffa9c624bcaee905ca0d990776c6192af3e
Krystian Wsul <krystianwsul@gmail.com>
2018-01-30 18:29:55 +0100

    GroupNotificationDeleteService: kotlin

---
+++ app/src/main/java/com/krystianwsul/checkme/notifications/GroupNotificationDeleteService.kt
@@ -0,0 +1,36 @@
+package com.krystianwsul.checkme.notifications
+
+import android.app.IntentService
+import android.content.Context
+import android.content.Intent
+import com.krystianwsul.checkme.domainmodel.DomainFactory
+import com.krystianwsul.checkme.persistencemodel.SaveService
+import com.krystianwsul.checkme.utils.InstanceKey
+import com.krystianwsul.checkme.utils.TaskKey
+import junit.framework.Assert
+import java.util.*
+
+class GroupNotificationDeleteService : IntentService("GroupNotificationDeleteService") {
+
+    companion object {
+
+        private const val INSTANCES_KEY = "instanceKeys"
+
+        fun getIntent(context: Context, instanceKeys: ArrayList<InstanceKey>) = Intent(context, GroupNotificationDeleteService::class.java).apply {
+            Assert.assertTrue(!instanceKeys.isEmpty())
+
+            Assert.assertTrue(instanceKeys.filter { it.type == TaskKey.Type.REMOTE }
+                    .mapNotNull { it.mScheduleKey.ScheduleTimePair.mCustomTimeKey }
+                    .all { it.type == TaskKey.Type.REMOTE })
+
+            putParcelableArrayListExtra(INSTANCES_KEY, instanceKeys)
+        }
+    }
+
+    override fun onHandleIntent(intent: Intent) {
+        val instanceKeys = intent.getParcelableArrayListExtra<InstanceKey>(INSTANCES_KEY)!!
+        Assert.assertTrue(!instanceKeys.isEmpty())
+
+        DomainFactory.getDomainFactory().setInstancesNotified(this, SaveService.Source.SERVICE, instanceKeys)
+    }
+}

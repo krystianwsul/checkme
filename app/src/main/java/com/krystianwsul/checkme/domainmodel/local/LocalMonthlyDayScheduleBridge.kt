ade878d626cee4cd1194e6d7a7311bdb5035adee
Krystian Wsul <krystianwsul@gmail.com>
2018-05-15 14:34:09 +0200

    LocalMonthlyDayScheduleBridge: kotlin

---
+++ app/src/main/java/com/krystianwsul/checkme/domainmodel/local/LocalMonthlyDayScheduleBridge.kt
@@ -0,0 +1,24 @@
+package com.krystianwsul.checkme.domainmodel.local
+
+import com.krystianwsul.checkme.domainmodel.MonthlyDayScheduleBridge
+import com.krystianwsul.checkme.persistencemodel.MonthlyDayScheduleRecord
+import com.krystianwsul.checkme.persistencemodel.ScheduleRecord
+import com.krystianwsul.checkme.utils.CustomTimeKey
+
+class LocalMonthlyDayScheduleBridge(scheduleRecord: ScheduleRecord, private val monthlyDayScheduleRecord: MonthlyDayScheduleRecord) : LocalScheduleBridge(scheduleRecord), MonthlyDayScheduleBridge {
+
+    override val dayOfMonth get() = monthlyDayScheduleRecord.dayOfMonth
+
+    override val beginningOfMonth get() = monthlyDayScheduleRecord.beginningOfMonth
+
+    override val customTimeKey get() = monthlyDayScheduleRecord.customTimeId?.let { CustomTimeKey(it) }
+
+    override val hour get() = monthlyDayScheduleRecord.hour
+
+    override val minute get() = monthlyDayScheduleRecord.minute
+
+    override fun delete() {
+        scheduleRecord.delete()
+        monthlyDayScheduleRecord.delete()
+    }
+}

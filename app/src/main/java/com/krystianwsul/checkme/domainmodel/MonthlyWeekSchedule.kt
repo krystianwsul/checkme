da426c9ab730e4c43acce6cca48ab53bf02d0cc9
Krystian Wsul <krystianwsul@gmail.com>
2018-05-15 13:46:39 +0200

    MonthlyWeekSchedule: kotlin

---
+++ app/src/main/java/com/krystianwsul/checkme/domainmodel/MonthlyWeekSchedule.kt
@@ -0,0 +1,110 @@
+package com.krystianwsul.checkme.domainmodel
+
+import android.content.Context
+import com.krystianwsul.checkme.R
+import com.krystianwsul.checkme.utils.ScheduleType
+import com.krystianwsul.checkme.utils.Utils
+import com.krystianwsul.checkme.utils.time.*
+import com.krystianwsul.checkme.utils.time.Date
+import junit.framework.Assert
+import java.util.*
+
+class MonthlyWeekSchedule(domainFactory: DomainFactory, private val monthlyWeekScheduleBridge: MonthlyWeekScheduleBridge) : RepeatingSchedule(domainFactory) {
+
+    override val scheduleBridge get() = monthlyWeekScheduleBridge
+
+    private val time: Time
+        get() {
+            val customTimeKey = monthlyWeekScheduleBridge.customTimeKey
+            return if (customTimeKey != null) {
+                domainFactory.getCustomTime(customTimeKey)
+            } else {
+                val hour = monthlyWeekScheduleBridge.hour!!
+                val minute = monthlyWeekScheduleBridge.minute!!
+                NormalTime(hour, minute)
+            }
+        }
+
+    val timePair: TimePair
+        get() {
+            val customTimeKey = monthlyWeekScheduleBridge.customTimeKey
+            val hour = monthlyWeekScheduleBridge.hour
+            val minute = monthlyWeekScheduleBridge.minute
+
+            return if (customTimeKey != null) {
+                Assert.assertTrue(hour == null)
+                Assert.assertTrue(minute == null)
+
+                TimePair(customTimeKey)
+            } else {
+                TimePair(HourMinute(hour!!, minute!!))
+            }
+        }
+
+    val dayOfMonth get() = monthlyWeekScheduleBridge.dayOfMonth
+
+    val dayOfWeek get() = DayOfWeek.values()[monthlyWeekScheduleBridge.dayOfWeek]
+
+    val beginningOfMonth get() = monthlyWeekScheduleBridge.beginningOfMonth
+
+    override val customTimeKey get() = monthlyWeekScheduleBridge.customTimeKey
+
+    override val scheduleType get() = ScheduleType.MONTHLY_WEEK
+
+    override fun getScheduleText(context: Context): String {
+        val day = monthlyWeekScheduleBridge.dayOfMonth.toString() + " " + dayOfWeek + " " + context.getString(R.string.monthDayStart) + " " + context.resources.getStringArray(R.array.month)[if (monthlyWeekScheduleBridge.beginningOfMonth) 0 else 1] + " " + context.getString(R.string.monthDayEnd)
+
+        return "$day: $time"
+    }
+
+    override fun getInstanceInDate(task: Task, date: Date, startHourMilli: HourMilli?, endHourMilli: HourMilli?): Instance? {
+        val dateThisMonth = getDate(date.year, date.month)
+
+        if (dateThisMonth != date)
+            return null
+
+        val hourMinute = time.getHourMinute(date.dayOfWeek)
+
+        if (startHourMilli != null && startHourMilli > hourMinute.toHourMilli())
+            return null
+
+        if (endHourMilli != null && endHourMilli <= hourMinute.toHourMilli())
+            return null
+
+        val scheduleDateTime = DateTime(date, time)
+        Assert.assertTrue(task.current(scheduleDateTime.timeStamp.toExactTimeStamp()))
+
+        return domainFactory.getInstance(task.taskKey, scheduleDateTime)
+    }
+
+    override fun getNextAlarm(now: ExactTimeStamp): TimeStamp? {
+        val today = now.date
+
+        val dateThisMonth = getDate(today.year, today.month)
+        val time = time
+        val thisMonth = DateTime(dateThisMonth, time).timeStamp
+
+        val endExactTimeStamp = getEndExactTimeStamp()
+
+        if (thisMonth.toExactTimeStamp() > now) {
+            return if (endExactTimeStamp != null && endExactTimeStamp <= thisMonth.toExactTimeStamp())
+                null
+            else
+                thisMonth
+        } else {
+            val calendar = now.calendar
+            calendar.add(Calendar.MONTH, 1)
+
+            val dateNextMonth = Date(calendar)
+
+            val nextMonth = DateTime(dateNextMonth, time).timeStamp
+
+            return if (endExactTimeStamp != null && endExactTimeStamp <= nextMonth.toExactTimeStamp())
+                null
+            else
+                nextMonth
+        }
+    }
+
+    private fun getDate(year: Int, month: Int) = Utils.getDateInMonth(year, month, monthlyWeekScheduleBridge.dayOfMonth, dayOfWeek, monthlyWeekScheduleBridge.beginningOfMonth)
+}

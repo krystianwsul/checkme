bed760f324c4dd07c0be88d47d2c1e7db40a5dde
Krystian Wsul <krystianwsul@gmail.com>
2018-05-17 12:28:49 +0200

    FabUser: kotlin

---
+++ app/src/main/java/com/krystianwsul/checkme/gui/FabUser.kt
@@ -0,0 +1,10 @@
+package com.krystianwsul.checkme.gui
+
+import android.support.design.widget.FloatingActionButton
+
+interface FabUser {
+
+    fun setFab(floatingActionButton: FloatingActionButton)
+
+    fun clearFab()
+}

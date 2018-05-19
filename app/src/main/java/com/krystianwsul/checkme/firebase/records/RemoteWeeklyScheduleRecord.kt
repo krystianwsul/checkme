6e3d737cc807369979d8cbb24a838e7db98ff8b3
Krystian Wsul <krystianwsul@gmail.com>
2018-05-19 14:03:14 +0200

    RemoteWeeklyScheduleRecord: kotlin

---
+++ app/src/main/java/com/krystianwsul/checkme/firebase/records/RemoteWeeklyScheduleRecord.kt
@@ -0,0 +1,32 @@
+package com.krystianwsul.checkme.firebase.records
+
+import com.krystianwsul.checkme.firebase.json.ScheduleWrapper
+import junit.framework.Assert
+
+class RemoteWeeklyScheduleRecord : RemoteScheduleRecord {
+
+    private val weeklyScheduleJson by lazy { createObject.weeklyScheduleJson!! }
+
+    override val startTime by lazy { weeklyScheduleJson.startTime }
+
+    override val endTime get() = weeklyScheduleJson.endTime
+
+    val dayOfWeek by lazy { weeklyScheduleJson.dayOfWeek }
+
+    val customTimeId by lazy { weeklyScheduleJson.customTimeId }
+
+    val hour by lazy { weeklyScheduleJson.hour }
+
+    val minute by lazy { weeklyScheduleJson.minute }
+
+    constructor(id: String, remoteTaskRecord: RemoteTaskRecord, scheduleWrapper: ScheduleWrapper) : super(id, remoteTaskRecord, scheduleWrapper) {}
+
+    constructor(remoteTaskRecord: RemoteTaskRecord, scheduleWrapper: ScheduleWrapper) : super(remoteTaskRecord, scheduleWrapper) {}
+
+    fun setEndTime(endTime: Long) {
+        Assert.assertTrue(weeklyScheduleJson.endTime == null)
+
+        weeklyScheduleJson.setEndTime(endTime)
+        addValue("$key/weeklyScheduleJson/endTime", endTime)
+    }
+}

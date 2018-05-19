f2fbd7e6b3ddf457ab73f911cbbfd341a7368b88
Krystian Wsul <krystianwsul@gmail.com>
2018-05-19 18:27:19 +0200

    MonthlyDayScheduleJson: kotlin

--- app/src/main/java/com/krystianwsul/checkme/firebase/json/MonthlyDayScheduleJson.kt
+++ app/src/main/java/com/krystianwsul/checkme/firebase/json/MonthlyDayScheduleJson.kt
@@ -1,8 +1,10 @@
 package com.krystianwsul.checkme.firebase.json
 
-class DailyScheduleJson(
+class MonthlyDayScheduleJson(
         startTime: Long = 0,
         endTime: Long? = null,
-        val customTimeId: String? = null,
+        val dayOfMonth: Int = 0,
+        val beginningOfMonth: Boolean = false,
+        val customTimeId: String?,
         val hour: Int? = null,
         val minute: Int? = null) : ScheduleJson(startTime, endTime)

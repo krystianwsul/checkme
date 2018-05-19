1254ae2cb4e3b7a9981ab7db6c0be19118aa5e52
Krystian Wsul <krystianwsul@gmail.com>
2018-05-19 18:35:34 +0200

    WeeklyScheduleJson: kotlin

---
+++ app/src/main/java/com/krystianwsul/checkme/firebase/json/WeeklyScheduleJson.kt
@@ -0,0 +1,9 @@
+package com.krystianwsul.checkme.firebase.json
+
+class WeeklyScheduleJson(
+        startTime: Long = 0,
+        endTime: Long? = null,
+        val dayOfWeek: Int = 0,
+        val customTimeId: String? = null,
+        val hour: Int? = null,
+        val minute: Int? = null) : ScheduleJson(startTime, endTime)

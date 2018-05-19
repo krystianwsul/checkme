9dc0d49aaf2a12bb7bfbcf24e337e8ca84db6698
Krystian Wsul <krystianwsul@gmail.com>
2018-05-19 18:10:51 +0200

    DailyScheduleJson: kotlin

---
+++ app/src/main/java/com/krystianwsul/checkme/firebase/json/DailyScheduleJson.kt
@@ -0,0 +1,6 @@
+package com.krystianwsul.checkme.firebase.json
+
+class DailyScheduleJson(
+        val customTimeId: String? = null,
+        val hour: Int? = null,
+        val minute: Int? = null) : ScheduleJson()

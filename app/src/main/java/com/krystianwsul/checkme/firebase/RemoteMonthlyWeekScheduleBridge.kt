36480ad989eb4e35b7b7138d66b663a48d019756
Krystian Wsul <krystianwsul@gmail.com>
2018-05-19 11:39:56 +0200

    RemoteMonthlyWeekScheduleBridge: kotlin

---
+++ app/src/main/java/com/krystianwsul/checkme/firebase/RemoteMonthlyWeekScheduleBridge.kt
@@ -0,0 +1,37 @@
+package com.krystianwsul.checkme.firebase
+
+import com.krystianwsul.checkme.domainmodel.DomainFactory
+import com.krystianwsul.checkme.domainmodel.MonthlyWeekScheduleBridge
+import com.krystianwsul.checkme.firebase.records.RemoteMonthlyWeekScheduleRecord
+import com.krystianwsul.checkme.utils.TaskKey
+
+class RemoteMonthlyWeekScheduleBridge(private val domainFactory: DomainFactory, private val remoteMonthlyWeekScheduleRecord: RemoteMonthlyWeekScheduleRecord) : MonthlyWeekScheduleBridge {
+
+    override val startTime by lazy { remoteMonthlyWeekScheduleRecord.startTime }
+
+    override val rootTaskKey by lazy { TaskKey(remoteMonthlyWeekScheduleRecord.projectId, remoteMonthlyWeekScheduleRecord.taskId) }
+
+    override val dayOfMonth get() = remoteMonthlyWeekScheduleRecord.dayOfMonth
+
+    override val dayOfWeek get() = remoteMonthlyWeekScheduleRecord.dayOfWeek
+
+    override val beginningOfMonth get() = remoteMonthlyWeekScheduleRecord.beginningOfMonth
+
+    override val customTimeKey = remoteMonthlyWeekScheduleRecord.customTimeId?.let {
+        domainFactory.getCustomTimeKey(remoteMonthlyWeekScheduleRecord.projectId, it)
+    }
+
+    override val hour get() = remoteMonthlyWeekScheduleRecord.hour
+
+    override val minute get() = remoteMonthlyWeekScheduleRecord.minute
+
+    override val remoteCustomTimeKey = remoteMonthlyWeekScheduleRecord.customTimeId?.let {
+        Pair(remoteMonthlyWeekScheduleRecord.projectId, it)
+    }
+
+    override fun getEndTime() = remoteMonthlyWeekScheduleRecord.endTime
+
+    override fun setEndTime(endTime: Long) = remoteMonthlyWeekScheduleRecord.setEndTime(endTime)
+
+    override fun delete() = remoteMonthlyWeekScheduleRecord.delete()
+}

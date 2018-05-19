1a57fa2ab3d4377e5e2145934fc3801b3daec236
Krystian Wsul <krystianwsul@gmail.com>
2018-05-19 12:53:10 +0200

    RemoteProjectUserRecord: kotlin

---
+++ app/src/main/java/com/krystianwsul/checkme/firebase/records/RemoteProjectUserRecord.kt
@@ -0,0 +1,45 @@
+package com.krystianwsul.checkme.firebase.records
+
+import android.text.TextUtils
+
+import com.krystianwsul.checkme.firebase.UserData
+import com.krystianwsul.checkme.firebase.json.UserJson
+import com.krystianwsul.checkme.utils.Utils
+
+import junit.framework.Assert
+
+class RemoteProjectUserRecord(create: Boolean, private val remoteProjectRecord: RemoteProjectRecord, override val createObject: UserJson) : RemoteRecord(create) {
+
+    companion object {
+
+        private const val USERS = "users"
+    }
+
+    val id by lazy { UserData.getKey(createObject.email) }
+
+    override val key by lazy { remoteProjectRecord.key + "/" + RemoteProjectRecord.PROJECT_JSON + "/" + USERS + "/" + id }
+
+    var name: String
+        get() = createObject.name
+        set(name) {
+            if (name == createObject.name)
+                return
+
+            createObject.name = name
+            addValue("$key/name", name)
+        }
+
+    val email by lazy { createObject.email }
+
+    fun setToken(token: String?, uuid: String) {
+        Assert.assertTrue(!TextUtils.isEmpty(uuid))
+
+        val tokens = createObject.tokens
+
+        if (Utils.stringEquals(tokens[uuid], token))
+            return
+
+        createObject.addToken(token, uuid)
+        addValue("$key/tokens/$uuid", token)
+    }
+}

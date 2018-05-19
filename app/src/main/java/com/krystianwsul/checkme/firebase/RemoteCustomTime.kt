375a553d20955d7bfdebadbc55204a100e20cc9c
Krystian Wsul <krystianwsul@gmail.com>
2018-05-19 11:22:56 +0200

    RemoteCustomTime: kotlin

---
+++ app/src/main/java/com/krystianwsul/checkme/firebase/RemoteCustomTime.kt
@@ -0,0 +1,47 @@
+package com.krystianwsul.checkme.firebase
+
+import com.krystianwsul.checkme.domainmodel.CustomTime
+import com.krystianwsul.checkme.domainmodel.DomainFactory
+import com.krystianwsul.checkme.firebase.records.RemoteCustomTimeRecord
+import com.krystianwsul.checkme.utils.CustomTimeKey
+import com.krystianwsul.checkme.utils.time.DayOfWeek
+import com.krystianwsul.checkme.utils.time.HourMinute
+import com.krystianwsul.checkme.utils.time.TimePair
+import java.util.*
+
+class RemoteCustomTime(private val domainFactory: DomainFactory, private val remoteProject: RemoteProject, val remoteCustomTimeRecord: RemoteCustomTimeRecord) : CustomTime {
+
+    val id by lazy { remoteCustomTimeRecord.id }
+
+    override val name get() = remoteCustomTimeRecord.name
+
+    override val hourMinutes
+        get() = DayOfWeek.values()
+                .map { it to getHourMinute(it) }
+                .toMap(TreeMap())
+
+    override val timePair // possibly should get local key from DomainFactory (instead I have to do it in RemoteInstance)
+        get() = TimePair(CustomTimeKey(remoteProject.id, remoteCustomTimeRecord.id), null)
+
+    override val customTimeKey by lazy { domainFactory.getCustomTimeKey(remoteProject.id, id) }
+
+    val projectId by lazy { remoteProject.id }
+
+    override fun getHourMinute(dayOfWeek: DayOfWeek) = when (dayOfWeek) {
+        DayOfWeek.SUNDAY -> HourMinute(remoteCustomTimeRecord.sundayHour, remoteCustomTimeRecord.sundayMinute)
+        DayOfWeek.MONDAY -> HourMinute(remoteCustomTimeRecord.mondayHour, remoteCustomTimeRecord.mondayMinute)
+        DayOfWeek.TUESDAY -> HourMinute(remoteCustomTimeRecord.tuesdayHour, remoteCustomTimeRecord.tuesdayMinute)
+        DayOfWeek.WEDNESDAY -> HourMinute(remoteCustomTimeRecord.wednesdayHour, remoteCustomTimeRecord.wednesdayMinute)
+        DayOfWeek.THURSDAY -> HourMinute(remoteCustomTimeRecord.thursdayHour, remoteCustomTimeRecord.thursdayMinute)
+        DayOfWeek.FRIDAY -> HourMinute(remoteCustomTimeRecord.fridayHour, remoteCustomTimeRecord.fridayMinute)
+        DayOfWeek.SATURDAY -> HourMinute(remoteCustomTimeRecord.saturdayHour, remoteCustomTimeRecord.saturdayMinute)
+    }
+
+    override fun toString() = name
+
+    fun delete() {
+        remoteProject.deleteCustomTime(this)
+
+        remoteCustomTimeRecord.delete()
+    }
+}

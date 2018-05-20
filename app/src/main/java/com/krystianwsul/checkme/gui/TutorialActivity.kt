d9ca365901382407a64aff46d961c8e57f91d6ef
Krystian Wsul <krystianwsul@gmail.com>
2018-05-20 10:59:15 +0200

    +TutorialActivity

---
+++ app/src/main/java/com/krystianwsul/checkme/gui/TutorialActivity.kt
@@ -0,0 +1,13 @@
+package com.krystianwsul.checkme.gui
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.krystianwsul.checkme.R
+
+class TutorialActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tutorial)
+    }
+}

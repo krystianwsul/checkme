3664c97aa59c351c7bde599a731f4bb105cc1e96
Krystian Wsul <krystianwsul@gmail.com>
2018-05-20 12:28:17 +0200

    AbstractDialogFragment: DialogFragment -> AppCompatDialogFragment

--- app/src/main/java/com/krystianwsul/checkme/gui/AbstractDialogFragment.kt
+++ app/src/main/java/com/krystianwsul/checkme/gui/AbstractDialogFragment.kt
@@ -1,10 +1,9 @@
 package com.krystianwsul.checkme.gui
 
-import android.support.v4.app.DialogFragment
-
+import android.support.v7.app.AppCompatDialogFragment
 import com.krystianwsul.checkme.MyCrashlytics
 
-abstract class AbstractDialogFragment : DialogFragment() {
+abstract class AbstractDialogFragment : AppCompatDialogFragment() {
 
     override fun onResume() {
         MyCrashlytics.log(javaClass.simpleName + ".onResume")

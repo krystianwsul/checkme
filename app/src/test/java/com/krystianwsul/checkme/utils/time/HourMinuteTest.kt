c51c7fe4f53ac104c07360a4d31438b0333f03fe
Krystian Wsul <krystianwsul@gmail.com>
2018-05-21 20:47:58 +0200

    HourMinuteTest: kotlin

---
+++ app/src/test/java/com/krystianwsul/checkme/utils/time/HourMinuteTest.kt
@@ -0,0 +1,32 @@
+package com.krystianwsul.checkme.utils.time
+
+import junit.framework.Assert
+
+import org.junit.Test
+
+class HourMinuteTest {
+
+    @Test
+    fun testGetNextHourNormal() {
+        val date = Date(2016, 1, 1)
+
+        val now = ExactTimeStamp(Date(2015, 1, 1), HourMilli(1, 5, 0, 0))
+
+        val (first, second) = HourMinute.getNextHour(date, now)
+
+        Assert.assertTrue(first == date)
+        Assert.assertTrue(second == HourMinute(2, 0))
+    }
+
+    @Test
+    fun testGetNextHourAfter23() {
+        val date = Date(2016, 1, 1)
+
+        val now = ExactTimeStamp(Date(2015, 1, 1), HourMilli(23, 5, 0, 0))
+
+        val (first, second) = HourMinute.getNextHour(date, now)
+
+        Assert.assertTrue(first == Date(2016, 1, 2))
+        Assert.assertTrue(second == HourMinute(0, 0))
+    }
+}

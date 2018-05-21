f7bdc314942257d4e631b9d38a210703e0f6444b
Krystian Wsul <krystianwsul@gmail.com>
2018-05-21 20:47:11 +0200

    ParentKeyTest: kotlin

---
+++ app/src/test/java/com/krystianwsul/checkme/loaders/ParentKeyTest.kt
@@ -0,0 +1,37 @@
+package com.krystianwsul.checkme.loaders
+
+import android.text.TextUtils
+import junit.framework.Assert
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Matchers.any
+import org.powermock.api.mockito.PowerMockito
+import org.powermock.core.classloader.annotations.PrepareForTest
+import org.powermock.modules.junit4.PowerMockRunner
+
+@RunWith(PowerMockRunner::class)
+@PrepareForTest(value = [(TextUtils::class)])
+class ParentKeyTest {
+
+    @Before
+    fun setUp() {
+        PowerMockito.mockStatic(TextUtils::class.java)
+
+        PowerMockito.`when`(TextUtils.isEmpty(any(CharSequence::class.java))).thenAnswer { invocation ->
+            (invocation.arguments[0] as? CharSequence).let { a ->
+                !(a != null && a.isNotEmpty())
+            }
+        }
+    }
+
+    @Test
+    fun testEquals() {
+        val projectId = "asdf"
+
+        val parentKey1 = CreateTaskLoader.ParentKey.ProjectParentKey(projectId)
+        val parentKey2 = CreateTaskLoader.ParentKey.ProjectParentKey(projectId)
+
+        Assert.assertTrue(parentKey1 == parentKey2)
+    }
+}

36a6f6eedc446540fd19dd65b0e272a7aa5ce0c5
Krystian Wsul <krystianwsul@gmail.com>
2019-05-12 19:06:54 +0200

    SettingsActivity: implement regular login

--- app/src/main/java/com/krystianwsul/checkme/gui/SettingsActivity.kt
+++ app/src/main/java/com/krystianwsul/checkme/gui/SettingsActivity.kt
@@ -4,11 +4,15 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.material.snackbar.Snackbar
 import com.krystianwsul.checkme.MyApplication
 import com.krystianwsul.checkme.R
 import com.krystianwsul.checkme.domainmodel.DomainFactory
 import com.krystianwsul.checkme.persistencemodel.SaveService
 import com.krystianwsul.checkme.utils.toSingle
+import kotlinx.android.synthetic.main.settings_activity.*
 import kotlinx.android.synthetic.main.toolbar.*
 
 class SettingsActivity : AbstractActivity() {
@@ -26,13 +30,30 @@ class SettingsActivity : AbstractActivity() {
 
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
 
-        supportFragmentManager
-                .beginTransaction()
-                .replace(R.id.settingsFrame, SettingsFragment())
-                .commit()
+        supportFragmentManager.apply {
+            if (findFragmentById(R.id.settingsFrame) == null)
+                beginTransaction().replace(R.id.settingsFrame, SettingsFragment()).commit()
+        }
+    }
+
+    private fun updateFromAccount(googleSignInAccount: GoogleSignInAccount) {
+        googleSignInAccount.photoUrl?.let { url ->
+            DomainFactory.addFirebaseListener {
+                it.updatePhotoUrl(SaveService.Source.GUI, url.toString())
+            }
+        }
+
+        Snackbar.make(settingsRoot, R.string.profileUpdated, Snackbar.LENGTH_SHORT).show()
     }
 
     class SettingsFragment : PreferenceFragmentCompat() {
+
+        companion object {
+
+            private const val RC_SIGN_IN = 1000
+        }
+
+        private val settingsActivity get() = (activity as SettingsActivity)
 
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey)
@@ -43,21 +64,25 @@ class SettingsActivity : AbstractActivity() {
                         .silentSignIn()
                         .toSingle()
                         .subscribe { wrapper ->
-                            if (wrapper.value != null) {
-                                wrapper.value
-                                        .photoUrl
-                                        ?.let { url ->
-                                            DomainFactory.addFirebaseListener {
-                                                it.updatePhotoUrl(SaveService.Source.GUI, url.toString())
-                                            }
-                                        }
-                            } else {
-                                // todo login
-                            }
+                            if (wrapper.value != null)
+                                settingsActivity.updateFromAccount(wrapper.value)
+                            else
+                                startActivityForResult(MyApplication.instance.googleSigninClient.signInIntent, RC_SIGN_IN)
                         }
 
                 true
             }
+        }
+
+        override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+            super.onActivityResult(requestCode, resultCode, data)
+
+            check(requestCode == RC_SIGN_IN)
+
+            Auth.GoogleSignInApi
+                    .getSignInResultFromIntent(data)!!
+                    .signInAccount
+                    ?.let { settingsActivity.updateFromAccount(it) }
         }
     }
 }

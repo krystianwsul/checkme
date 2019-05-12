11dd8ef846c3899f0609657d21a7a9cf1b867005
Krystian Wsul <krystianwsul@gmail.com>
2019-05-12 18:53:45 +0200

    SettingsActivity: implement silent login

--- app/src/main/java/com/krystianwsul/checkme/gui/SettingsActivity.kt
+++ app/src/main/java/com/krystianwsul/checkme/gui/SettingsActivity.kt
@@ -2,9 +2,13 @@ package com.krystianwsul.checkme.gui
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import com.krystianwsul.checkme.MyApplication
 import com.krystianwsul.checkme.R
+import com.krystianwsul.checkme.domainmodel.DomainFactory
+import com.krystianwsul.checkme.persistencemodel.SaveService
+import com.krystianwsul.checkme.utils.toSingle
 import kotlinx.android.synthetic.main.toolbar.*
 
 class SettingsActivity : AbstractActivity() {
@@ -32,6 +36,28 @@ class SettingsActivity : AbstractActivity() {
 
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey)
+
+            findPreference<Preference>(getString(R.string.accountDetails))!!.setOnPreferenceClickListener {
+                MyApplication.instance
+                        .googleSigninClient
+                        .silentSignIn()
+                        .toSingle()
+                        .subscribe { wrapper ->
+                            if (wrapper.value != null) {
+                                wrapper.value
+                                        .photoUrl
+                                        ?.let { url ->
+                                            DomainFactory.addFirebaseListener {
+                                                it.updatePhotoUrl(SaveService.Source.GUI, url.toString())
+                                            }
+                                        }
+                            } else {
+                                // todo login
+                            }
+                        }
+
+                true
+            }
         }
     }
 }

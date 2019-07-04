ade7cf0895be2444facdef4a0bef89f418744ef4
krystianwsul <krystianwsul@gmail.com>
2019-07-04 07:15:02 +0200

    CreateTaskActivity: fix keyboard resize

---
+++ app/src/main/java/com/krystianwsul/checkme/gui/tasks/InsetLayout.kt
@@ -0,0 +1,15 @@
+package com.krystianwsul.checkme.gui.tasks
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.WindowInsets
+import android.widget.FrameLayout
+
+class InsetLayout @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : FrameLayout(context, attrs, defStyleAttr) {
+
+    init {
+        fitsSystemWindows = true
+    }
+
+    override fun onApplyWindowInsets(insets: WindowInsets) = super.onApplyWindowInsets(insets.replaceSystemWindowInsets(0, insets.systemWindowInsetTop, 0, insets.systemWindowInsetBottom))!!
+}

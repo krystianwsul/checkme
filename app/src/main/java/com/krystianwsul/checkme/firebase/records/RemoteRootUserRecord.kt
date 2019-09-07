1e9de06c34eaced9eb7bcbce5300cd32a2c12603
krystianwsul <krystianwsul@gmail.com>
2019-09-07 13:33:46 +0200

    RemoteRootUserRecord: decouple

--- app/src/main/java/com/krystianwsul/checkme/firebase/records/RemoteRootUserRecord.kt
+++ app/src/main/java/com/krystianwsul/checkme/firebase/records/RemoteRootUserRecord.kt
@@ -1,6 +1,5 @@
 package com.krystianwsul.checkme.firebase.records
 
-import android.text.TextUtils
 import com.krystianwsul.checkme.firebase.UserData
 import com.krystianwsul.common.firebase.UserWrapper
 
@@ -34,7 +33,7 @@ open class RemoteRootUserRecord(create: Boolean, override val createObject: User
     open val photoUrl get() = userJson.photoUrl
 
     fun removeFriendOf(friendId: String) {
-        check(!TextUtils.isEmpty(friendId))
+        check(friendId.isNotEmpty())
 
         val friendOf = createObject.friendOf
         check(friendOf.containsKey(friendId))

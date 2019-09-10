9493fd672a41b5cac9e6360fb2c41c48f94cac24
krystianwsul <krystianwsul@gmail.com>
2019-09-10 16:57:10 +0200

    TaskHierarchyContainer: remove java references

--- app/src/main/java/com/krystianwsul/checkme/utils/TaskHierarchyContainer.kt
+++ app/src/main/java/com/krystianwsul/checkme/utils/TaskHierarchyContainer.kt
@@ -1,18 +1,14 @@
 package com.krystianwsul.checkme.utils
 
-import com.google.common.collect.HashMultimap
 import com.krystianwsul.checkme.domainmodel.TaskHierarchy
 import com.krystianwsul.common.utils.TaskKey
-
-import java.util.*
 
 class TaskHierarchyContainer<T, U : TaskHierarchy> {
 
     private val taskHierarchiesById = HashMap<T, U>()
 
-    private val taskHierarchiesByParent = HashMultimap.create<TaskKey, U>()
-
-    private val taskHierarchiesByChild = HashMultimap.create<TaskKey, U>()
+    private val taskHierarchiesByParent = MultiMap<U>()
+    private val taskHierarchiesByChild = MultiMap<U>()
 
     fun add(id: T, taskHierarchy: U) {
         check(!taskHierarchiesById.containsKey(id))
@@ -40,11 +36,30 @@ class TaskHierarchyContainer<T, U : TaskHierarchy> {
         check(taskHierarchiesByParent.remove(parentTaskKey, taskHierarchy))
     }
 
-    fun getByChildTaskKey(childTaskKey: TaskKey): Set<U> = taskHierarchiesByChild.get(childTaskKey)!!
+    fun getByChildTaskKey(childTaskKey: TaskKey): Set<U> = taskHierarchiesByChild.get(childTaskKey)
 
-    fun getByParentTaskKey(parentTaskKey: TaskKey): Set<U> = taskHierarchiesByParent.get(parentTaskKey)!!
+    fun getByParentTaskKey(parentTaskKey: TaskKey): Set<U> = taskHierarchiesByParent.get(parentTaskKey)
 
     fun getById(id: T) = taskHierarchiesById[id]!!
 
     val all: Collection<TaskHierarchy> get() = taskHierarchiesById.values
+
+    private class MultiMap<U : TaskHierarchy> {
+
+        private val values = mutableMapOf<TaskKey, MutableSet<U>>()
+
+        fun put(taskKey: TaskKey, taskHierarchy: U): Boolean {
+            if (!values.containsKey(taskKey))
+                values[taskKey] = mutableSetOf()
+            return values.getValue(taskKey).add(taskHierarchy)
+        }
+
+        fun containsEntry(taskKey: TaskKey, taskHierarchy: U) = values[taskKey]?.contains(taskHierarchy)
+                ?: false
+
+        fun remove(taskKey: TaskKey, taskHierarchy: U) = values[taskKey]?.remove(taskHierarchy)
+                ?: false
+
+        fun get(taskKey: TaskKey) = values.getValue(taskKey).toMutableSet()
+    }
 }

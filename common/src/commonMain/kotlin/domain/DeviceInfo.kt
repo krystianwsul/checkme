f9eb7b814a1a3f9311768d71185c5290b9badbf4
krystianwsul <krystianwsul@gmail.com>
2019-09-10 11:54:20 +0200

    DeviceInfo: common

---
+++ common/src/commonMain/kotlin/domain/DeviceInfo.kt
@@ -0,0 +1,10 @@
+package com.krystianwsul.common.domain
+
+data class DeviceInfo(
+        val userInfo: UserInfo,
+        var token: String?) {
+
+    val email get() = userInfo.email
+    val name get() = userInfo.name
+    val key get() = userInfo.key
+}

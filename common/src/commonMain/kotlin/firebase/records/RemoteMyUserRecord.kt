a8e91a87fd2661c14bbbf7138543f658c02c6584
krystianwsul <krystianwsul@gmail.com>
2019-10-19 11:21:08 +0200

    RemoteMyUserRecord: committer

--- common/src/commonMain/kotlin/firebase/records/RemoteMyUserRecord.kt
+++ common/src/commonMain/kotlin/firebase/records/RemoteMyUserRecord.kt
@@ -18,35 +18,9 @@ class RemoteMyUserRecord(
         addValue("$key/$USER_DATA/tokens/$uuid", token)
     }
 
-    override var photoUrl
-        get() = super.photoUrl
-        set(value) {
-            if (value == userJson.photoUrl)
-                return
+    override var photoUrl by Committer(userJson::photoUrl, "$key/$USER_DATA")
 
-            check(!value.isNullOrEmpty())
+    override var defaultReminder by Committer(createObject::defaultReminder)
 
-            userJson.photoUrl = value
-            addValue("$key/$USER_DATA/photoUrl", value)
-        }
-
-    override var defaultReminder: Boolean
-        get() = createObject.defaultReminder
-        set(value) {
-            if (value == createObject.defaultReminder)
-                return
-
-            createObject.defaultReminder = value
-            addValue("$key/defaultReminder", value)
-        }
-
-    override var defaultTab: Int
-        get() = createObject.defaultTab
-        set(value) {
-            if (value == createObject.defaultTab)
-                return
-
-            createObject.defaultTab = value
-            addValue("$key/defaultTab", value)
-        }
+    override var defaultTab by Committer(createObject::defaultTab)
 }

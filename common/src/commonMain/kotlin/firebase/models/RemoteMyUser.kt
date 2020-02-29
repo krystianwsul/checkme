2730f4aa6ae07958535a4475974d80e4dcb0b2a2
krystianwsul <krystianwsul@gmail.com>
2020-02-29 14:23:24 +0100

    RemoteMyUser: fix error in previous commit

--- common/src/commonMain/kotlin/firebase/models/RemoteMyUser.kt
+++ common/src/commonMain/kotlin/firebase/models/RemoteMyUser.kt
@@ -4,4 +4,11 @@ import com.krystianwsul.common.firebase.RemoteMyUserInterface
 import com.krystianwsul.common.firebase.records.RemoteMyUserRecord
 
 
-class RemoteMyUser(private val remoteMyUserRecord: RemoteMyUserRecord) : RemoteRootUser(remoteMyUserRecord), RemoteMyUserInterface by remoteMyUserRecord
+class RemoteMyUser(private val remoteMyUserRecord: RemoteMyUserRecord) : RemoteRootUser(remoteMyUserRecord), RemoteMyUserInterface by remoteMyUserRecord {
+
+    override var photoUrl
+        get() = super.photoUrl
+        set(value) {
+            remoteMyUserRecord.photoUrl = value
+        }
+}

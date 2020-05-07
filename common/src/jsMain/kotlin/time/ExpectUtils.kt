dab412292b06a36f601f9aaeec607cb00ba905e1
krystianwsul <krystianwsul@gmail.com>
2020-05-07 12:31:18 +0200

    common.time.ExpectUtils: fix formatting month

--- common/src/jsMain/kotlin/time/ExpectUtils.kt
+++ common/src/jsMain/kotlin/time/ExpectUtils.kt
@@ -2,6 +2,6 @@ package com.krystianwsul.common.time
 
 import com.soywiz.klock.DateTimeTz
 
-actual fun DateTimeTz.formatDate() = toString("yyyy-mm-dd")
+actual fun DateTimeTz.formatDate() = toString("yyyy-MM-dd")
 
 actual fun DateTimeTz.formatTime() = toString("HH:mm")

cbe38902f559770ad54b772b4f517d438865b8da
krystianwsul <krystianwsul@gmail.com>
2020-05-30 18:52:40 +0200

    AllRemindersDialogFragment: fix ordering

--- app/src/main/java/com/krystianwsul/checkme/gui/edit/AllRemindersDialogFragment.kt
+++ app/src/main/java/com/krystianwsul/checkme/gui/edit/AllRemindersDialogFragment.kt
@@ -15,8 +15,8 @@ class AllRemindersDialogFragment : AbstractDialogFragment() {
     lateinit var listener: (Boolean) -> Unit
 
     override fun onCreateDialog(savedInstanceState: Bundle?) = MaterialAlertDialogBuilder(requireContext()).setMessage(R.string.joinAllFutureReminders)
-            .setNegativeButton(R.string.allFutureReminders) { _, _ -> listener(true) }
-            .setPositiveButton(R.string.justThese) { _, _ -> listener(false) }
+            .setPositiveButton(R.string.allFutureReminders) { _, _ -> listener(true) }
+            .setNegativeButton(R.string.justThese) { _, _ -> listener(false) }
             .setNeutralButton(R.string.removeInstancesCancel) { _, _ -> }
             .create()
 }

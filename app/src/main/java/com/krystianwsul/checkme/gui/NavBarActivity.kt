2a8447110433e94a00ef9a75ded06de65df6db70
krystianwsul <krystianwsul@gmail.com>
2020-09-03 21:26:06 +0200

    NavBarActivity: fix landscape navbar color for button nav

--- app/src/main/java/com/krystianwsul/checkme/gui/NavBarActivity.kt
+++ app/src/main/java/com/krystianwsul/checkme/gui/NavBarActivity.kt
@@ -13,17 +13,16 @@ abstract class NavBarActivity : AbstractActivity() {
         val landscape = resources.isLandscape
 
         window.apply {
-            var flags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            if (!landscape) {
+                var flags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
 
-            navigationBarColor = ContextCompat.getColor(context, R.color.primaryColor12Solid)
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+                    flags = flags or View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
 
-            if (!landscape)
-                flags = flags or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                decorView.systemUiVisibility = decorView.systemUiVisibility or flags
 
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
-                flags = flags or View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
-
-            decorView.systemUiVisibility = decorView.systemUiVisibility or flags
+                navigationBarColor = ContextCompat.getColor(context, R.color.primaryColor12Solid)
+            }
         }
 
         super.onCreate(savedInstanceState)

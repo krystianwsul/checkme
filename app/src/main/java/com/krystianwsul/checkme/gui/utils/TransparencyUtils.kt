f4a83465c9dbffc7b205e301b931cb484c6c2676
krystianwsul <krystianwsul@gmail.com>
2020-09-14 21:56:58 +0200

    TransparencyUtils: fix deprecation issues

--- app/src/main/java/com/krystianwsul/checkme/gui/utils/TransparencyUtils.kt
+++ app/src/main/java/com/krystianwsul/checkme/gui/utils/TransparencyUtils.kt
@@ -3,18 +3,27 @@ package com.krystianwsul.checkme.gui.utils
 import android.os.Build
 import android.view.View
 import android.view.Window
+import android.view.WindowInsetsController
 import androidx.core.content.ContextCompat
+import androidx.core.view.WindowCompat
 import com.krystianwsul.checkme.R
 
 fun Window.setNavBarTransparency(landscape: Boolean) {
     if (!landscape) {
-        var flags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+        WindowCompat.setDecorFitsSystemWindows(this, false)
 
         navigationBarColor = ContextCompat.getColor(context, R.color.primaryColor12)
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
-            flags = flags or View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
-
-        decorView.systemUiVisibility = decorView.systemUiVisibility or flags
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            insetsController!!.setSystemBarsAppearance(
+                    WindowInsetsController.APPEARANCE_LIGHT_NAVIGATION_BARS,
+                    WindowInsetsController.APPEARANCE_LIGHT_NAVIGATION_BARS
+            )
+        } else if (Build.VERSION.SDK_INT > Build.VERSION_CODES.O) {
+            @Suppress("DEPRECATION")
+            decorView.apply {
+                systemUiVisibility = systemUiVisibility or View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
+            }
+        }
     }
 }

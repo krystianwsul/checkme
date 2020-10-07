8df8dbacd1d525da76458674b5a05cf858a1b4ec
krystianwsul <krystianwsul@gmail.com>
2020-10-07 16:11:10 +0200

    TaskListFragment: gray out all tasks that are hidden outside of "show deleted" search

--- app/src/main/java/com/krystianwsul/checkme/gui/tasks/TaskListFragment.kt
+++ app/src/main/java/com/krystianwsul/checkme/gui/tasks/TaskListFragment.kt
@@ -612,7 +612,7 @@ class TaskListFragment : AbstractFragment(), FabUser, ListItemAddedScroller {
                 return treeNode
             }
 
-            private val disabledOverride get() = colorDisabled.takeUnless { childTaskData.current }
+            private val disabledOverride get() = colorDisabled.takeUnless { childTaskData.isVisible }
 
             override val children
                 get() = if ((childTaskData.children.isEmpty() || treeNode.isExpanded) && childTaskData.note.isNullOrEmpty()) {

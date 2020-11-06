6331c019eddb39d93c8760483ba7cf8c393a6afc
krystianwsul <krystianwsul@gmail.com>
2020-11-06 17:13:24 +0100

    NotificationWrapperImplO: add medium channel

--- app/src/main/java/com/krystianwsul/checkme/domainmodel/notifications/NotificationWrapperImplO.kt
+++ app/src/main/java/com/krystianwsul/checkme/domainmodel/notifications/NotificationWrapperImplO.kt
@@ -13,16 +13,34 @@ open class NotificationWrapperImplO : NotificationWrapperImplN() {
     companion object {
 
         private const val CHANNEL_ID = "channel"
-        private val CHANNEL = NotificationChannel(CHANNEL_ID, "Reminders", NotificationManager.IMPORTANCE_HIGH)
+
+        private val CHANNEL = NotificationChannel(
+                CHANNEL_ID,
+                "Heads-up reminders",
+                NotificationManager.IMPORTANCE_HIGH
+        )
+
+        private const val MEDIUM_CHANNEL_ID = "channel"
+
+        private val MEDIUM_CHANNEL = NotificationChannel(
+                MEDIUM_CHANNEL_ID,
+                "Regular reminders",
+                NotificationManager.IMPORTANCE_DEFAULT
+        )
 
         private const val SILENT_CHANNEL_ID = "silentChannel"
-        private val SILENT_CHANNEL = NotificationChannel(SILENT_CHANNEL_ID, "Silent reminders", NotificationManager.IMPORTANCE_LOW)
+
+        private val SILENT_CHANNEL = NotificationChannel(
+                SILENT_CHANNEL_ID,
+                "Silent reminders",
+                NotificationManager.IMPORTANCE_LOW
+        )
     }
 
     init {
         CHANNEL.enableVibration(true)
 
-        notificationManager.createNotificationChannels(listOf(CHANNEL, SILENT_CHANNEL))
+        notificationManager.createNotificationChannels(listOf(CHANNEL, MEDIUM_CHANNEL, SILENT_CHANNEL))
     }
 
     override fun newBuilder(silent: Boolean) = NotificationCompat.Builder(MyApplication.instance, if (silent) SILENT_CHANNEL_ID else CHANNEL_ID)

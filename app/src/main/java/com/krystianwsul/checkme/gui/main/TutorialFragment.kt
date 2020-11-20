a1bc3fda5b7ac25b14c533a07131377d7d857002
krystianwsul <krystianwsul@gmail.com>
2020-11-20 13:34:04 +0100

    TutorialFragment: migrate to ViewBinding

--- app/src/main/java/com/krystianwsul/checkme/gui/main/TutorialFragment.kt
+++ app/src/main/java/com/krystianwsul/checkme/gui/main/TutorialFragment.kt
@@ -6,8 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.krystianwsul.checkme.R
+import com.krystianwsul.checkme.databinding.FragmentTutorialBinding
 import com.krystianwsul.checkme.gui.base.AbstractFragment
-import kotlinx.android.synthetic.main.fragment_tutorial.*
+import com.krystianwsul.checkme.gui.utils.ResettableProperty
 
 class TutorialFragment : AbstractFragment() {
 
@@ -16,13 +17,14 @@ class TutorialFragment : AbstractFragment() {
         private const val POSITION_KEY = "position"
 
         fun newInstance(position: Int) = TutorialFragment().apply {
-            arguments = Bundle().apply {
-                putInt(POSITION_KEY, position)
-            }
+            arguments = Bundle().apply { putInt(POSITION_KEY, position) }
         }
     }
 
     private var position = 0
+
+    private val bindingProperty = ResettableProperty<FragmentTutorialBinding>()
+    private var binding by bindingProperty
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,16 +32,14 @@ class TutorialFragment : AbstractFragment() {
         position = requireArguments().getInt(POSITION_KEY)
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) = inflater.inflate(R.layout.fragment_tutorial, container, false)!!
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) = FragmentTutorialBinding.inflate(inflater, container, false).also { binding = it }.root
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        tutorialSignIn?.apply {
-            visibility = if ((activity as TutorialActivity).help) View.GONE else View.INVISIBLE
-        }
+        binding.tutorialSignIn?.visibility = if ((activity as TutorialActivity).help) View.GONE else View.INVISIBLE
 
-        tutorialImage.setImageResource(when (position) {
+        binding.tutorialImage.setImageResource(when (position) {
             0 -> R.drawable.tutorial_1
             1 -> R.drawable.tutorial_2
             2 -> R.drawable.tutorial_3
@@ -47,12 +47,18 @@ class TutorialFragment : AbstractFragment() {
             else -> throw IllegalArgumentException()
         })
 
-        tutorialText.setText(when (position) {
+        binding.tutorialText.setText(when (position) {
             0 -> R.string.tutorial1
             1 -> R.string.tutorial2
             2 -> R.string.tutorial3
             3 -> R.string.tutorial4
             else -> throw IllegalArgumentException()
         })
+    }
+
+    override fun onDestroyView() {
+        bindingProperty.reset()
+
+        super.onDestroyView()
     }
 }

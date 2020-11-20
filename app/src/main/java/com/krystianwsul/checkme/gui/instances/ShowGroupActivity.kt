06bace9527c631ced80c409b74b43f8a0fce7298
krystianwsul <krystianwsul@gmail.com>
2020-11-20 13:19:36 +0100

    ShowGroupActivity: migrate to ViewBinding

--- app/src/main/java/com/krystianwsul/checkme/gui/instances/ShowGroupActivity.kt
+++ app/src/main/java/com/krystianwsul/checkme/gui/instances/ShowGroupActivity.kt
@@ -6,6 +6,8 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.view.ActionMode
 import com.krystianwsul.checkme.R
+import com.krystianwsul.checkme.databinding.ActivityShowGroupBinding
+import com.krystianwsul.checkme.databinding.BottomBinding
 import com.krystianwsul.checkme.domainmodel.DomainFactory
 import com.krystianwsul.checkme.domainmodel.extensions.clearTaskEndTimeStamps
 import com.krystianwsul.checkme.domainmodel.extensions.setTaskEndTimeStamps
@@ -15,6 +17,7 @@ import com.krystianwsul.checkme.gui.instances.list.GroupListListener
 import com.krystianwsul.checkme.gui.instances.tree.NodeHolder
 import com.krystianwsul.checkme.persistencemodel.SaveService
 import com.krystianwsul.checkme.utils.startDate
+import com.krystianwsul.checkme.utils.tryGetFragment
 import com.krystianwsul.checkme.viewmodels.ShowGroupViewModel
 import com.krystianwsul.checkme.viewmodels.getViewModel
 import com.krystianwsul.common.time.ExactTimeStamp
@@ -22,10 +25,6 @@ import com.krystianwsul.common.time.TimeStamp
 import com.krystianwsul.common.utils.TaskKey
 import com.krystianwsul.treeadapter.TreeViewAdapter
 import io.reactivex.rxkotlin.plusAssign
-import kotlinx.android.synthetic.main.activity_show_group.*
-import kotlinx.android.synthetic.main.bottom.*
-import kotlinx.android.synthetic.main.empty_text.*
-import kotlinx.android.synthetic.main.toolbar_collapse.*
 import java.io.Serializable
 
 class ShowGroupActivity : AbstractActivity(), GroupListListener {
@@ -36,10 +35,10 @@ class ShowGroupActivity : AbstractActivity(), GroupListListener {
 
         private const val TAG_DELETE_INSTANCES = "deleteInstances"
 
-        fun getIntent(exactTimeStamp: ExactTimeStamp.Local, context: Context) = Intent(context, ShowGroupActivity::class
-                .java).apply {
-            putExtra(TIME_KEY, exactTimeStamp.long)
-        }
+        fun getIntent(exactTimeStamp: ExactTimeStamp.Local, context: Context) = Intent(
+                context,
+                ShowGroupActivity::class.java
+        ).apply { putExtra(TIME_KEY, exactTimeStamp.long) }
     }
 
     private lateinit var timeStamp: TimeStamp
@@ -48,11 +47,15 @@ class ShowGroupActivity : AbstractActivity(), GroupListListener {
 
     private lateinit var showGroupViewModel: ShowGroupViewModel
 
-    override val snackbarParent get() = showGroupCoordinator!!
+    override val snackbarParent get() = binding.showGroupCoordinator
 
     private val deleteInstancesListener = { taskKeys: Serializable, removeInstances: Boolean ->
         @Suppress("UNCHECKED_CAST")
-        val taskUndoData = DomainFactory.instance.setTaskEndTimeStamps(SaveService.Source.GUI, taskKeys as Set<TaskKey>, removeInstances)
+        val taskUndoData = DomainFactory.instance.setTaskEndTimeStamps(
+                SaveService.Source.GUI,
+                taskKeys as Set<TaskKey>,
+                removeInstances
+        )
 
         showSnackbarRemoved(taskUndoData.taskKeys.size) {
             DomainFactory.instance.clearTaskEndTimeStamps(SaveService.Source.GUI, taskUndoData)
@@ -64,15 +67,25 @@ class ShowGroupActivity : AbstractActivity(), GroupListListener {
         override fun onReceive(context: Context?, intent: Intent?) = showGroupViewModel.refresh()
     }
 
-    override val instanceSearch by lazy { collapseAppBarLayout.searchData }
+    override val instanceSearch by lazy {
+        binding.showGroupToolbarCollapseInclude
+                .collapseAppBarLayout
+                .searchData
+    }
 
     private var data: ShowGroupViewModel.Data? = null
 
+    private lateinit var binding: ActivityShowGroupBinding
+    private lateinit var bottomBinding: BottomBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_show_group)
 
-        groupListFragment.listener = this
+        binding = ActivityShowGroupBinding.inflate(layoutInflater)
+        bottomBinding = BottomBinding.bind(binding.root)
+        setContentView(binding.root)
+
+        binding.groupListFragment.listener = this
 
         check(intent.hasExtra(TIME_KEY))
 
@@ -81,19 +94,21 @@ class ShowGroupActivity : AbstractActivity(), GroupListListener {
 
         timeStamp = TimeStamp.fromMillis(time)
 
-        groupListFragment.setFab(bottomFab)
+        binding.groupListFragment.setFab(bottomBinding.bottomFab)
 
-        collapseAppBarLayout.apply {
-            hideShowDeleted()
+        binding.showGroupToolbarCollapseInclude
+                .collapseAppBarLayout
+                .apply {
+                    hideShowDeleted()
 
-            inflateMenu(R.menu.show_task_menu_top)
-            setOnMenuItemClickListener {
-                when (it.itemId) {
-                    R.id.actionShowTaskSearch -> collapseAppBarLayout.startSearch()
-                    else -> throw IllegalArgumentException()
+                    inflateMenu(R.menu.show_task_menu_top)
+                    setOnMenuItemClickListener {
+                        when (it.itemId) {
+                            R.id.actionShowTaskSearch -> startSearch()
+                            else -> throw IllegalArgumentException()
+                        }
+                    }
                 }
-            }
-        }
 
         updateTopMenu()
         initBottomBar()
@@ -104,23 +119,25 @@ class ShowGroupActivity : AbstractActivity(), GroupListListener {
             createDisposable += data.subscribe { onLoadFinished(it) }
         }
 
-        (supportFragmentManager.findFragmentByTag(TAG_DELETE_INSTANCES) as? RemoveInstancesDialogFragment)?.listener = deleteInstancesListener
+        tryGetFragment<RemoveInstancesDialogFragment>(TAG_DELETE_INSTANCES)?.listener = deleteInstancesListener
 
         startDate(receiver)
     }
 
     private fun updateTopMenu() {
-        collapseAppBarLayout.menu.apply {
-            findItem(R.id.actionShowTaskSearch).isVisible = !data?.groupListDataWrapper
-                    ?.instanceDatas
-                    .isNullOrEmpty()
-        }
+        binding.showGroupToolbarCollapseInclude
+                .collapseAppBarLayout
+                .menu
+                .findItem(R.id.actionShowTaskSearch)
+                .isVisible = !data?.groupListDataWrapper
+                ?.instanceDatas
+                .isNullOrEmpty()
     }
 
     override fun onStart() {
         super.onStart()
 
-        groupListFragment.checkCreatedTaskKey()
+        binding.groupListFragment.checkCreatedTaskKey()
     }
 
     private fun onLoadFinished(data: ShowGroupViewModel.Data) {
@@ -128,7 +145,9 @@ class ShowGroupActivity : AbstractActivity(), GroupListListener {
 
         val immediate = data.immediate
 
-        collapseAppBarLayout.setText(data.displayText, null, emptyTextLayout, immediate)
+        binding.showGroupToolbarCollapseInclude
+                .collapseAppBarLayout
+                .setText(data.displayText, null, binding.groupListFragment.emptyTextLayout, immediate)
 
         if (data.groupListDataWrapper == null) {
             finish()
@@ -136,7 +155,7 @@ class ShowGroupActivity : AbstractActivity(), GroupListListener {
             return
         }
 
-        groupListFragment.setTimeStamp(timeStamp, data.dataId, immediate, data.groupListDataWrapper)
+        binding.groupListFragment.setTimeStamp(timeStamp, data.dataId, immediate, data.groupListDataWrapper)
 
         updateTopMenu()
     }
@@ -158,13 +177,15 @@ class ShowGroupActivity : AbstractActivity(), GroupListListener {
     }
 
     override fun initBottomBar() {
-        bottomAppBar.apply {
+        bottomBinding.bottomAppBar.apply {
             replaceMenu(R.menu.menu_select_all)
 
             setOnMenuItemClickListener { item ->
                 check(item.itemId == R.id.action_select_all)
 
-                groupListFragment.treeViewAdapter.selectAll()
+                binding.groupListFragment
+                        .treeViewAdapter
+                        .selectAll()
 
                 true
             }
@@ -177,11 +198,16 @@ class ShowGroupActivity : AbstractActivity(), GroupListListener {
                 .show(supportFragmentManager, TAG_DELETE_INSTANCES)
     }
 
-    override fun getBottomBar() = bottomAppBar!!
+    override fun getBottomBar() = bottomBinding.bottomAppBar
 
     private fun updateBottomMenu() {
-        bottomAppBar.menu.findItem(R.id.action_select_all)?.isVisible = selectAllVisible
+        bottomBinding.bottomAppBar
+                .menu
+                .findItem(R.id.action_select_all)
+                ?.isVisible = selectAllVisible
     }
 
-    override fun setToolbarExpanded(expanded: Boolean) = collapseAppBarLayout.setExpanded(expanded)
+    override fun setToolbarExpanded(expanded: Boolean) = binding.showGroupToolbarCollapseInclude
+            .collapseAppBarLayout
+            .setExpanded(expanded)
 }

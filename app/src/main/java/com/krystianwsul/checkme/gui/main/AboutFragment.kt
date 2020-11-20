2dfd14f2b27209a28e7da4c7f6b836c349b7e644
krystianwsul <krystianwsul@gmail.com>
2020-11-20 17:20:15 +0100

    AboutFragment: migrate to ViewBinding

--- app/src/main/java/com/krystianwsul/checkme/gui/main/AboutFragment.kt
+++ app/src/main/java/com/krystianwsul/checkme/gui/main/AboutFragment.kt
@@ -11,11 +11,12 @@ import android.widget.ImageView
 import com.krystianwsul.checkme.BuildConfig
 import com.krystianwsul.checkme.R
 import com.krystianwsul.checkme.RemoteConfig
+import com.krystianwsul.checkme.databinding.FragmentAboutBinding
 import com.krystianwsul.checkme.gui.base.AbstractFragment
+import com.krystianwsul.checkme.gui.utils.ResettableProperty
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.rxkotlin.addTo
-import kotlinx.android.synthetic.main.fragment_about.*
 import mehdi.sakout.aboutpage.AboutPage
 import mehdi.sakout.aboutpage.Element
 import java.util.concurrent.TimeUnit
@@ -27,7 +28,10 @@ class AboutFragment : AbstractFragment() {
         fun newInstance() = AboutFragment()
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) = inflater.inflate(R.layout.fragment_about, container, false)!!
+    private val bindingProperty = ResettableProperty<FragmentAboutBinding>()
+    private var binding by bindingProperty
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) = FragmentAboutBinding.inflate(inflater, container, false).also { binding = it }.root
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -38,27 +42,34 @@ class AboutFragment : AbstractFragment() {
                 .subscribe { element.intent = Intent(Intent.ACTION_VIEW, Uri.parse(it.beniaUrl)) }
                 .addTo(viewCreatedDisposable)
 
-        aboutRoot.addView(AboutPage(requireContext()).setImage(R.drawable.ic_launcher_round_try_sign2_unscaled)
-                .setDescription(getString(R.string.aboutDescription))
-                .addItem(element)
-                .addEmail("krystianwsul@gmail.com")
-                .addPlayStore("com.krystianwsul.checkme")
-                .addItem(Element().setTitle(getString(R.string.version) + " " + BuildConfig.VERSION_NAME))
-                .create().apply {
-                    findViewById<ImageView>(mehdi.sakout.aboutpage.R.id.image).apply {
-                        layoutParams = layoutParams.apply {
-                            width = resources.getDimension(R.dimen.splashWidth).toInt()
-                            height = resources.getDimension(R.dimen.splashHeight).toInt()
-                        }
-                    }
-                })
+        binding.aboutRoot
+                .addView(AboutPage(requireContext()).setImage(R.drawable.ic_launcher_round_try_sign2_unscaled)
+                        .setDescription(getString(R.string.aboutDescription))
+                        .addItem(element)
+                        .addEmail("krystianwsul@gmail.com")
+                        .addPlayStore("com.krystianwsul.checkme")
+                        .addItem(Element().setTitle(getString(R.string.version) + " " + BuildConfig.VERSION_NAME))
+                        .create().apply {
+                            findViewById<ImageView>(mehdi.sakout.aboutpage.R.id.image).apply {
+                                layoutParams = layoutParams.apply {
+                                    width = resources.getDimension(R.dimen.splashWidth).toInt()
+                                    height = resources.getDimension(R.dimen.splashHeight).toInt()
+                                }
+                            }
+                        })
     }
 
     fun onShown() {
         Observable.just(Unit)
                 .delay(1, TimeUnit.SECONDS)
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe { aboutRoot.apply { smoothScrollTo(0, bottom) } }
+                .subscribe { binding.aboutRoot.apply { smoothScrollTo(0, bottom) } }
                 .addTo(viewCreatedDisposable)
+    }
+
+    override fun onDestroyView() {
+        bindingProperty.reset()
+
+        super.onDestroyView()
     }
 }

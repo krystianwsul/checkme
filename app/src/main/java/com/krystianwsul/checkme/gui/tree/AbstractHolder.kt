dbf23d894e84372b20aff12092a0f9102ebbf6e5
krystianwsul <krystianwsul@gmail.com>
2020-11-29 12:56:58 +0100

    BaseAdapter: move rx into AbstractHolder

--- app/src/main/java/com/krystianwsul/checkme/gui/tree/AbstractHolder.kt
+++ app/src/main/java/com/krystianwsul/checkme/gui/tree/AbstractHolder.kt
@@ -6,12 +6,18 @@ import android.widget.LinearLayout
 import android.widget.RelativeLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.chip.ChipGroup
+import com.jakewharton.rxbinding3.view.clicks
+import com.jakewharton.rxbinding3.view.longClicks
 import com.krystianwsul.checkme.gui.instances.tree.singleline.SingleLineHolder
 import com.krystianwsul.checkme.gui.tree.avatar.AvatarHolder
+import com.krystianwsul.checkme.gui.tree.checkable.CheckBoxState
 import com.krystianwsul.checkme.gui.tree.checkable.CheckableHolder
+import com.krystianwsul.checkme.gui.tree.checkable.CheckableModelNode
 import com.krystianwsul.checkme.gui.tree.expandable.ExpandableHolder
 import com.krystianwsul.checkme.gui.tree.multiline.MultiLineHolder
 import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.rxkotlin.addTo
+import io.reactivex.rxkotlin.merge
 
 abstract class AbstractHolder(view: View) :
         RecyclerView.ViewHolder(view),
@@ -34,4 +40,34 @@ abstract class AbstractHolder(view: View) :
     final override val compositeDisposable = CompositeDisposable()
 
     override val holderPosition get() = adapterPosition
+
+    override fun onViewAttachedToWindow() {
+        itemView.clicks()
+                .mapNodes()
+                .subscribe { (treeNode, _) -> treeNode.onClick(this) }
+                .addTo(compositeDisposable)
+
+        itemView.longClicks { true }
+                .mapNodes()
+                .subscribe { (_, groupHolderNode) -> groupHolderNode.onLongClick(this) }
+                .addTo(compositeDisposable)
+
+        rowExpand.clicks()
+                .mapNodes()
+                .subscribe { (treeNode, _) -> treeNode.onExpandClick() }
+                .addTo(compositeDisposable)
+
+        /* todo delegate consider moving this into the other classes.  Maybe publish observables in the holder,
+        subscribe in the delegate, and add a disposable to the holder that gets cleared when something binds to it?
+         */
+        listOf(
+                rowCheckBoxFrame.clicks().doOnNext { rowCheckBox.toggle() },
+                rowCheckBox.clicks()
+        ).merge()
+                .mapNodes()
+                .subscribe { (_, groupHolderNode) ->
+                    ((groupHolderNode as? CheckableModelNode<*>)?.checkBoxState as? CheckBoxState.Visible)?.listener?.invoke()
+                }
+                .addTo(compositeDisposable)
+    }
 }
--- app/src/main/java/com/krystianwsul/checkme/gui/tree/BaseAdapter.kt
+++ app/src/main/java/com/krystianwsul/checkme/gui/tree/BaseAdapter.kt
@@ -1,15 +1,8 @@
 package com.krystianwsul.checkme.gui.tree
 
 import android.view.ViewGroup
-import com.jakewharton.rxbinding3.view.clicks
-import com.jakewharton.rxbinding3.view.longClicks
-import com.krystianwsul.checkme.gui.tree.checkable.CheckBoxState
-import com.krystianwsul.checkme.gui.tree.checkable.CheckableModelNode
 import com.krystianwsul.treeadapter.TreeModelAdapter
 import com.krystianwsul.treeadapter.TreeNodeCollection
-import io.reactivex.Observable
-import io.reactivex.rxkotlin.addTo
-import io.reactivex.rxkotlin.merge
 
 abstract class BaseAdapter : TreeModelAdapter<AbstractHolder> {
 
@@ -21,41 +14,7 @@ abstract class BaseAdapter : TreeModelAdapter<AbstractHolder> {
     final override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
             NodeType.values()[viewType].onCreateViewHolder(this, parent)
 
-    final override fun onViewAttachedToWindow(holder: AbstractHolder) {
-        holder.onViewAttachedToWindow()
-
-        holder.apply {
-            fun Observable<*>.mapNodes() = map { holderPosition }.filter { it >= 0 }.map(::getNodes)
-
-            itemView.clicks()
-                    .mapNodes()
-                    .subscribe { (treeNode, _) -> treeNode.onClick(this) }
-                    .addTo(compositeDisposable)
-
-            itemView.longClicks { true }
-                    .mapNodes()
-                    .subscribe { (_, groupHolderNode) -> groupHolderNode.onLongClick(holder) }
-                    .addTo(compositeDisposable)
-
-            rowExpand.clicks()
-                    .mapNodes()
-                    .subscribe { (treeNode, _) -> treeNode.onExpandClick() }
-                    .addTo(compositeDisposable)
-
-            /* todo delegate consider moving this into the other classes.  Maybe publish observables in the holder,
-            subscribe in the delegate, and add a disposable to the holder that gets cleared when something binds to it?
-             */
-            listOf(
-                    rowCheckBoxFrame.clicks().doOnNext { rowCheckBox.toggle() },
-                    rowCheckBox.clicks()
-            ).merge()
-                    .mapNodes()
-                    .subscribe { (_, groupHolderNode) ->
-                        ((groupHolderNode as? CheckableModelNode<*>)?.checkBoxState as? CheckBoxState.Visible)?.listener?.invoke()
-                    }
-                    .addTo(compositeDisposable)
-        }
-    }
+    final override fun onViewAttachedToWindow(holder: AbstractHolder) = holder.onViewAttachedToWindow()
 
     final override fun onViewDetachedFromWindow(holder: AbstractHolder) = holder.onViewDetachedFromWindow()
 }

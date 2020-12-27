e41dff7ac2f10ef192cc286fe0f00ec1aed147ed
krystianwsul <krystianwsul@gmail.com>
2020-12-27 12:31:10 +0100

    +DomainFactoryRule

--- app/src/test/java/com/krystianwsul/checkme/domainmodel/DomainFactoryTest.kt
+++ app/src/test/java/com/krystianwsul/checkme/domainmodel/DomainFactoryTest.kt
@@ -1,158 +1,26 @@
 package com.krystianwsul.checkme.domainmodel
 
-import com.krystianwsul.checkme.MyApplication
-import com.krystianwsul.checkme.MyCrashlytics
 import com.krystianwsul.checkme.Preferences
 import com.krystianwsul.checkme.domainmodel.extensions.*
-import com.krystianwsul.checkme.domainmodel.notifications.NotificationWrapper
-import com.krystianwsul.checkme.firebase.factories.FriendsFactory
-import com.krystianwsul.checkme.firebase.factories.MyUserFactory
-import com.krystianwsul.checkme.firebase.factories.ProjectsFactory
-import com.krystianwsul.checkme.firebase.loaders.ProjectLoader
-import com.krystianwsul.checkme.firebase.loaders.SharedProjectsLoader
 import com.krystianwsul.checkme.persistencemodel.SaveService
-import com.krystianwsul.common.domain.DeviceDbInfo
-import com.krystianwsul.common.domain.DeviceInfo
-import com.krystianwsul.common.domain.UserInfo
-import com.krystianwsul.common.firebase.DatabaseWrapper
-import com.krystianwsul.common.firebase.json.PrivateProjectJson
-import com.krystianwsul.common.firebase.records.PrivateProjectRecord
 import com.krystianwsul.common.time.Date
 import com.krystianwsul.common.time.ExactTimeStamp
 import com.krystianwsul.common.time.HourMinute
 import com.krystianwsul.common.time.TimePair
 import com.krystianwsul.common.utils.ScheduleData
-import com.krystianwsul.common.utils.UserKey
 import com.soywiz.klock.hours
-import io.mockk.*
-import io.reactivex.disposables.CompositeDisposable
-import org.junit.*
 import org.junit.Assert.assertEquals
+import org.junit.Rule
+import org.junit.Test
 
 class DomainFactoryTest {
 
     // remember to add overloads to pass in "now" for testing complex scenarios
 
-    companion object {
+    @get:Rule
+    val domainFactoryRule = DomainFactoryRule()
 
-        private val deviceDbInfo = DeviceDbInfo(
-                DeviceInfo(
-                        spyk(UserInfo("email", "name", "uid")) {
-                            every { key } returns UserKey("key")
-                        },
-                        "token",
-                ),
-                "uuid",
-        )
-
-        @BeforeClass
-        @JvmStatic
-        fun beforeClass() {
-            MyApplication._sharedPreferences = mockk(relaxed = true)
-            MyApplication._context = mockk(relaxed = true)
-
-            mockkObject(Preferences)
-            every { Preferences.tickLog } returns mockk(relaxed = true)
-            every { Preferences.shortcuts = any() } returns Unit
-
-            mockkObject(MyCrashlytics)
-            every { MyCrashlytics.log(any()) } returns Unit
-
-            mockkObject(NotificationWrapper)
-            every { NotificationWrapper.instance } returns mockk(relaxed = true)
-
-            mockkObject(BackendNotifier)
-            every { BackendNotifier.notify(any(), any(), any()) } returns Unit
-        }
-
-        @AfterClass
-        @JvmStatic
-        fun afterClass() {
-            MyApplication._sharedPreferences = null
-            MyApplication._context = null
-
-            unmockkObject(Preferences)
-
-            unmockkObject(MyCrashlytics)
-
-            unmockkObject(NotificationWrapper)
-
-            unmockkObject(BackendNotifier)
-        }
-    }
-
-    private val domainFactoryStartTime = ExactTimeStamp.Local(
-            Date(2020, 12, 20),
-            HourMinute(19, 0),
-    )
-
-    private val compositeDisposable = CompositeDisposable()
-    private lateinit var domainFactory: DomainFactory
-
-    @Before
-    fun before() {
-        val myUserFactory = mockk<MyUserFactory> {
-            every { isSaved } returns false
-            every { save(any()) } returns Unit
-            every { user } returns mockk(relaxed = true)
-        }
-
-        val databaseWrapper = mockk<DatabaseWrapper> {
-            var taskId = 0
-            every { getPrivateTaskRecordId(any()) } answers { "taskId" + ++taskId }
-
-            var scheduleId = 0
-            every { getPrivateScheduleRecordId(any(), any()) } answers { "scheduleId" + ++scheduleId }
-
-            var noScheduleOrParentId = 0
-            every { newPrivateNoScheduleOrParentRecordId(any(), any()) } answers {
-                "noScheduleOrParentId" + ++noScheduleOrParentId
-            }
-
-            var taskHierarchyId = 0
-            every { getPrivateTaskHierarchyRecordId(any()) } answers { "taskHierarchyId" + ++taskHierarchyId }
-        }
-
-        val projectsFactory = ProjectsFactory(
-                mockk(),
-                mockk(relaxed = true),
-                ProjectLoader.InitialProjectEvent(
-                        mockk(relaxed = true),
-                        PrivateProjectRecord(
-                                databaseWrapper,
-                                deviceDbInfo.userInfo,
-                                PrivateProjectJson(startTime = domainFactoryStartTime.long, startTimeOffset = domainFactoryStartTime.offset)
-                        ),
-                        mapOf()
-                ),
-                mockk(relaxed = true),
-                SharedProjectsLoader.InitialProjectsEvent(listOf()),
-                domainFactoryStartTime,
-                mockk(relaxed = true),
-                compositeDisposable,
-        ) { deviceDbInfo }
-
-        val friendsFactory = mockk<FriendsFactory> {
-            every { isSaved } returns false
-            every { save(any()) } returns Unit
-        }
-
-        domainFactory = DomainFactory(
-                mockk(relaxed = true),
-                myUserFactory,
-                projectsFactory,
-                friendsFactory,
-                deviceDbInfo,
-                domainFactoryStartTime,
-                domainFactoryStartTime,
-                compositeDisposable,
-        )
-    }
-
-    @After
-    fun after() {
-        compositeDisposable.clear()
-    }
+    private val domainFactory get() = domainFactoryRule.domainFactory
 
     @Test
     fun testCreatingTask() {

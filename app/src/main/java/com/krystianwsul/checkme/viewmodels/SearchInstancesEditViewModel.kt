6083c7190dd698f970d88a30a88451dab284b2a4
krystianwsul <krystianwsul@gmail.com>
2020-12-29 20:08:06 +0100

    +SearchInstancesEditViewModel

---
+++ app/src/main/java/com/krystianwsul/checkme/viewmodels/SearchInstancesEditViewModel.kt
@@ -0,0 +1,58 @@
+package com.krystianwsul.checkme.viewmodels
+
+import android.os.Parcelable
+import com.krystianwsul.checkme.domainmodel.DomainFactory
+import com.krystianwsul.checkme.domainmodel.extensions.getSearchInstancesEditData
+import com.krystianwsul.checkme.gui.edit.dialogs.ParentPickerFragment
+import com.krystianwsul.checkme.gui.instances.list.GroupListDataWrapper
+import com.krystianwsul.common.criteria.SearchCriteria
+import com.krystianwsul.common.utils.normalized
+
+class SearchInstancesEditViewModel : DomainViewModel<SearchInstancesEditViewModel.Data>() {
+
+    private var parameters = Parameters()
+
+    override val domainListener = object : DomainListener<Data>() {
+
+        override fun getDataResult(domainFactory: DomainFactory) = domainFactory.getSearchInstancesEditData(
+                parameters.searchCriteria,
+                parameters.page
+        )
+    }
+
+    fun start(searchCriteria: SearchCriteria, page: Int) {
+        val newParameters = Parameters(searchCriteria, page)
+
+        if (parameters != newParameters) {
+            parameters = newParameters
+
+            refresh()
+        } else {
+            internalStart()
+        }
+    }
+
+    data class Data(
+            val groupListDataWrapper: GroupListDataWrapper,
+            val showLoader: Boolean,
+            val searchCriteria: SearchCriteria,
+    ) : DomainData()
+
+    data class InstanceEntryData(
+            override val name: String,
+            override val childEntryDatas: Collection<InstanceEntryData>,
+            override val entryKey: Parcelable,
+            override val details: String?,
+            override val note: String?,
+            override val sortKey: EditViewModel.SortKey,
+    ) : ParentPickerFragment.EntryData {
+
+        override val normalizedFields by lazy { listOfNotNull(name, note).map { it.normalized() } }
+
+        override fun normalize() {
+            normalizedFields
+        }
+    }
+
+    private data class Parameters(val searchCriteria: SearchCriteria = SearchCriteria.empty, val page: Int = 0)
+}

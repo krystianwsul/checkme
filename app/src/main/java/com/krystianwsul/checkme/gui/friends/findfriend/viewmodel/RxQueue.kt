7ef79af7962e8cbcfaf40e75c0e1679a74d23c53
krystianwsul <krystianwsul@gmail.com>
2021-01-12 18:57:55 +0100

    +RxQueueTest

--- app/src/main/java/com/krystianwsul/checkme/gui/friends/findfriend/viewmodel/RxQueue.kt
+++ app/src/main/java/com/krystianwsul/checkme/gui/friends/findfriend/viewmodel/RxQueue.kt
@@ -1,19 +1,41 @@
 package com.krystianwsul.checkme.gui.friends.findfriend.viewmodel
 
+import com.jakewharton.rxrelay3.PublishRelay
 import com.jakewharton.rxrelay3.Relay
-import com.krystianwsul.checkme.utils.toV3
-import com.victorrendina.rxqueue2.QueueRelay
 import io.reactivex.rxjava3.core.Observer
+import java.util.concurrent.ConcurrentLinkedQueue
 
 class RxQueue<T : Any> : Relay<T>() {
 
-    private val queueRelay = QueueRelay.create<T>()
+    private val queue = ConcurrentLinkedQueue<T>()
 
-    override fun accept(value: T) = queueRelay.accept(value)
+    private val relay = PublishRelay.create<T>()
 
-    override fun hasObservers() = queueRelay.hasObservers()
+    override fun accept(value: T) {
+        if (relay.hasObservers()) {
+            when (queue.size) {
+                0 -> queue.add(value)
+                1 -> {
+                    queue.clear()
+                    queue.add(value)
+                }
+                else -> throw IllegalStateException()
+            }
+
+            relay.accept(value)
+        } else {
+            queue.add(value)
+        }
+    }
+
+    override fun hasObservers() = relay.hasObservers()
 
     override fun subscribeActual(observer: Observer<in T>) {
-        queueRelay.toV3().subscribe(observer)
+        if (relay.hasObservers()) throw IllegalStateException()
+
+        relay.subscribe(observer)
+
+        repeat((0 until queue.size - 1).count()) { relay.accept(queue.poll()) }
+        if (queue.isNotEmpty()) relay.accept(queue.peek())
     }
 }
--- app/src/test/java/com/krystianwsul/checkme/gui/friends/findfriend/viewmodel/RxQueueTest.kt
+++ app/src/test/java/com/krystianwsul/checkme/gui/friends/findfriend/viewmodel/RxQueueTest.kt
@@ -7,6 +7,28 @@ import org.junit.Test
 class RxQueueTest {
 
     @Test
+    fun testRegularEvents() {
+        val rxQueue = RxQueue<Int>()
+
+        val testObserver = TestObserver<Int>()
+        rxQueue.subscribe(testObserver)
+
+        rxQueue.accept(1)
+        rxQueue.accept(2)
+        rxQueue.accept(3)
+
+        assertEquals(listOf(1, 2, 3), testObserver.values())
+    }
+
+    @Test(expected = NullPointerException::class)
+    fun testMultipleObservers() {
+        val rxQueue = RxQueue<Int>()
+
+        rxQueue.subscribe(TestObserver())
+        rxQueue.subscribe(TestObserver())
+    }
+
+    @Test
     fun testQueuedUpEvents() {
         val rxQueue = RxQueue<Int>()
 
@@ -30,14 +52,24 @@ class RxQueueTest {
 
         val testObserver1 = TestObserver<Int>()
         rxQueue.subscribe(testObserver1)
-
         assertEquals(listOf(1, 2, 3), testObserver1.values())
-
         testObserver1.dispose()
 
         val testObserver2 = TestObserver<Int>()
         rxQueue.subscribe(testObserver2)
-
         assertEquals(listOf(3), testObserver2.values())
+        testObserver2.dispose()
+
+        val testObserver3 = TestObserver<Int>()
+        rxQueue.subscribe(testObserver3)
+        assertEquals(listOf(3), testObserver3.values())
+        testObserver3.dispose()
+
+        rxQueue.accept(4)
+        rxQueue.accept(5)
+
+        val testObserver4 = TestObserver<Int>()
+        rxQueue.subscribe(testObserver4)
+        assertEquals(listOf(3, 4, 5), testObserver4.values())
     }
 }

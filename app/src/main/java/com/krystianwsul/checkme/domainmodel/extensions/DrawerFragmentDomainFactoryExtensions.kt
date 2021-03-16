d351576e2c4dd4f82d2110d375acf83b479cdc2e
krystianwsul <krystianwsul@gmail.com>
2021-03-16 19:18:10 +0100

    DrawerFragmentDomainFactoryExtensions: rx

--- app/src/main/java/com/krystianwsul/checkme/domainmodel/extensions/DrawerFragmentDomainFactoryExtensions.kt
+++ app/src/main/java/com/krystianwsul/checkme/domainmodel/extensions/DrawerFragmentDomainFactoryExtensions.kt
@@ -2,11 +2,14 @@ package com.krystianwsul.checkme.domainmodel.extensions
 
 import com.krystianwsul.checkme.MyCrashlytics
 import com.krystianwsul.checkme.domainmodel.DomainFactory
-import com.krystianwsul.checkme.domainmodel.DomainFactory.Companion.syncOnDomain
 import com.krystianwsul.checkme.viewmodels.DrawerViewModel
+import com.krystianwsul.common.firebase.SchedulerType
+import com.krystianwsul.common.firebase.SchedulerTypeHolder
 
-fun DomainFactory.getDrawerData(): DrawerViewModel.Data = syncOnDomain {
+fun DomainFactory.getDrawerData(): DrawerViewModel.Data {
     MyCrashlytics.log("DomainFactory.getDrawerData")
 
-    myUserFactory.user.run { DrawerViewModel.Data(name, email, photoUrl) }
+    SchedulerTypeHolder.instance.requireScheduler(SchedulerType.DOMAIN)
+
+    return myUserFactory.user.run { DrawerViewModel.Data(name, email, photoUrl) }
 }

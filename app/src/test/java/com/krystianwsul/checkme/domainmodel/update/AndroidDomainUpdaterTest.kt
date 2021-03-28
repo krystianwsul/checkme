0d6bbbf7889d3c64a201bd2a38365e2a9f613e14
krystianwsul <krystianwsul@gmail.com>
2021-03-28 12:54:43 +0200

    +AndroidDomainUpdaterTest.testSingleItemNotReady

--- app/src/test/java/com/krystianwsul/checkme/domainmodel/update/AndroidDomainUpdaterTest.kt
+++ app/src/test/java/com/krystianwsul/checkme/domainmodel/update/AndroidDomainUpdaterTest.kt
@@ -2,14 +2,20 @@ package com.krystianwsul.checkme.domainmodel.update
 
 import com.jakewharton.rxrelay3.PublishRelay
 import com.krystianwsul.checkme.domainmodel.DomainFactory
+import com.krystianwsul.checkme.domainmodel.DomainFactoryRule
 import com.krystianwsul.checkme.viewmodels.NullableWrapper
 import io.mockk.mockk
+import org.junit.Assert.assertEquals
+import org.junit.Rule
 import org.junit.Test
 
 class AndroidDomainUpdaterTest {
 
+    @get:Rule
+    val domainFactoryRule = DomainFactoryRule()
+
     @Test
-    fun testQueue() {
+    fun testInitQueue() {
         val isReady = PublishRelay.create<NullableWrapper<DomainFactory>>()
 
         val queue = AndroidDomainUpdater.Queue(isReady)
@@ -17,7 +23,30 @@ class AndroidDomainUpdaterTest {
 
         val results = mutableListOf<Int>()
 
-        isReady.accept(NullableWrapper(mockk()))
+        isReady.accept(NullableWrapper())
+
+        disposable.dispose()
+    }
+
+    @Test
+    fun testSingleItemNotReady() {
+        val isReady = PublishRelay.create<NullableWrapper<DomainFactory>>()
+
+        val queue = AndroidDomainUpdater.Queue(isReady)
+        val disposable = queue.subscribe()
+
+        val results = mutableListOf<Int>()
+
+        isReady.accept(NullableWrapper())
+
+        val testObserver = queue.add { domainFactory, local ->
+            results += 0
+
+            DomainUpdater.Result("a", mockk<DomainUpdater.Params>())
+        }.test()
+
+        assertEquals(listOf<Int>(), results)
+        testObserver.assertEmpty()
 
         disposable.dispose()
     }

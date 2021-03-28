913123b14c1d936d477842b482a8e399c8974991
krystianwsul <krystianwsul@gmail.com>
2021-03-28 12:58:25 +0200

    AndroidDomainUpdaterTest: move stuff into before/after

--- app/src/test/java/com/krystianwsul/checkme/domainmodel/update/AndroidDomainUpdaterTest.kt
+++ app/src/test/java/com/krystianwsul/checkme/domainmodel/update/AndroidDomainUpdaterTest.kt
@@ -5,7 +5,10 @@ import com.krystianwsul.checkme.domainmodel.DomainFactory
 import com.krystianwsul.checkme.domainmodel.DomainFactoryRule
 import com.krystianwsul.checkme.viewmodels.NullableWrapper
 import io.mockk.mockk
+import io.reactivex.rxjava3.disposables.Disposable
+import org.junit.After
 import org.junit.Assert.assertEquals
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
@@ -14,40 +17,40 @@ class AndroidDomainUpdaterTest {
     @get:Rule
     val domainFactoryRule = DomainFactoryRule()
 
+    private lateinit var isReady: PublishRelay<NullableWrapper<DomainFactory>>
+    private lateinit var queue: AndroidDomainUpdater.Queue
+    private lateinit var queueDisposable: Disposable
+    private lateinit var results: MutableList<Int>
+
+    @Before
+    fun before() {
+        isReady = PublishRelay.create()
+        queue = AndroidDomainUpdater.Queue(isReady)
+        queueDisposable = queue.subscribe()
+        results = mutableListOf()
+    }
+
+    @After
+    fun after() {
+        queueDisposable.dispose()
+    }
+
     @Test
     fun testInitQueue() {
-        val isReady = PublishRelay.create<NullableWrapper<DomainFactory>>()
-
-        val queue = AndroidDomainUpdater.Queue(isReady)
-        val disposable = queue.subscribe()
-
-        val results = mutableListOf<Int>()
-
         isReady.accept(NullableWrapper())
-
-        disposable.dispose()
     }
 
     @Test
     fun testSingleItemNotReady() {
-        val isReady = PublishRelay.create<NullableWrapper<DomainFactory>>()
-
-        val queue = AndroidDomainUpdater.Queue(isReady)
-        val disposable = queue.subscribe()
-
-        val results = mutableListOf<Int>()
-
         isReady.accept(NullableWrapper())
 
-        val testObserver = queue.add { domainFactory, local ->
-            results += 0
+        val testObserver = queue.add { _, _ ->
+            results.add(0)
 
             DomainUpdater.Result("a", mockk<DomainUpdater.Params>())
         }.test()
 
         assertEquals(listOf<Int>(), results)
         testObserver.assertEmpty()
-
-        disposable.dispose()
     }
 }

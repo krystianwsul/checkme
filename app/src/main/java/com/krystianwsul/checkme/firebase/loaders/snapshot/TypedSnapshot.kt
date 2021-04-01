a32a5c77e5f078a1b408528221c764e23613e273
krystianwsul <krystianwsul@gmail.com>
2021-04-01 15:26:45 +0200

    +TypedSnapshot

---
+++ app/src/main/java/com/krystianwsul/checkme/firebase/loaders/snapshot/TypedSnapshot.kt
@@ -0,0 +1,15 @@
+package com.krystianwsul.checkme.firebase.loaders.snapshot
+
+import com.google.firebase.database.DataSnapshot
+
+interface TypedSnapshot<T : Any> : Snapshot {
+
+    class Impl<T : Any>(private val dataSnapshot: DataSnapshot) : TypedSnapshot<T> {
+
+        override val key get() = dataSnapshot.key!!
+
+        override fun exists() = dataSnapshot.exists()
+
+        override fun <T> getValue(valueType: Class<T>) = dataSnapshot.getValue(valueType)
+    }
+}

0c0a3716e719ec8b331f3c7624530c9966856063
krystianwsul <krystianwsul@gmail.com>
2021-04-02 14:46:01 +0200

    re-add broken imports

--- common/src/commonMain/kotlin/firebase/json/JsonWrapper.kt
+++ common/src/commonMain/kotlin/firebase/json/JsonWrapper.kt
@@ -1,9 +1,8 @@
 package com.krystianwsul.common.firebase.json
 
-import kotlinx.serialization.Serializable
 import kotlin.jvm.JvmOverloads
 
 @Serializable
-class JsonWrapper @JvmOverloads constructor(
-        var projectJson: SharedProjectJson = SharedProjectJson()
+data class JsonWrapper @JvmOverloads constructor(
+        var projectJson: SharedProjectJson = SharedProjectJson(),
 )

a026b48524f52c2dd439297471a8de9f48b69811
krystianwsul <krystianwsul@gmail.com>
2021-04-02 12:37:56 +0200

    Utils: use new scan for output

--- app/src/main/java/com/krystianwsul/checkme/firebase/Utils.kt
+++ app/src/main/java/com/krystianwsul/checkme/firebase/Utils.kt
@@ -1,10 +1,9 @@
 package com.krystianwsul.checkme.firebase
 
+import com.krystianwsul.checkme.utils.mapNotNull
 import io.reactivex.rxjava3.core.Maybe
 import io.reactivex.rxjava3.core.Observable
-import io.reactivex.rxjava3.core.Single
 import io.reactivex.rxjava3.kotlin.Observables
-import io.reactivex.rxjava3.kotlin.merge
 
 fun <T : Any, U : Any> mergePaperAndRx(
         paperMaybe: Maybe<T>,
@@ -19,37 +18,11 @@ fun <T : Any, U : Any> mergePaperAndRx(
             firebaseObservable.take(1)
                     .map<AndroidDatabaseWrapper.LoadState<U>> { AndroidDatabaseWrapper.LoadState.Loaded(it) }
                     .startWithItem(AndroidDatabaseWrapper.LoadState.Initial()),
-    ).share()
-
-    // three pairs. ignore pair 1, because that's just both initial
-    val secondPairSingle = permutationObservable.skip(1).firstOrError()
-    val thirdPairSingle = permutationObservable.skip(2).firstOrError()
-
-    val firstValueObservable: Observable<U> = secondPairSingle.flatMapObservable { (paper, firebase) ->
-        if (paper is AndroidDatabaseWrapper.LoadState.Loaded) {
-            check(firebase is AndroidDatabaseWrapper.LoadState.Initial)
-
-            listOf(
-                    Single.just(paperToSnapshot(paper.value)).toObservable(),
-                    thirdPairSingle.flatMapMaybe { (paper2, firebase2) ->
-                        check(paper2 is AndroidDatabaseWrapper.LoadState.Loaded)
-                        check(firebase2 is AndroidDatabaseWrapper.LoadState.Loaded)
-
-                        if (equal(paper2.value, firebase2.value)) {
-                            Maybe.empty()
-                        } else {
-                            Maybe.just(firebase2.value)
-                        }
-                    }.toObservable()
-            ).merge()
-        } else {
-            check(firebase is AndroidDatabaseWrapper.LoadState.Loaded)
-
-            Observable.just(firebase.value)
-        }
+    ).scan<PairState<T, U>>(PairState.Initial(paperToSnapshot)) { oldPairState, (newPaperState, newFirebaseState) ->
+        oldPairState.processNextPair(newPaperState, newFirebaseState)
     }
 
-    return listOf(firstValueObservable, firebaseObservable.skip(1)).merge()
+    return permutationObservable.mapNotNull { it.emission }.mergeWith(firebaseObservable.skip(1))
 }
 
 private sealed class PairState<T : Any, U : Any> {

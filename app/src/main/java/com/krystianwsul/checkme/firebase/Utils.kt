b2d12dbccb70a2c17ada16c37c0c5529306a4476
krystianwsul <krystianwsul@gmail.com>
2021-04-02 12:12:36 +0200

    +PairState

--- app/src/main/java/com/krystianwsul/checkme/firebase/Utils.kt
+++ app/src/main/java/com/krystianwsul/checkme/firebase/Utils.kt
@@ -19,13 +19,13 @@ inline fun <T : Any, U : Any> mergePaperAndRx(
             firebaseObservable.take(1)
                     .map<AndroidDatabaseWrapper.LoadState<U>> { AndroidDatabaseWrapper.LoadState.Loaded(it) }
                     .startWithItem(AndroidDatabaseWrapper.LoadState.Initial()),
-    )
+    ).share()
 
     // three pairs. ignore pair 1, because that's just both initial
     val secondPairSingle = permutationObservable.skip(1).firstOrError()
     val thirdPairSingle = permutationObservable.skip(2).firstOrError()
 
-    val firstValueSingle: Observable<U> = secondPairSingle.flatMapObservable { (paper, firebase) ->
+    val firstValueObservable: Observable<U> = secondPairSingle.flatMapObservable { (paper, firebase) ->
         if (paper is AndroidDatabaseWrapper.LoadState.Loaded) {
             check(firebase is AndroidDatabaseWrapper.LoadState.Initial)
 
@@ -49,5 +49,93 @@ inline fun <T : Any, U : Any> mergePaperAndRx(
         }
     }
 
-    return listOf(firstValueSingle, firebaseObservable.skip(1)).merge()
+    return listOf(firstValueObservable, firebaseObservable.skip(1)).merge()
+}
+
+private sealed class PairState<T : Any, U : Any> {
+
+    abstract fun processNextPair(
+            newPaperState: AndroidDatabaseWrapper.LoadState<T>,
+            newFirebaseState: AndroidDatabaseWrapper.LoadState<U>,
+    ): PairState<T, U>
+
+    // todo fun emit U
+
+    class Initial<T : Any, U : Any> : PairState<T, U>() {
+
+        override fun processNextPair(
+                newPaperState: AndroidDatabaseWrapper.LoadState<T>,
+                newFirebaseState: AndroidDatabaseWrapper.LoadState<U>,
+        ): PairState<T, U> {
+            check(newPaperState is AndroidDatabaseWrapper.LoadState.Initial)
+            check(newFirebaseState is AndroidDatabaseWrapper.LoadState.Initial)
+
+            return SkippingFirst()
+        }
+    }
+
+    class SkippingFirst<T : Any, U : Any> : PairState<T, U>() {
+
+        override fun processNextPair(
+                newPaperState: AndroidDatabaseWrapper.LoadState<T>,
+                newFirebaseState: AndroidDatabaseWrapper.LoadState<U>,
+        ): PairState<T, U> {
+            return if (newPaperState is AndroidDatabaseWrapper.LoadState.Initial) {
+                check(newFirebaseState is AndroidDatabaseWrapper.LoadState.Loaded)
+
+                FirebaseCameFirst(newPaperState, newFirebaseState.value) // todo paper log warning if paper comes after this
+            } else {
+                check(newPaperState is AndroidDatabaseWrapper.LoadState.Loaded)
+                check(newFirebaseState is AndroidDatabaseWrapper.LoadState.Initial)
+
+                PaperCameFirst(newPaperState.value, newFirebaseState)
+            }
+        }
+    }
+
+    class FirebaseCameFirst<T : Any, U : Any>(
+            paperState: AndroidDatabaseWrapper.LoadState.Initial<T>,
+            private val firebase: U,
+    ) : PairState<T, U>() {
+
+        override fun processNextPair(
+                newPaperState: AndroidDatabaseWrapper.LoadState<T>,
+                newFirebaseState: AndroidDatabaseWrapper.LoadState<U>,
+        ): PairState<T, U> {
+            check(newPaperState is AndroidDatabaseWrapper.LoadState.Loaded)
+            check(newFirebaseState is AndroidDatabaseWrapper.LoadState.Loaded)
+
+            check(newFirebaseState.value == firebase)
+
+            // todo paper log warning
+
+            return Terminal()
+        }
+    }
+
+    class PaperCameFirst<T : Any, U : Any>(
+            private val paper: T,
+            private val firebaseState: AndroidDatabaseWrapper.LoadState<U>,
+    ) : PairState<T, U>() {
+
+        override fun processNextPair(
+                newPaperState: AndroidDatabaseWrapper.LoadState<T>,
+                newFirebaseState: AndroidDatabaseWrapper.LoadState<U>,
+        ): PairState<T, U> {
+            check(newPaperState is AndroidDatabaseWrapper.LoadState.Loaded)
+            check(newFirebaseState is AndroidDatabaseWrapper.LoadState.Loaded)
+
+            // todo emit firebase if different from paper
+
+            return Terminal()
+        }
+    }
+
+    class Terminal<T : Any, U : Any>() : PairState<T, U>() {
+
+        override fun processNextPair(
+                newPaperState: AndroidDatabaseWrapper.LoadState<T>,
+                newFirebaseState: AndroidDatabaseWrapper.LoadState<U>,
+        ): PairState<T, U> = throw IllegalStateException()
+    }
 }

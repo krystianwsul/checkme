0a4b7355e2ab30b5993279d88d450dab9382093a
krystianwsul <krystianwsul@gmail.com>
2021-04-14 19:07:04 +0200

    ProjectLoader: remove instance rx

--- app/src/main/java/com/krystianwsul/checkme/firebase/loaders/ProjectLoader.kt
+++ app/src/main/java/com/krystianwsul/checkme/firebase/loaders/ProjectLoader.kt
@@ -1,6 +1,5 @@
 package com.krystianwsul.checkme.firebase.loaders
 
-import com.badoo.reaktive.rxjavainterop.asRxJava3Observable
 import com.krystianwsul.checkme.firebase.snapshot.Snapshot
 import com.krystianwsul.checkme.utils.cacheImmediate
 import com.krystianwsul.checkme.utils.mapNotNull
@@ -8,13 +7,10 @@ import com.krystianwsul.common.firebase.ChangeType
 import com.krystianwsul.common.firebase.ChangeWrapper
 import com.krystianwsul.common.firebase.json.Parsable
 import com.krystianwsul.common.firebase.records.ProjectRecord
-import com.krystianwsul.common.firebase.records.TaskRecord
 import com.krystianwsul.common.utils.ProjectType
-import com.krystianwsul.common.utils.TaskKey
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.core.Single
 import io.reactivex.rxjava3.disposables.CompositeDisposable
-import io.reactivex.rxjava3.kotlin.merge
 import io.reactivex.rxjava3.kotlin.plusAssign
 
 interface ProjectLoader<T : ProjectType, U : Parsable> { // U: Project JSON type
@@ -45,78 +41,27 @@ interface ProjectLoader<T : ProjectType, U : Parsable> { // U: Project JSON type
 
         private fun <T> Observable<T>.replayImmediate() = replay().apply { domainDisposable += connect() }!!
 
-        private val projectRecordObservable = snapshotObservable.mapNotNull { projectManager.set(it) }.let {
-            if (initialProjectRecord != null) {
-                it.startWithItem(ChangeWrapper(ChangeType.LOCAL, initialProjectRecord))
-            } else {
-                it
-            }
-        }
-                .replay()
-                .apply { domainDisposable += connect() } // todo instances just out of curiosity, is this ever disposed/terminated?
-
-        private data class ProjectData<T : ProjectType>(
-                val internallyUpdated: Boolean,
-                val changeWrapper: ChangeWrapper<out ProjectRecord<T>>,
-                val taskMap: Map<TaskKey, TaskRecord<T>>,
-        )
-
-        private data class InstanceData<T : ProjectType>(
-                val internallyUpdated: Boolean,
-                val taskRecord: TaskRecord<T>,
-        )
-
-        private val rootInstanceDatabaseRx: Observable<MapChanges<ProjectData<T>, TaskKey, InstanceData<T>>> =
-                projectRecordObservable.switchMap { changeWrapper ->
-                    val taskObservable = changeWrapper.data
-                            .taskRecordsRelay
-                            .asRxJava3Observable()
-                            .map {
-                                it.mapKeys { it.value.taskKey }
-                            }
-                            .share()
-
-                    listOf(
-                            taskObservable.take(1).map { ProjectData(false, changeWrapper, it) },
-                            taskObservable.skip(1).map { ProjectData(true, changeWrapper, it) }
-                    ).merge()
+        private val projectRecordObservable = snapshotObservable.mapNotNull { projectManager.set(it) }
+                .let {
+                    if (initialProjectRecord != null) {
+                        it.startWithItem(ChangeWrapper(ChangeType.LOCAL, initialProjectRecord))
+                    } else {
+                        it
+                    }
                 }
-                        .processChanges(
-                                { it.taskMap.keys },
-                                { (internallyUpdated, _, newData), taskKey ->
-                                    val taskRecord = newData.getValue(taskKey)
-
-                                    InstanceData(
-                                            internallyUpdated,
-                                            taskRecord,
-                                    )
-                                },
-                        )
-                        .replayImmediate()
+                .replayImmediate() // todo instances just out of curiosity, is this ever disposed/terminated?
 
         // first snapshot of everything
         override val initialProjectEvent = projectRecordObservable.firstOrError()
-                .map {
-                    val (changeType, projectRecord) = it
-
-                    ChangeWrapper(
-                            changeType,
-                            InitialProjectEvent(
-                                    projectManager,
-                                    projectRecord,
-                            ),
-                    )
-                }
+                .map { it.newData(InitialProjectEvent(projectManager, it.data)) }
                 .cacheImmediate(domainDisposable)
 
         // Here we observe remaining changes to the project or tasks, which don't affect the instance observables
-        override val changeProjectEvents = rootInstanceDatabaseRx.skip(1)
-                .filter { !it.original.internallyUpdated }
+        override val changeProjectEvents = projectRecordObservable.skip(1)
                 .map {
-                    val (changeType, projectRecord) = it.original.changeWrapper
-                    check(changeType == ChangeType.REMOTE)
+                    check(it.changeType == ChangeType.REMOTE)
 
-                    ChangeWrapper(changeType, ChangeProjectEvent(projectRecord))
+                    it.newData(ChangeProjectEvent(it.data))
                 }
                 .replayImmediate()
     }

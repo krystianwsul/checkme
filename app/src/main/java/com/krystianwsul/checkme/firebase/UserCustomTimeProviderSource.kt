b86be6124454056852b9714e7f561d1f8cf02233
krystianwsul <krystianwsul@gmail.com>
2021-04-14 20:07:11 +0200

    +UserCustomTimeProviderSource

---
+++ app/src/main/java/com/krystianwsul/checkme/firebase/UserCustomTimeProviderSource.kt
@@ -0,0 +1,24 @@
+package com.krystianwsul.checkme.firebase
+
+import com.krystianwsul.common.firebase.records.ProjectRecord
+import com.krystianwsul.common.time.JsonTime
+import com.krystianwsul.common.time.Time
+import com.krystianwsul.common.utils.CustomTimeKey
+import io.reactivex.rxjava3.core.Observable
+
+class UserCustomTimeProviderSource {
+
+    // todo source emit only remote changes
+    fun observeUserCustomTimeProvider(projectRecord: ProjectRecord<*>): Observable<JsonTime.UserCustomTimeProvider> {
+        // todo source consider way to notify that a project is removed
+
+        return Observable.just(
+                object : JsonTime.UserCustomTimeProvider {
+
+                    override fun getUserCustomTime(userCustomTimeKey: CustomTimeKey.User): Time.Custom.User {
+                        TODO("todo source")
+                    }
+                }
+        )
+    }
+}

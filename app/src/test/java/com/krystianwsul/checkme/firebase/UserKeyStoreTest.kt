ed20f5a5e5ff48b25449ce029d99760d992fec59
krystianwsul <krystianwsul@gmail.com>
2021-04-15 18:19:55 +0200

    UserKeyStoreTest: add sanity check for simple user change

--- app/src/test/java/com/krystianwsul/checkme/firebase/UserKeyStoreTest.kt
+++ app/src/test/java/com/krystianwsul/checkme/firebase/UserKeyStoreTest.kt
@@ -1,10 +1,11 @@
 package com.krystianwsul.checkme.firebase
 
+import com.jakewharton.rxrelay3.PublishRelay
+import com.krystianwsul.common.firebase.ChangeType
 import com.krystianwsul.common.firebase.ChangeWrapper
 import com.krystianwsul.common.utils.UserKey
 import io.reactivex.rxjava3.disposables.CompositeDisposable
 import io.reactivex.rxjava3.observers.TestObserver
-import io.reactivex.rxjava3.subjects.PublishSubject
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
@@ -12,15 +13,15 @@ import org.junit.Test
 class UserKeyStoreTest {
 
     private lateinit var compositeDisposable: CompositeDisposable
-    private lateinit var myUserChangeWrapperSubject: PublishSubject<ChangeWrapper<Set<UserKey>>>
+    private lateinit var myUserChangeWrapperRelay: PublishRelay<ChangeWrapper<Set<UserKey>>>
     private lateinit var userKeyStore: UserKeyStore
     private lateinit var testObserver: TestObserver<ChangeWrapper<Map<UserKey, UserKeyStore.LoadUserData>>>
 
     @Before
     fun before() {
         compositeDisposable = CompositeDisposable()
-        myUserChangeWrapperSubject = PublishSubject.create()
-        userKeyStore = UserKeyStore(myUserChangeWrapperSubject, compositeDisposable)
+        myUserChangeWrapperRelay = PublishRelay.create()
+        userKeyStore = UserKeyStore(myUserChangeWrapperRelay, compositeDisposable)
         testObserver = userKeyStore.loadUserDataObservable.test()
     }
 
@@ -32,5 +33,20 @@ class UserKeyStoreTest {
     @Test
     fun testInitial() {
         testObserver.assertEmpty()
+    }
+
+    @Test
+    fun testMyUserChanges() {
+        myUserChangeWrapperRelay.accept(ChangeWrapper(ChangeType.REMOTE, setOf()))
+        testObserver.assertValue(ChangeWrapper(ChangeType.REMOTE, mapOf()))
+
+        myUserChangeWrapperRelay.accept(ChangeWrapper(ChangeType.LOCAL, setOf(UserKey("1"))))
+        testObserver.assertValueAt(
+                1,
+                ChangeWrapper(
+                        ChangeType.LOCAL,
+                        mapOf(UserKey("1") to UserKeyStore.LoadUserData.Friend(null))
+                ),
+        )
     }
 }

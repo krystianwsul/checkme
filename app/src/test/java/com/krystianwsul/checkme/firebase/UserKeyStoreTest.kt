66b89bb485ff41f2bac2e572403826ae689b89a7
krystianwsul <krystianwsul@gmail.com>
2021-04-15 18:50:51 +0200

    UserKeyStoreTest: add simple check for adding friend

--- app/src/test/java/com/krystianwsul/checkme/firebase/UserKeyStoreTest.kt
+++ app/src/test/java/com/krystianwsul/checkme/firebase/UserKeyStoreTest.kt
@@ -3,14 +3,35 @@ package com.krystianwsul.checkme.firebase
 import com.jakewharton.rxrelay3.PublishRelay
 import com.krystianwsul.common.firebase.ChangeType
 import com.krystianwsul.common.firebase.ChangeWrapper
+import com.krystianwsul.common.firebase.DomainThreadChecker
+import com.krystianwsul.common.firebase.json.UserWrapper
+import com.krystianwsul.common.firebase.records.RootUserRecord
 import com.krystianwsul.common.utils.UserKey
+import io.mockk.mockk
 import io.reactivex.rxjava3.disposables.CompositeDisposable
 import io.reactivex.rxjava3.observers.TestObserver
 import org.junit.After
 import org.junit.Before
+import org.junit.BeforeClass
 import org.junit.Test
 
 class UserKeyStoreTest {
+
+    companion object {
+
+        private val userKey1 = UserKey("1")
+        private val userKey2 = UserKey("2")
+        private val userKey3 = UserKey("3")
+        private val userKey4 = UserKey("4")
+        private val userKey5 = UserKey("5")
+        private val userKey6 = UserKey("6")
+
+        @JvmStatic
+        @BeforeClass
+        fun beforeClass() {
+            DomainThreadChecker.instance = mockk(relaxed = true)
+        }
+    }
 
     private lateinit var compositeDisposable: CompositeDisposable
     private lateinit var myUserChangeWrapperRelay: PublishRelay<ChangeWrapper<Set<UserKey>>>
@@ -40,13 +61,48 @@ class UserKeyStoreTest {
         myUserChangeWrapperRelay.accept(ChangeWrapper(ChangeType.REMOTE, setOf()))
         testObserver.assertValue(ChangeWrapper(ChangeType.REMOTE, mapOf()))
 
-        myUserChangeWrapperRelay.accept(ChangeWrapper(ChangeType.LOCAL, setOf(UserKey("1"))))
+        myUserChangeWrapperRelay.accept(ChangeWrapper(ChangeType.LOCAL, setOf(userKey1)))
         testObserver.assertValueAt(
                 1,
                 ChangeWrapper(
                         ChangeType.LOCAL,
-                        mapOf(UserKey("1") to UserKeyStore.LoadUserData.Friend(null))
+                        mapOf(userKey1 to UserKeyStore.LoadUserData.Friend(null))
                 ),
         )
+    }
+
+    private fun newUserRecord(userKey: UserKey) = RootUserRecord(mockk(), false, UserWrapper(), userKey)
+
+    @Test
+    fun testAddFriend() {
+        // start with some dummy data from myUser
+        myUserChangeWrapperRelay.accept(ChangeWrapper(ChangeType.REMOTE, setOf(userKey1)))
+        val currentMap = mutableMapOf(userKey1 to UserKeyStore.LoadUserData.Friend(null))
+        testObserver.assertValue(ChangeWrapper(ChangeType.REMOTE, currentMap))
+
+        // add a friend
+        val rootUserRecord2 = newUserRecord(userKey2)
+        userKeyStore.addFriend(rootUserRecord2)
+
+        currentMap[userKey2] =
+                UserKeyStore.LoadUserData.Friend(UserKeyStore.AddFriendData(userKey2.key, rootUserRecord2.userWrapper))
+
+        testObserver.assertValueAt(1, ChangeWrapper(ChangeType.LOCAL, currentMap))
+    }
+
+    @Test
+    fun testAddFriendOverwriteWithRemoteChange() {
+        testAddFriend()
+
+        myUserChangeWrapperRelay.accept(ChangeWrapper(ChangeType.REMOTE, setOf(userKey3)))
+        val currentMap = mutableMapOf(userKey3 to UserKeyStore.LoadUserData.Friend(null))
+        testObserver.assertValueAt(2, ChangeWrapper(ChangeType.REMOTE, currentMap))
+
+        val rootUserRecord4 = newUserRecord(userKey4)
+
+        userKeyStore.addFriend(rootUserRecord4)
+        currentMap[userKey4] =
+                UserKeyStore.LoadUserData.Friend(UserKeyStore.AddFriendData(userKey4.key, rootUserRecord4.userWrapper))
+        testObserver.assertValueAt(3, ChangeWrapper(ChangeType.LOCAL, currentMap))
     }
 }

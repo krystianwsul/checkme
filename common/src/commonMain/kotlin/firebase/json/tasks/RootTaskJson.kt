a47a747b71fa9672e4afcf9bc5e08de0f5ebcaa7
krystianwsul <krystianwsul@gmail.com>
2021-04-20 20:14:32 +0200

    +RootTaskJson

---
+++ common/src/commonMain/kotlin/firebase/json/tasks/RootTaskJson.kt
@@ -0,0 +1,24 @@
+package com.krystianwsul.common.firebase.json.tasks
+
+import com.krystianwsul.common.firebase.json.InstanceJson
+import com.krystianwsul.common.firebase.json.NoScheduleOrParentJson
+import com.krystianwsul.common.firebase.json.schedule.RootScheduleWrapper
+import com.krystianwsul.common.firebase.json.taskhierarchies.NestedTaskHierarchyJson
+import kotlinx.serialization.Serializable
+import kotlin.jvm.JvmOverloads
+
+@Serializable
+data class RootTaskJson @JvmOverloads constructor(
+        override var name: String = "",
+        override val startTime: Long = 0,
+        override var startTimeOffset: Double? = null,
+        override var endTime: Long? = null,
+        override var note: String? = null,
+        override var instances: MutableMap<String, InstanceJson> = mutableMapOf(),
+        override var schedules: MutableMap<String, RootScheduleWrapper> = mutableMapOf(),
+        override var image: TaskJson.Image? = null,
+        override var endData: TaskJson.EndData? = null,
+        override var noScheduleOrParent: Map<String, NoScheduleOrParentJson> = mutableMapOf(),
+        override var ordinal: Double? = null,
+        override var taskHierarchies: Map<String, NestedTaskHierarchyJson> = mapOf(),
+) : TaskJson

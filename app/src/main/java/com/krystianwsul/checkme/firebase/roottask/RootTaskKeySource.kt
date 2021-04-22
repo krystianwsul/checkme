28d314a484b52dfafbfbb732ccf641e487b3c240
krystianwsul <krystianwsul@gmail.com>
2021-04-22 19:54:23 +0200

    +RootTaskKeySource.rootTaskKeysObservable

--- app/src/main/java/com/krystianwsul/checkme/firebase/roottask/RootTaskKeySource.kt
+++ app/src/main/java/com/krystianwsul/checkme/firebase/roottask/RootTaskKeySource.kt
@@ -1,5 +1,6 @@
 package com.krystianwsul.checkme.firebase.roottask
 
+import com.jakewharton.rxrelay3.PublishRelay
 import com.krystianwsul.common.firebase.records.task.RootTaskRecord
 import com.krystianwsul.common.utils.ProjectKey
 import com.krystianwsul.common.utils.TaskKey
@@ -7,12 +8,40 @@ import io.reactivex.rxjava3.disposables.CompositeDisposable
 
 class RootTaskKeySource(private val domainDisposable: CompositeDisposable) {
 
+    private val projectEvents = PublishRelay.create<ProjectEvent>()
+
+    val rootTaskKeysObservable = projectEvents.scan(ProjectAggregate()) { aggregate, projectEvent ->
+        when (projectEvent) {
+            is ProjectEvent.ProjectAddedOrUpdated -> aggregate.copy(
+                    aggregate.projectMap
+                            .toMutableMap()
+                            .apply { put(projectEvent.projectKey, projectEvent.rootTaskKeys) }
+            )
+            is ProjectEvent.ProjectsRemoved -> {
+                val newMap = aggregate.projectMap.toMutableMap()
+
+                projectEvent.projectKeys.forEach {
+                    check(newMap.containsKey(it))
+
+                    newMap.remove(it)
+                }
+
+                aggregate.copy(newMap)
+            }
+        }
+    }
+            .skip(1)
+            .map { it.output }
+            .distinctUntilChanged()
+
     fun onProjectAddedOrUpdated(projectKey: ProjectKey<*>, rootTaskKeys: Set<TaskKey.Root>) {
         // this covers:
         // private project: remote initial load, remote changes to tasks
         // shared project: both remote and local initial load, remote changes to tasks
 
         // this contributes to final observable by adding keys, or updating keys for given project
+
+        projectEvents.accept(ProjectEvent.ProjectAddedOrUpdated(projectKey, rootTaskKeys))
     }
 
     fun onTaskAddedLocally(parentKey: ParentKey, taskKey: TaskKey, taskRecord: RootTaskRecord) {
@@ -26,18 +55,40 @@ class RootTaskKeySource(private val domainDisposable: CompositeDisposable) {
     }
 
     fun onProjectsRemoved(projectKeys: Set<ProjectKey<*>>) {
-        // todo task fetch
+        projectEvents.accept(ProjectEvent.ProjectsRemoved(projectKeys))
     }
 
     /**
      * todo task fetch add callbacks for recursive tasks, similar to projects.
      */
 
-    // todo task fetch custom times, similar to projects
+    /**
+     * todo task fetch custom times, similar to projects. We will need to load custom times in response to remote
+     * changes, including initial remote loads.  But not local edits, since those can only assign our own custom times.
+     * But local edits may affect bookkeeping, in that changing a time on a task may make a certain custom time no
+     * longer needed.
+     */
 
     sealed class ParentKey {
 
         data class Project(val projectKey: ProjectKey<*>) : ParentKey()
         data class Task(val taskKey: TaskKey.Root) : ParentKey()
+    }
+
+    private sealed class ProjectEvent {
+
+        data class ProjectAddedOrUpdated(
+                val projectKey: ProjectKey<*>,
+                val rootTaskKeys: Set<TaskKey.Root>,
+        ) : ProjectEvent()
+
+        data class ProjectsRemoved(val projectKeys: Set<ProjectKey<*>>) : ProjectEvent()
+    }
+
+    private data class ProjectAggregate(val projectMap: Map<ProjectKey<*>, Set<TaskKey.Root>> = mapOf()) {
+
+        val output = projectMap.values
+                .flatten()
+                .toSet()
     }
 }

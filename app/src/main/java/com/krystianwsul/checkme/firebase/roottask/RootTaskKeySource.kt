45c4bfad9d97bec422590023ba89f31e520951b9
krystianwsul <krystianwsul@gmail.com>
2021-04-22 19:29:07 +0200

    RootTaskKeySource.onChangeTaskParent

--- app/src/main/java/com/krystianwsul/checkme/firebase/roottask/RootTaskKeySource.kt
+++ app/src/main/java/com/krystianwsul/checkme/firebase/roottask/RootTaskKeySource.kt
@@ -1,5 +1,6 @@
 package com.krystianwsul.checkme.firebase.roottask
 
+import com.krystianwsul.common.firebase.records.task.RootTaskRecord
 import com.krystianwsul.common.utils.ProjectKey
 import com.krystianwsul.common.utils.TaskKey
 import io.reactivex.rxjava3.core.Completable
@@ -15,18 +16,27 @@ class RootTaskKeySource(private val domainDisposable: CompositeDisposable) {
         // this contributes to final observable by adding keys, or updating keys for given project
     }
 
+    fun onAddTask(parentKey: ParentKey, taskKey: TaskKey, taskRecord: RootTaskRecord) {
+        // todo task fetch not sure if this will require rootTaskRecord; depends on RootTaskManager impl
+
+        // this covers task being added locally.  Not sure if it will need to return anything
+    }
+
+    fun onChangeTaskParent(oldParentKey: ParentKey, newParentKey: ParentKey, taskKey: TaskKey.Root) {
+        // todo task fetch
+    }
+
     /** todo task fetch
      * List of events that affect which root tasks need to be wired up:
      *
      * (done onProjectAddedOrUpdated) Initial private project load: completable, watch firebase
-     *
      * (done onProjectAddedOrUpdated) Initial shared project load (each): completable, watch firebase
      *
      * Project edits:
-     * - Task added locally: immediate, watch firebase, feed initial task record through RX
+     * - (done onAddTask) Task added locally: immediate, watch firebase, feed initial task record through RX
      * - (done onProjectAddedOrUpdated) Task added remotely: completable: onProjectAddedOrUpdated
      * - Tasks removed remotely: stop watching firebase, remove from model
-     * - Task project edited: atomically change bookkeeping for which project it belongs to
+     * - (done onChangeTaskParent) Task project edited: atomically change bookkeeping for which project it belongs to
      *
      * Projects removed: immediate, stop watching firebase
      *
@@ -41,5 +51,11 @@ class RootTaskKeySource(private val domainDisposable: CompositeDisposable) {
 
     fun onProjectRecordUpdatedRemotely(projectKey: ProjectKey<*>, remoteTaskKeys: Set<TaskKey.Root>): Completable {
         TODO("todo task fetch")
+    }
+
+    sealed class ParentKey {
+
+        data class Project(val projectKey: ProjectKey<*>) : ParentKey()
+        data class Task(val taskKey: TaskKey.Root) : ParentKey()
     }
 }

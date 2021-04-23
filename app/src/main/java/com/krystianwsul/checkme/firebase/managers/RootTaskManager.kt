49dfd367b15512cbe4b28186dced4b603e3254e5
krystianwsul <krystianwsul@gmail.com>
2021-04-23 15:06:30 +0200

    RootTaskManager: add setter

--- app/src/main/java/com/krystianwsul/checkme/firebase/managers/RootTaskManager.kt
+++ app/src/main/java/com/krystianwsul/checkme/firebase/managers/RootTaskManager.kt
@@ -1,13 +1,34 @@
 package com.krystianwsul.checkme.firebase.managers
 
+import com.krystianwsul.checkme.firebase.snapshot.Snapshot
 import com.krystianwsul.common.firebase.DatabaseWrapper
+import com.krystianwsul.common.firebase.json.tasks.RootTaskJson
 import com.krystianwsul.common.firebase.managers.MapRecordManager
 import com.krystianwsul.common.firebase.records.task.RootTaskRecord
 import com.krystianwsul.common.utils.TaskKey
 
-class RootTaskManager : MapRecordManager<TaskKey.Root, RootTaskRecord>() {
+class RootTaskManager(private val databaseWrapper: DatabaseWrapper) : MapRecordManager<TaskKey.Root, RootTaskRecord>() {
+
+    companion object {
+
+        private fun Snapshot<RootTaskJson>.toKey() = TaskKey.Root(key)
+    }
 
     override val databasePrefix = DatabaseWrapper.TASKS_KEY
 
     override fun valueToRecord(value: RootTaskRecord) = value
+
+    fun set(snapshot: Snapshot<RootTaskJson>) = set(
+            snapshot.toKey(),
+            { it.createObject != snapshot.value },
+            {
+                snapshot.takeIf { it.exists }?.let {
+                    RootTaskRecord(
+                            it.key,
+                            it.value!!,
+                            databaseWrapper,
+                    )
+                }
+            },
+    )?.data
 }

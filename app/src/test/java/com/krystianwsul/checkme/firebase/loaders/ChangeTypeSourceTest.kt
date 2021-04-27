f0fd2824249b6dff53e1d44012d6c5a89c39251a
krystianwsul <krystianwsul@gmail.com>
2021-04-27 18:03:26 +0200

    +ChangeTypeSourceTest.testSingleProjectUpdateChildTask

--- app/src/test/java/com/krystianwsul/checkme/firebase/loaders/ChangeTypeSourceTest.kt
+++ app/src/test/java/com/krystianwsul/checkme/firebase/loaders/ChangeTypeSourceTest.kt
@@ -357,4 +357,44 @@ class ChangeTypeSourceTest {
             )
         }
     }
+
+    @Test
+    fun testSingleProjectUpdateChildTask() {
+        testInitial()
+        acceptPrivateProject(PrivateProjectJson(rootTaskIds = mutableMapOf(taskKey1.taskId to true)))
+
+        rootTasksLoaderProvider.accept(
+                taskKey1,
+                RootTaskJson(
+                        noScheduleOrParent = mapOf(
+                                "noScheduleOrParentId" to NoScheduleOrParentJson(projectId = privateProjectId),
+                        ),
+                        rootTaskIds = mutableMapOf(taskKey2.taskId to true)
+                ),
+        )
+
+        rootTasksLoaderProvider.accept(
+                taskKey2,
+                RootTaskJson(
+                        startTimeOffset = 0.0,
+                        taskHierarchies = mapOf(
+                                "taskHierarchyId" to NestedTaskHierarchyJson(parentTaskId = taskKey1.taskId)
+                        ),
+                ),
+        )
+        checkEmpty()
+
+        taskEmissionChecker.checkRemote {
+            rootTasksLoaderProvider.accept(
+                    taskKey2,
+                    RootTaskJson(
+                            name = "changedName",
+                            startTimeOffset = 0.0,
+                            taskHierarchies = mapOf(
+                                    "taskHierarchyId" to NestedTaskHierarchyJson(parentTaskId = taskKey1.taskId)
+                            ),
+                    ),
+            )
+        }
+    }
 }

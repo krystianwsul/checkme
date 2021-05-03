bdff71fa4004790e9d994f8279524f7d1bfa8dc4
krystianwsul <krystianwsul@gmail.com>
2021-05-03 13:40:15 +0200

    Task: update todo

--- common/src/commonMain/kotlin/firebase/models/task/Task.kt
+++ common/src/commonMain/kotlin/firebase/models/task/Task.kt
@@ -32,7 +32,7 @@ abstract class Task(
     companion object {
 
         // todo after flipping switch, make json properties val
-        const val WRITE_ROOT_TASKS = false // todo task after edit enable for tests
+        const val WRITE_ROOT_TASKS = false // todo task after instance parent check tests, enable other write flags
     }
 
     abstract val parent: Parent

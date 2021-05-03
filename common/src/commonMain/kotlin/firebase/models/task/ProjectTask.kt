e1b02d9be908263e8517adf5f9c6a13223841f78
krystianwsul <krystianwsul@gmail.com>
2021-05-03 20:22:22 +0200

    ProjectTask: cleanup

--- common/src/commonMain/kotlin/firebase/models/task/ProjectTask.kt
+++ common/src/commonMain/kotlin/firebase/models/task/ProjectTask.kt
@@ -73,6 +73,4 @@ class ProjectTask(override val project: Project<*>, private val taskRecord: Proj
         noScheduleOrParentIntervals.forEach { it.noScheduleOrParent.fixOffsets() }
         existingInstances.values.forEach { it.fixOffsets() }
     }
-
-    override fun addRootTaskIdToProject() {}
 }

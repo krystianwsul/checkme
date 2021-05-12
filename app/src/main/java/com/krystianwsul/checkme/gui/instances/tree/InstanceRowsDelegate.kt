6f5be2e500fe1a5c0d8164f0f65aee2a11371d70
krystianwsul <krystianwsul@gmail.com>
2021-05-12 20:34:38 +0200

    -InstanceNodeDelegate

--- app/src/main/java/com/krystianwsul/checkme/gui/instances/tree/InstanceRowsDelegate.kt
+++ app/src/main/java/com/krystianwsul/checkme/gui/instances/tree/InstanceRowsDelegate.kt
@@ -1,20 +1,43 @@
 package com.krystianwsul.checkme.gui.instances.tree
 
+import com.krystianwsul.checkme.R
 import com.krystianwsul.checkme.gui.instances.list.GroupListDataWrapper
 import com.krystianwsul.checkme.gui.tree.delegates.multiline.MultiLineModelNode
+import com.krystianwsul.checkme.gui.tree.delegates.multiline.MultiLineRow
 import com.krystianwsul.treeadapter.TreeNode
 
 class InstanceRowsDelegate(
-    instanceData: GroupListDataWrapper.InstanceData,
+    private val instanceData: GroupListDataWrapper.InstanceData,
     private val treeNode: TreeNode<*>,
     showDetails: Boolean = true,
 ) : MultiLineModelNode.RowsDelegate {
 
-    private val instanceNodeDelegate = InstanceNodeDelegate(instanceData)
+    private val secondaryColor = if (instanceData.taskCurrent) R.color.textSecondary else R.color.textDisabled
 
-    override val name = instanceNodeDelegate.name
+    override val name = MultiLineRow.Visible(
+        instanceData.name,
+        if (instanceData.taskCurrent) R.color.textPrimary else R.color.textDisabled,
+    )
 
-    override val details = if (showDetails) instanceNodeDelegate.details else null
+    override val details: MultiLineRow.Visible? = instanceData.takeIf { showDetails }
+        ?.displayText
+        .takeUnless { it.isNullOrEmpty() }
+        ?.let { MultiLineRow.Visible(it, secondaryColor) }
 
-    override val children get() = instanceNodeDelegate.getChildren(treeNode)
+    override val children: MultiLineRow?
+        get() {
+            if (!treeNode.isExpanded) return null
+
+            val text = treeNode.allChildren
+                .filter { it.modelNode is NotDoneGroupNode && it.canBeShown() }
+                .map { it.modelNode as NotDoneGroupNode }
+                .takeIf { it.isNotEmpty() }
+                ?.sorted()
+                ?.joinToString(", ") { it.singleInstanceData.name }
+                ?: instanceData.note.takeIf { !it.isNullOrEmpty() }
+
+            return text?.let {
+                MultiLineRow.Visible(it, if (instanceData.taskCurrent) R.color.textSecondary else R.color.textDisabled)
+            }
+        }
 }

01764221afba001b5dea5a7422711d0be7c62244
krystianwsul <krystianwsul@gmail.com>
2021-06-13 20:34:08 +0200

    RootTaskDependencyStateContainerTest: add more tests

--- app/src/test/java/com/krystianwsul/checkme/firebase/roottask/RootTaskDependencyStateContainerTest.kt
+++ app/src/test/java/com/krystianwsul/checkme/firebase/roottask/RootTaskDependencyStateContainerTest.kt
@@ -59,4 +59,137 @@ class RootTaskDependencyStateContainerTest {
         container.onLoaded(makeRecordMock(taskKey2))
         assertTrue(container.hasDependentTasks(taskKey1))
     }
+
+    @Test
+    fun testLoadTwoChildren() {
+        val taskKey1 = TaskKey.Root("taskKey1")
+        val taskKey2 = TaskKey.Root("taskKey2")
+        val taskKey3 = TaskKey.Root("taskKey3")
+
+        container.onLoaded(makeRecordMock(taskKey1, setOf(taskKey2, taskKey3)))
+        assertFalse(container.hasDependentTasks(taskKey1))
+
+        container.onLoaded(makeRecordMock(taskKey2))
+        assertFalse(container.hasDependentTasks(taskKey1))
+
+        container.onLoaded(makeRecordMock(taskKey3))
+        assertTrue(container.hasDependentTasks(taskKey1))
+    }
+
+    @Test
+    fun testLoadTwoChildrenUpdate() {
+        val taskKey1 = TaskKey.Root("taskKey1")
+        val taskKey2 = TaskKey.Root("taskKey2")
+        val taskKey3 = TaskKey.Root("taskKey3")
+
+        container.onLoaded(makeRecordMock(taskKey1, setOf(taskKey2, taskKey3)))
+        assertFalse(container.hasDependentTasks(taskKey1))
+
+        container.onLoaded(makeRecordMock(taskKey2))
+        assertFalse(container.hasDependentTasks(taskKey1))
+
+        container.onLoaded(makeRecordMock(taskKey1, setOf(taskKey2)))
+        assertTrue(container.hasDependentTasks(taskKey1))
+    }
+
+    @Test
+    fun testLoadTwoChildrenDouble() {
+        val taskKey1 = TaskKey.Root("taskKey1")
+
+        val taskKey2 = TaskKey.Root("taskKey2")
+        val taskKey3 = TaskKey.Root("taskKey3")
+
+        val taskKey4 = TaskKey.Root("taskKey4")
+
+        container.onLoaded(makeRecordMock(taskKey1, setOf(taskKey2, taskKey3)))
+        assertFalse(container.hasDependentTasks(taskKey1))
+
+        container.onLoaded(makeRecordMock(taskKey2))
+        assertFalse(container.hasDependentTasks(taskKey1))
+
+        container.onLoaded(makeRecordMock(taskKey3, setOf(taskKey4)))
+        assertFalse(container.hasDependentTasks(taskKey1))
+        assertFalse(container.hasDependentTasks(taskKey3))
+
+        container.onLoaded(makeRecordMock(taskKey4))
+        assertTrue(container.hasDependentTasks(taskKey1))
+        assertTrue(container.hasDependentTasks(taskKey3))
+    }
+
+    @Test
+    fun testLoadTwoChildrenRemoved() {
+        val taskKey1 = TaskKey.Root("taskKey1")
+
+        val taskKey2 = TaskKey.Root("taskKey2")
+        val taskKey3 = TaskKey.Root("taskKey3")
+
+        val taskKey4 = TaskKey.Root("taskKey4")
+
+        container.onLoaded(makeRecordMock(taskKey1, setOf(taskKey2, taskKey3)))
+        assertFalse(container.hasDependentTasks(taskKey1))
+
+        container.onLoaded(makeRecordMock(taskKey2))
+        assertFalse(container.hasDependentTasks(taskKey1))
+
+        container.onLoaded(makeRecordMock(taskKey3, setOf(taskKey4)))
+        assertFalse(container.hasDependentTasks(taskKey1))
+        assertFalse(container.hasDependentTasks(taskKey3))
+
+        container.onLoaded(makeRecordMock(taskKey4))
+        assertTrue(container.hasDependentTasks(taskKey1))
+        assertTrue(container.hasDependentTasks(taskKey3))
+
+        container.onRemoved(taskKey4)
+        assertFalse(container.hasDependentTasks(taskKey1))
+        assertFalse(container.hasDependentTasks(taskKey3))
+    }
+
+    @Test
+    fun testLoadTwoChildrenUpdateRemoved() {
+        val taskKey1 = TaskKey.Root("taskKey1")
+
+        val taskKey2 = TaskKey.Root("taskKey2")
+        val taskKey3 = TaskKey.Root("taskKey3")
+
+        val taskKey4 = TaskKey.Root("taskKey4")
+
+        container.onLoaded(makeRecordMock(taskKey1, setOf(taskKey2, taskKey3)))
+        assertFalse(container.hasDependentTasks(taskKey1))
+
+        container.onLoaded(makeRecordMock(taskKey2))
+        assertFalse(container.hasDependentTasks(taskKey1))
+
+        container.onLoaded(makeRecordMock(taskKey3, setOf(taskKey4)))
+        assertFalse(container.hasDependentTasks(taskKey1))
+        assertFalse(container.hasDependentTasks(taskKey3))
+
+        container.onLoaded(makeRecordMock(taskKey4))
+        assertTrue(container.hasDependentTasks(taskKey1))
+        assertTrue(container.hasDependentTasks(taskKey3))
+
+        container.onLoaded(makeRecordMock(taskKey1, setOf(taskKey2)))
+
+        container.onRemoved(taskKey4)
+        assertTrue(container.hasDependentTasks(taskKey1))
+        assertFalse(container.hasDependentTasks(taskKey3))
+    }
+
+    @Test
+    fun testCyclical() {
+        val taskKey1 = TaskKey.Root("taskKey1")
+        val taskKey2 = TaskKey.Root("taskKey2")
+        val taskKey3 = TaskKey.Root("taskKey3")
+
+        container.onLoaded(makeRecordMock(taskKey1, setOf(taskKey2)))
+        assertFalse(container.hasDependentTasks(taskKey1))
+
+        container.onLoaded(makeRecordMock(taskKey2, setOf(taskKey3)))
+        assertFalse(container.hasDependentTasks(taskKey1))
+        assertFalse(container.hasDependentTasks(taskKey2))
+
+        container.onLoaded(makeRecordMock(taskKey3, setOf(taskKey1)))
+        assertTrue(container.hasDependentTasks(taskKey1))
+        assertTrue(container.hasDependentTasks(taskKey2))
+        assertTrue(container.hasDependentTasks(taskKey3))
+    }
 }

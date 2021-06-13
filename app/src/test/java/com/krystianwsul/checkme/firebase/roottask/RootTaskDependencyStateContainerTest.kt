d370f18686b793789a026a4a016e32573223b028
krystianwsul <krystianwsul@gmail.com>
2021-06-13 20:11:53 +0200

    RootTaskDependencyStateContainerTest: add some basic tests (failing)

--- app/src/test/java/com/krystianwsul/checkme/firebase/roottask/RootTaskDependencyStateContainerTest.kt
+++ app/src/test/java/com/krystianwsul/checkme/firebase/roottask/RootTaskDependencyStateContainerTest.kt
@@ -10,7 +10,10 @@ import org.junit.Test
 
 class RootTaskDependencyStateContainerTest {
 
-    private fun makeRecordMock(taskKey: TaskKey.Root, childTaskKeys: Set<TaskKey.Root>) = mockk<RootTaskRecord> {
+    private fun makeRecordMock(
+        taskKey: TaskKey.Root,
+        childTaskKeys: Set<TaskKey.Root> = emptySet(),
+    ) = mockk<RootTaskRecord> {
         every { this@mockk.taskKey } returns taskKey
         every { getDependentTaskKeys() } returns childTaskKeys
     }
@@ -25,5 +28,35 @@ class RootTaskDependencyStateContainerTest {
     @Test
     fun testInitial() {
         assertTrue(container.stateHoldersByTaskKey.isEmpty())
+    }
+
+    @Test
+    fun testLoadNoChildren() {
+        val taskKey = TaskKey.Root("taskKey")
+
+        container.onLoaded(makeRecordMock(taskKey))
+
+        assertTrue(container.hasDependentTasks(taskKey))
+    }
+
+    @Test
+    fun testLoadOneChildAbsent() {
+        val taskKey1 = TaskKey.Root("taskKey1")
+        val taskKey2 = TaskKey.Root("taskKey2")
+
+        container.onLoaded(makeRecordMock(taskKey1, setOf(taskKey2)))
+        assertFalse(container.hasDependentTasks(taskKey1))
+    }
+
+    @Test
+    fun testLoadOneChildLoaded() {
+        val taskKey1 = TaskKey.Root("taskKey1")
+        val taskKey2 = TaskKey.Root("taskKey2")
+
+        container.onLoaded(makeRecordMock(taskKey1, setOf(taskKey2)))
+        assertFalse(container.hasDependentTasks(taskKey1))
+
+        container.onLoaded(makeRecordMock(taskKey2))
+        assertTrue(container.hasDependentTasks(taskKey1))
     }
 }

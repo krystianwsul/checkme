62a053aa7f0a36adb80f8577957a6b7eec1916e4
krystianwsul <krystianwsul@gmail.com>
2021-06-16 19:06:11 +0200

    DomainListener: user UserScope instead of DomainFactory for instanceRelay

--- app/src/main/java/com/krystianwsul/checkme/viewmodels/DomainListener.kt
+++ app/src/main/java/com/krystianwsul/checkme/viewmodels/DomainListener.kt
@@ -2,6 +2,7 @@ package com.krystianwsul.checkme.viewmodels
 
 import com.jakewharton.rxrelay3.BehaviorRelay
 import com.krystianwsul.checkme.domainmodel.DomainFactory
+import com.krystianwsul.checkme.domainmodel.UserScope
 import com.krystianwsul.checkme.domainmodel.observeOnDomain
 import com.krystianwsul.checkme.utils.filterNotNull
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
@@ -32,35 +33,37 @@ abstract class DomainListener<D : DomainData> {
 
         var listenerAdded = false
 
-        disposable = DomainFactory.instanceRelay
-                .observeOnDomain()
-                .filterNotNull()
-                .switchMap { domainFactory ->
-                    /**
-                     * What's expected here: the DomainListener may be initialized before the DomainFactory is
-                     * available.  And, it can stick around after the DomainFactory is destroyed, such as on logout.
-                     * But after logout, all DomainListeners should be destroyed, so we should never see more than
-                     * one event get this far.
-                     */
-                    check(!listenerAdded)
+        disposable = UserScope.instanceRelay
+            .observeOnDomain()
+            .filterNotNull()
+            .switchMap { userScope ->
+                /**
+                 * What's expected here: the DomainListener may be initialized before the DomainFactory is
+                 * available.  And, it can stick around after the DomainFactory is destroyed, such as on logout.
+                 * But after logout, all DomainListeners should be destroyed, so we should never see more than
+                 * one event get this far.
+                 */
+                check(!listenerAdded)
 
-                    listenerAdded = true
+                listenerAdded = true
 
-                    domainFactory.domainListenerManager
-                            .addListener(this)
-                            .map { domainFactory }
+                userScope.domainListenerManager
+                    .addListener(this)
+                    .map { userScope }
+            }
+            .switchMapSingle { it.domainFactorySingle }
+            .map { it as DomainFactory }
+            .toFlowable(BackpressureStrategy.LATEST)
+            .observeOnDomain()
+            .map { getDataResult(it).data!! }
+            .filter { data.value != it }
+            .doFinally {
+                if (listenerAdded) {
+                    UserScope.nullableInstance
+                        ?.domainListenerManager
+                        ?.removeListener(this)
                 }
-                .toFlowable(BackpressureStrategy.LATEST)
-                .observeOnDomain()
-                .map { getDataResult(it).data!! }
-                .filter { data.value != it }
-                .doFinally {
-                    if (listenerAdded) {
-                        DomainFactory.nullableInstance
-                                ?.domainListenerManager
-                                ?.removeListener(this)
-                    }
-                }
+            }
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe(data)
     }

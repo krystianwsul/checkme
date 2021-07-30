54d9f92ed10574f0e66a8ddc0abd2f5d68da3ded
krystianwsul <krystianwsul@gmail.com>
2021-07-30 17:38:08 +0200

    update todos

--- common/src/main/kotlin/utils/CurrentOffset.kt
+++ common/src/main/kotlin/utils/CurrentOffset.kt
@@ -4,23 +4,29 @@ import com.krystianwsul.common.time.ExactTimeStamp
 
 interface CurrentOffset {
 
-    val startExactTimeStampOffset: ExactTimeStamp // todo now
-    val endExactTimeStampOffset: ExactTimeStamp? // todo now
+    val startExactTimeStampOffset: ExactTimeStamp
+    val endExactTimeStampOffset: ExactTimeStamp?
 
+    // todo now
     fun notDeletedOffset(exactTimeStamp: ExactTimeStamp) = endExactTimeStampOffset?.let { it > exactTimeStamp } != false
 
+    // todo now
     fun afterStartOffset(exactTimeStamp: ExactTimeStamp) = startExactTimeStampOffset <= exactTimeStamp
 
+    // todo now
     fun currentOffset(exactTimeStamp: ExactTimeStamp) = afterStartOffset(exactTimeStamp) && notDeletedOffset(exactTimeStamp)
 
+    // todo now
     fun requireNotDeletedOffset(exactTimeStamp: ExactTimeStamp) {
         if (!notDeletedOffset(exactTimeStamp)) throwTime(exactTimeStamp)
     }
 
+    // todo now
     fun requireCurrentOffset(exactTimeStamp: ExactTimeStamp) {
         if (!currentOffset(exactTimeStamp)) throwTime(exactTimeStamp)
     }
 
+    // todo now
     fun requireNotCurrentOffset(exactTimeStamp: ExactTimeStamp) {
         if (currentOffset(exactTimeStamp)) throwTime(exactTimeStamp)
     }

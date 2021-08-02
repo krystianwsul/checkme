07cc014f54c42e7159e4f6fc8dd284e6fdae8385
krystianwsul <krystianwsul@gmail.com>
2021-08-02 19:44:21 +0200

    +RootTaskDependencyResolver.directDependencyStateCache

--- common/src/main/kotlin/firebase/models/task/RootTaskDependencyResolver.kt
+++ common/src/main/kotlin/firebase/models/task/RootTaskDependencyResolver.kt
@@ -5,6 +5,52 @@ import com.krystianwsul.common.firebase.models.cache.invalidatableCache
 
 class RootTaskDependencyResolver(private val rootTask: RootTask) {
 
+    private sealed class DirectDependencyState {
+
+        object Absent : DirectDependencyState()
+
+        class Present(val tasks: List<RootTask>) : DirectDependencyState()
+    }
+
+    private val directDependenciesStateCache: InvalidatableCache<DirectDependencyState> =
+        invalidatableCache(rootTask.clearableInvalidatableManager) { invalidatableCache ->
+            val taskRecord = rootTask.taskRecord
+            val userCustomTimeProvider = rootTask.userCustomTimeProvider
+            val rootModelChangeManager = rootTask.rootModelChangeManager
+            val parent = rootTask.parent
+
+            val customTimeKeys = taskRecord.getUserCustomTimeKeys()
+            val customTimes = customTimeKeys.mapNotNull(userCustomTimeProvider::tryGetUserCustomTime)
+
+            if (customTimes.size < customTimeKeys.size) {
+                val removable = rootModelChangeManager.userInvalidatableManager.addInvalidatable(invalidatableCache)
+
+                return@invalidatableCache InvalidatableCache.ValueHolder(DirectDependencyState.Absent) { removable.remove() }
+            }
+
+            val taskKeys = taskRecord.getDirectDependencyTaskKeys()
+            val tasks = taskKeys.mapNotNull(parent::tryGetRootTask)
+
+            if (tasks.size < taskKeys.size) {
+                val removable = rootModelChangeManager.rootTaskInvalidatableManager.addInvalidatable(invalidatableCache)
+
+                return@invalidatableCache InvalidatableCache.ValueHolder(DirectDependencyState.Absent) { removable.remove() }
+            }
+
+            val customTimeRemovables = customTimes.map {
+                it.user
+                    .clearableInvalidatableManager
+                    .addInvalidatable(invalidatableCache)
+            }
+
+            val taskRemovables = tasks.map { it.clearableInvalidatableManager.addInvalidatable(invalidatableCache) }
+
+            InvalidatableCache.ValueHolder(DirectDependencyState.Present(tasks)) {
+                customTimeRemovables.forEach { it.remove() }
+                taskRemovables.forEach { it.remove() }
+            }
+        }
+
     val dependenciesLoadedCache: InvalidatableCache<Boolean> =
         invalidatableCache(rootTask.clearableInvalidatableManager) { invalidatableCache ->
             val taskRecord = rootTask.taskRecord

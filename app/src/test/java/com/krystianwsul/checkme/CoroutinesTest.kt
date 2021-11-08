f26af2c5c159f1eac47975e4f024aa3cc9692d42
krystianwsul <krystianwsul@gmail.com>
2021-11-08 17:23:34 +0100

    +CoroutinesTest

---
+++ app/src/test/java/com/krystianwsul/checkme/CoroutinesTest.kt
@@ -0,0 +1,50 @@
+package com.krystianwsul.checkme
+
+import com.krystianwsul.checkme.firebase.loaders.RxErrorChecker
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.kotlin.addTo
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.rx3.asObservable
+import org.junit.After
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+
+class CoroutinesTest {
+
+    private val compositeDisposable = CompositeDisposable()
+
+    private lateinit var rxErrorChecker: RxErrorChecker
+
+    @Before
+    fun before() {
+        rxErrorChecker = RxErrorChecker()
+    }
+
+    @After
+    fun after() {
+        compositeDisposable.clear()
+
+        rxErrorChecker.check()
+    }
+
+    @Test
+    fun testSharedFlowAsPublishRelay() {
+        var eventPassedThrough = false
+
+        val sharedFlow = MutableSharedFlow<Unit>()
+
+        sharedFlow.asObservable()
+            .subscribe {
+                check(!eventPassedThrough)
+
+                eventPassedThrough = true
+            }
+            .addTo(compositeDisposable)
+
+        runBlocking { sharedFlow.emit(Unit) }
+
+        assertTrue(eventPassedThrough)
+    }
+}

bcc198ce99ea47ee8f7ba2cf5a7cbcfcd1313638
krystianwsul <krystianwsul@gmail.com>
2021-11-09 17:22:30 +0100

    ExpandableHolder: use ComposeView

--- app/src/main/java/com/krystianwsul/checkme/gui/tree/delegates/expandable/ExpandableDelegate.kt
+++ app/src/main/java/com/krystianwsul/checkme/gui/tree/delegates/expandable/ExpandableDelegate.kt
@@ -1,5 +1,8 @@
 package com.krystianwsul.checkme.gui.tree.delegates.expandable
 
+import androidx.compose.foundation.Image
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import com.krystianwsul.checkme.R
@@ -15,16 +18,24 @@ class ExpandableDelegate(private val treeNode: TreeNode<*>) : NodeDelegate {
             rowExpand.apply {
                 isVisible = treeNode.expandVisible
 
-                setImageResource(
-                    if (treeNode.isExpanded)
-                        R.drawable.ic_expand_less_black_36dp
-                    else
-                        R.drawable.ic_expand_more_black_36dp
-                )
+                setContent { ExpandImage() }
             }
 
             rowExpandMargin.isVisible = !treeNode.expandVisible
         }
+    }
+
+    @Composable
+    private fun ExpandImage() {
+        val imageResource = if (treeNode.isExpanded)
+            R.drawable.ic_expand_less_black_36dp
+        else
+            R.drawable.ic_expand_more_black_36dp
+
+        Image(
+            painter = painterResource(id = imageResource),
+            contentDescription = null,
+        )
     }
 
     data class State(private val expandVisible: Boolean, private val isExpanded: Boolean)
--- app/src/main/java/com/krystianwsul/checkme/gui/tree/delegates/expandable/ExpandableHolder.kt
+++ app/src/main/java/com/krystianwsul/checkme/gui/tree/delegates/expandable/ExpandableHolder.kt
@@ -1,14 +1,14 @@
 package com.krystianwsul.checkme.gui.tree.delegates.expandable
 
 import android.view.View
-import android.widget.ImageView
+import androidx.compose.ui.platform.ComposeView
 import com.jakewharton.rxbinding4.view.clicks
 import com.krystianwsul.checkme.gui.tree.BaseHolder
 import io.reactivex.rxjava3.kotlin.addTo
 
 interface ExpandableHolder : BaseHolder {
 
-    val rowExpand: ImageView
+    val rowExpand: ComposeView
     val rowExpandMargin: View
 
     override fun startRx() {

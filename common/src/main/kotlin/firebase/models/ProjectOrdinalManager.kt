76c8102d69aae1bddd53897b0b06858b3172c478
krystianwsul <krystianwsul@gmail.com>
2021-12-30 10:58:46 +0100

    +ProjectOrdinalManager.Key.Entry

--- common/src/main/kotlin/firebase/models/ProjectOrdinalManager.kt
+++ common/src/main/kotlin/firebase/models/ProjectOrdinalManager.kt
@@ -8,12 +8,12 @@ class ProjectOrdinalManager(private val project: SharedProject) {
 
     private var ordinals = mutableMapOf<Key, Value>()
 
-    fun setOrdinal(instanceKeys: Set<InstanceKey>, ordinal: Double, now: ExactTimeStamp.Local) {
-        ordinals[Key(instanceKeys)] = Value(ordinal, now)
+    fun setOrdinal(instances: Set<Instance>, ordinal: Double, now: ExactTimeStamp.Local) {
+        ordinals[Key(instances)] = Value(ordinal, now)
     }
 
-    private fun <T> getMatchByAspect(searchKey: Key, aspectSelector: (InstanceKey) -> T): Double? {
-        fun Key.toMatchElements() = instanceKeys.map(aspectSelector).toSet()
+    private fun <T> getMatchByAspect(searchKey: Key, aspectSelector: (Key.Entry) -> T): Double? {
+        fun Key.toMatchElements() = entries.map(aspectSelector).toSet()
 
         val inputMatchElements = searchKey.toMatchElements()
 
@@ -34,16 +34,27 @@ class ProjectOrdinalManager(private val project: SharedProject) {
     }
 
     // todo ordinal add info about instanceDateTime
-    fun getOrdinal(instanceKeys: Set<InstanceKey>): Double {
-        getMatchByAspect(Key(instanceKeys)) { it }?.let { return it }
-        getMatchByAspect(Key(instanceKeys)) { it.taskKey }?.let { return it }
+    fun getOrdinal(instances: Set<Instance>): Double {
+        getMatchByAspect(Key(instances)) { it.instanceKey }?.let { return it }
+        getMatchByAspect(Key(instances)) { it.instanceKey.taskKey }?.let { return it }
 
         // match those that contain the most instances with the exact same DateTime
 
         return project.projectKey.getOrdinal()
     }
 
-    private data class Key(val instanceKeys: Set<InstanceKey>)
+    private data class Key(val entries: Set<Entry>) {
+
+        companion object {
+
+            operator fun invoke(instances: Set<Instance>) = Key(instances.map(::Entry).toSet())
+        }
+
+        data class Entry(val instanceKey: InstanceKey) {
+
+            constructor(instance: Instance) : this(instance.instanceKey)
+        }
+    }
 
     private data class Value(val ordinal: Double, val updated: ExactTimeStamp.Local)
 }

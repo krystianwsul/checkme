5cc35cf9b4f6704e9c5aa4d91cf0b62642efa5cc
krystianwsul <krystianwsul@gmail.com>
2022-01-01 15:48:34 +0100

    OrdinalProcessor: +MutableOrdinalEntry +MutableKeyEntry

--- common/src/main/kotlin/relevance/OrdinalProcessor.kt
+++ common/src/main/kotlin/relevance/OrdinalProcessor.kt
@@ -3,6 +3,9 @@ package com.krystianwsul.common.relevance
 import com.krystianwsul.common.firebase.models.ProjectOrdinalManager
 import com.krystianwsul.common.firebase.models.RootUser
 import com.krystianwsul.common.firebase.models.project.Project
+import com.krystianwsul.common.time.DateTimePair
+import com.krystianwsul.common.time.ExactTimeStamp
+import com.krystianwsul.common.utils.InstanceKey
 
 class OrdinalProcessor(private val users: Collection<RootUser>, private val relevantProjects: Set<Project<*>>) {
 
@@ -20,12 +23,46 @@ class OrdinalProcessor(private val users: Collection<RootUser>, private val rele
     }
 
     private fun processProject(projectOrdinalManager: ProjectOrdinalManager) {
-        val originalEntries = projectOrdinalManager.allEntries
+        val mutableOrdinalEntries = projectOrdinalManager.allEntries
+            .map(::MutableOrdinalEntry)
+            .toMutableList()
 
-        originalEntries.forEach { (key, value) ->
-            key.entries.forEach { keyEntry ->
-                val taskKey = keyEntry.instanceKey.taskKey
-            }
+        mutableOrdinalEntries.forEach { mutableEntryKey ->
+        }
+
+        val immutableEntries = mutableOrdinalEntries.associate { it.toImmutableEntryPair() }
+    }
+
+    private data class MutableOrdinalEntry(
+        val mutableKeyEntries: MutableList<MutableKeyEntry>,
+        val ordinal: Double,
+        val updated: ExactTimeStamp.Local,
+    ) {
+
+        constructor(pair: Pair<ProjectOrdinalManager.Key, ProjectOrdinalManager.Value>) : this(
+            pair.first
+                .entries
+                .map(::MutableKeyEntry)
+                .toMutableList(),
+            pair.second.ordinal,
+            pair.second.updated,
+        )
+
+        fun toImmutableEntryPair(): Pair<ProjectOrdinalManager.Key, ProjectOrdinalManager.Value> {
+            return Pair(
+                ProjectOrdinalManager.Key(mutableKeyEntries.map { it.toImmutableKeyEntry() }.toSet()),
+                ProjectOrdinalManager.Value(ordinal, updated)
+            )
+        }
+    }
+
+    private data class MutableKeyEntry(val instanceKey: InstanceKey, val instanceDateTimePair: DateTimePair) {
+
+        constructor(immutableEntry: ProjectOrdinalManager.Key.Entry) :
+                this(immutableEntry.instanceKey, immutableEntry.instanceDateTimePair)
+
+        fun toImmutableKeyEntry(): ProjectOrdinalManager.Key.Entry {
+            return ProjectOrdinalManager.Key.Entry(instanceKey, instanceDateTimePair)
         }
     }
 }

b1bee277e6f0f6f127b42ca16cfb40c1c36f9200
krystianwsul <krystianwsul@gmail.com>
2022-01-03 19:17:36 +0100

    ProjectOrdinalManagerSerializationTest: finish testing serialization

--- app/src/test/java/com/krystianwsul/common/firebase/models/users/ProjectOrdinalManagerSerializationTest.kt
+++ app/src/test/java/com/krystianwsul/common/firebase/models/users/ProjectOrdinalManagerSerializationTest.kt
@@ -1,9 +1,8 @@
 package com.krystianwsul.common.firebase.models.users
 
-import com.krystianwsul.common.time.Date
-import com.krystianwsul.common.time.DateTimePair
-import com.krystianwsul.common.time.HourMinute
-import com.krystianwsul.common.time.TimePair
+import com.krystianwsul.common.time.*
+import com.krystianwsul.common.utils.ProjectKey
+import com.krystianwsul.common.utils.TaskKey
 import io.mockk.mockk
 import org.junit.Assert.assertEquals
 import org.junit.Test
@@ -17,5 +16,55 @@ class ProjectOrdinalManagerSerializationTest {
         val outputDateTimePair = DateTimePair.fromJson(mockk(), dateTimePairJson)
 
         assertEquals(inputDateTimePair, outputDateTimePair)
+    }
+
+    @Test
+    fun testTaskInfoRootTaskKeySerialization() {
+        val taskKey = TaskKey.Root("taskKey")
+
+        val inputTaskInfo = ProjectOrdinalManager.Key.TaskInfo(taskKey, null)
+        val taskInfoJson = inputTaskInfo.toJson()
+        val outputTaskInfo = ProjectOrdinalManager.Key.TaskInfo.fromJson(mockk(), taskInfoJson)
+
+        assertEquals(taskKey, outputTaskInfo.taskKey)
+    }
+
+    @Test
+    fun testTaskInfoProjectTaskKeySerialization() {
+        val taskKey = TaskKey.Project(ProjectKey.Private("projectKey"), "taskKey")
+
+        val inputTaskInfo = ProjectOrdinalManager.Key.TaskInfo(taskKey, null)
+        val taskInfoJson = inputTaskInfo.toJson()
+        val outputTaskInfo = ProjectOrdinalManager.Key.TaskInfo.fromJson(mockk(), taskInfoJson)
+
+        assertEquals(taskKey, outputTaskInfo.taskKey)
+    }
+
+    @Test
+    fun testDateSerialization() {
+        val date = Date(2022, 1, 3)
+
+        val inputDateOrDayOfWeek = DateOrDayOfWeek.Date(date)
+        val dateOrDayOfWeekJson = inputDateOrDayOfWeek.toJson()
+        val outputDateOrDayOfWeek = DateOrDayOfWeek.fromJson(dateOrDayOfWeekJson)
+
+        assertEquals(
+            date,
+            outputDateOrDayOfWeek.let { it as DateOrDayOfWeek.Date }.date,
+        )
+    }
+
+    @Test
+    fun testDayOfWeekSerialization() {
+        val dayOfWeek = DayOfWeek.WEDNESDAY
+
+        val inputDateOrDayOfWeek = DateOrDayOfWeek.DayOfWeek(dayOfWeek)
+        val dateOrDayOfWeekJson = inputDateOrDayOfWeek.toJson()
+        val outputDateOrDayOfWeek = DateOrDayOfWeek.fromJson(dateOrDayOfWeekJson)
+
+        assertEquals(
+            dayOfWeek,
+            outputDateOrDayOfWeek.let { it as DateOrDayOfWeek.DayOfWeek }.dayOfWeek,
+        )
     }
 }

ce066fb09cfcb1b6fa5563f59a1fe97bfb1918f6
krystianwsul <krystianwsul@gmail.com>
2022-01-03 19:19:36 +0100

    ProjectOrdinalKeyEntryJson: cleanup

--- common/src/main/kotlin/firebase/json/users/ProjectOrdinalKeyEntryJson.kt
+++ common/src/main/kotlin/firebase/json/users/ProjectOrdinalKeyEntryJson.kt
@@ -2,20 +2,15 @@ package com.krystianwsul.common.firebase.json.users
 
 data class ProjectOrdinalKeyEntryJson @JvmOverloads constructor(
     val taskInfoJson: TaskInfoJson? = null,
-    val instanceDateOrDayOfWeek: String = "", // todo ordinal use method from instanceJson, plus DayOfWeek.ordinal
-    val instanceTime: String = "", // todo ordinal use method from instanceJson
+    val instanceDateOrDayOfWeek: String = "",
+    val instanceTime: String = "",
 ) {
 
-    // todo ordinal unit test this whole thing, round-trip, for variations
-
     data class TaskInfoJson @JvmOverloads constructor(
-        val taskKey: String = "", // todo ordinal use method from Task.fromShortcut
+        val taskKey: String = "",
         val scheduleDateTimePairJson: DateTimePairJson? = null,
     ) {
 
-        data class DateTimePairJson @JvmOverloads constructor(
-            val date: String = "", // todo ordinal use method from instanceJson
-            val time: String = "", // todo ordinal use method from instanceJson
-        )
+        data class DateTimePairJson @JvmOverloads constructor(val date: String = "", val time: String = "")
     }
 }

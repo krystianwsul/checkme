e6a8b434efbddd2d93710c610af956c15e45ec5c
krystianwsul <krystianwsul@gmail.com>
2022-01-07 15:15:04 +0100

    Task: fix bug for interrupting getInstances

--- common/src/main/kotlin/firebase/models/task/Task.kt
+++ common/src/main/kotlin/firebase/models/task/Task.kt
@@ -359,29 +359,31 @@ sealed class Task(
         check(!gettingInstances)
         gettingInstances = true
 
-        InterruptionChecker.throwIfInterrupted()
+        return try {
+            InterruptionChecker.throwIfInterrupted()
 
-        return if (filterVisible && !notDeleted && endData!!.deleteInstances) {
-            getExistingInstances(startExactTimeStamp, endExactTimeStamp, onlyRoot).filter { it.done != null }
-        } else {
-            val instanceInfoSequences = mutableListOf<Sequence<InstanceInfo>>()
+            if (filterVisible && !notDeleted && endData!!.deleteInstances) {
+                getExistingInstances(startExactTimeStamp, endExactTimeStamp, onlyRoot).filter { it.done != null }
+            } else {
+                val instanceInfoSequences = mutableListOf<Sequence<InstanceInfo>>()
 
-            instanceInfoSequences +=
-                getExistingInstances(startExactTimeStamp, endExactTimeStamp, onlyRoot).map(::InstanceInfo)
+                instanceInfoSequences +=
+                    getExistingInstances(startExactTimeStamp, endExactTimeStamp, onlyRoot).map(::InstanceInfo)
 
-            if (!onlyRoot) {
-                instanceInfoSequences += getParentInstances(
-                    startExactTimeStamp,
-                    endExactTimeStamp,
-                    now,
-                    excludedParentTasks,
-                )
+                if (!onlyRoot) {
+                    instanceInfoSequences += getParentInstances(
+                        startExactTimeStamp,
+                        endExactTimeStamp,
+                        now,
+                        excludedParentTasks,
+                    )
+                }
+
+                instanceInfoSequences += getScheduleInstances(startExactTimeStamp, endExactTimeStamp).map(::InstanceInfo)
+
+                combineInstanceInfoSequences(instanceInfoSequences).toInstances()
             }
-
-            instanceInfoSequences += getScheduleInstances(startExactTimeStamp, endExactTimeStamp).map(::InstanceInfo)
-
-            combineInstanceInfoSequences(instanceInfoSequences).toInstances()
-        }.also {
+        } finally {
             check(gettingInstances)
             gettingInstances = false
         }

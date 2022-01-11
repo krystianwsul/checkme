422b896800d9bb4501f6e43758c4f6a0fc07755e
krystianwsul <krystianwsul@gmail.com>
2022-01-11 16:52:19 +0100

    CoroutinesTest: whip up replacement for BehaviorRelay

--- app/src/test/java/com/krystianwsul/checkme/CoroutinesTest.kt
+++ app/src/test/java/com/krystianwsul/checkme/CoroutinesTest.kt
@@ -1,12 +1,14 @@
 package com.krystianwsul.checkme
 
 import com.krystianwsul.checkme.firebase.loaders.RxErrorChecker
+import com.krystianwsul.common.utils.singleOrEmpty
 import io.reactivex.rxjava3.disposables.CompositeDisposable
 import io.reactivex.rxjava3.kotlin.addTo
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.rx3.asObservable
 import org.junit.After
+import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Test
@@ -65,5 +67,34 @@ class CoroutinesTest {
         sharedFlow.tryEmit(Unit)
 
         assertTrue(eventPassedThrough)
+    }
+
+    @Test
+    fun testSharedFlowAsBehaviorRelay() {
+        val sharedFlow = MutableSharedFlow<Int>(replay = 1)
+
+        var lastEmission = 0
+
+        sharedFlow.asObservable()
+            .subscribe { lastEmission = it }
+            .addTo(compositeDisposable)
+
+        sharedFlow.tryEmit(1)
+        assertEquals(1, lastEmission)
+
+        sharedFlow.tryEmit(2)
+        assertEquals(2, lastEmission)
+
+        var newLastEmission = 0
+
+        val disposable = sharedFlow.asObservable().subscribe {
+            check(newLastEmission == 0)
+            newLastEmission = it
+        }
+
+        assertEquals(2, newLastEmission)
+        disposable.dispose()
+
+        assertEquals(2, sharedFlow.replayCache.singleOrEmpty())
     }
 }

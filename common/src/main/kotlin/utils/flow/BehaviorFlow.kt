001a366d56004ea969f2ef07427212147ba6b648
krystianwsul <krystianwsul@gmail.com>
2022-01-11 16:57:24 +0100

    +BehaviorFlow

---
+++ common/src/main/kotlin/utils/flow/BehaviorFlow.kt
@@ -0,0 +1,25 @@
+package com.krystianwsul.common.utils.flow
+
+import com.krystianwsul.common.utils.singleOrEmpty
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
+
+class BehaviorFlow<T> private constructor(private val flow: MutableSharedFlow<T>) : Flow<T> by flow {
+
+    companion object {
+
+        fun <T> invoke(initialValue: T? = null): BehaviorFlow<T> {
+            val flow = MutableSharedFlow<T>(replay = 1)
+
+            initialValue?.let(flow::tryEmit)
+
+            return BehaviorFlow(flow)
+        }
+    }
+
+    val valueOrNull get() = flow.replayCache.singleOrEmpty()
+
+    val value get() = valueOrNull!!
+
+    val hasValue get() = valueOrNull != null
+}

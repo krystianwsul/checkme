e79cbc94e5b3daa029f056225e4f9bc04ed76af3
krystianwsul <krystianwsul@gmail.com>
2022-04-04 19:36:37 +0200

    SearchContext: +NoSearch +Normal

--- common/src/main/kotlin/firebase/models/search/SearchContext.kt
+++ common/src/main/kotlin/firebase/models/search/SearchContext.kt
@@ -8,29 +8,21 @@ import com.krystianwsul.common.interrupt.InterruptionChecker
 import com.krystianwsul.common.time.ExactTimeStamp
 
 // todo searchContext add MyUser in here, then clean up SearchData
-class SearchContext private constructor(
-    private val searchCriteria: SearchCriteria,
-    private val searchingChildrenOfQueryMatch: Boolean,
+sealed class SearchContext(
+    protected val searchCriteria: SearchCriteria,
+    protected val searchingChildrenOfQueryMatch: Boolean, // todo searchContext separate subclass
 ) {
 
     companion object {
 
         fun startSearch(searchCriteria: SearchCriteria) = new(searchCriteria, false)
 
-        private fun new(searchCriteria: SearchCriteria, searchingChildrenOfQueryMatch: Boolean) =
-            SearchContext(searchCriteria, searchingChildrenOfQueryMatch)
-    }
-
-    fun getChildrenSearchContext(filterResult: FilterResult) = when (filterResult) {
-        FilterResult.Exclude -> this
-        is FilterResult.NoSearch -> this
-        is FilterResult.Include -> if (filterResult.matchesSearch) {
-            when (searchCriteria.search) {
-                is SearchCriteria.Search.Query -> new(searchCriteria, true)
-                is SearchCriteria.Search.TaskKey -> new(searchCriteria.clearSearch(), false)
+        private fun new(searchCriteria: SearchCriteria, searchingChildrenOfQueryMatch: Boolean): SearchContext {
+            return if (searchCriteria.isEmpty) {
+                NoSearch
+            } else {
+                Normal(searchCriteria, searchingChildrenOfQueryMatch)
             }
-        } else {
-            this
         }
     }
 
@@ -40,93 +32,155 @@ class SearchContext private constructor(
 
     fun <T> search(action: SearchContext.() -> T) = run(action)
 
-    private fun childHierarchyMatches(task: Task, onlyHierarchy: Boolean = false): FilterResult {
-        InterruptionChecker.throwIfInterrupted()
+    abstract fun Sequence<Task>.filterSearch(onlyHierarchy: Boolean = false): Sequence<Pair<Task, FilterResult>>
 
-        return task.getMatchResult(searchCriteria.search).let {
-            it.getFilterResult() ?: run {
-                if (searchingChildrenOfQueryMatch) {
-                    FilterResult.Include(false)
-                } else {
-                    val childTasks = if (onlyHierarchy) task.getHierarchyChildTasks() else task.getChildTasks()
-
-                    if (childTasks.any { !childHierarchyMatches(it, onlyHierarchy).doesntMatch })
-                        FilterResult.Include(false)
-                    else
-                        FilterResult.Exclude
-                }
-            }
-        }
-    }
-
-    fun Sequence<Task>.filterSearch(onlyHierarchy: Boolean = false) =
-        if (searchCriteria.search.isEmpty) {
-            map { it to FilterResult.NoSearch("e") }
-        } else {
-            // todo taskKey this could return a subtype of FilterCriteria, i.e. the subset where doesnMatch = false
-            map { it to childHierarchyMatches(it, onlyHierarchy) }.filter { !it.second.doesntMatch }
-        }
-
-    fun Sequence<Task>.filterSearchCriteria(
+    abstract fun Sequence<Task>.filterSearchCriteria(
         myUser: MyUser,
         now: ExactTimeStamp.Local,
-    ): Sequence<Pair<Task, FilterResult>> {
-        if (searchCriteria.isTaskEmpty) return map { it to FilterResult.NoSearch("b") }
+    ): Sequence<Pair<Task, FilterResult>>
 
-        val filtered1 = if (searchCriteria.showAssignedToOthers) {
-            this
-        } else {
-            filter { it.isAssignedToMe(myUser) }
-        }
-
-        val filtered2 = filtered1.filterSearch()
-
-        return if (searchCriteria.taskCriteria.showDeleted) {
-            filtered2
-        } else {
-            filtered2.filter { it.first.isVisible(now) }
-        }
-    }
-
-    // todo taskKey this could return the task.matchesSearch result to optimize building child searchCriteria in the calling function
-    fun Sequence<Instance>.filterSearchCriteria(
+    abstract fun Sequence<Instance>.filterSearchCriteria(
         now: ExactTimeStamp.Local,
         myUser: MyUser,
         assumeChild: Boolean,
-    ): Sequence<Pair<Instance, FilterResult>> = if (searchCriteria.isInstanceEmpty) {
-        this.map { it to FilterResult.NoSearch("i") }
-    } else {
-        fun childHierarchyMatches(instance: Instance, assumeChild: Boolean): FilterResult {
+    ): Sequence<Pair<Instance, FilterResult>>
+
+    abstract fun getChildrenSearchContext(filterResult: FilterResult): SearchContext
+
+    object NoSearch : SearchContext(SearchCriteria.empty, false) {
+
+        override fun Sequence<Task>.filterSearch(onlyHierarchy: Boolean) = map { it to FilterResult.NoSearch("e") }
+
+        override fun Sequence<Task>.filterSearchCriteria(
+            myUser: MyUser,
+            now: ExactTimeStamp.Local,
+        ) = map { it to FilterResult.NoSearch("b") }
+
+        override fun Sequence<Instance>.filterSearchCriteria(
+            now: ExactTimeStamp.Local,
+            myUser: MyUser,
+            assumeChild: Boolean,
+        ): Sequence<Pair<Instance, FilterResult>> = map { it to FilterResult.NoSearch("i") }
+
+        override fun getChildrenSearchContext(filterResult: FilterResult): SearchContext {
+            check(filterResult is FilterResult.NoSearch)
+
+            return this
+        }
+    }
+
+    class Normal(searchCriteria: SearchCriteria, searchingChildrenOfQueryMatch: Boolean) :
+        SearchContext(searchCriteria, searchingChildrenOfQueryMatch) {
+
+        init {
+            check(!searchCriteria.isEmpty)
+        }
+
+        private fun childHierarchyMatches(task: Task, onlyHierarchy: Boolean = false): FilterResult {
             InterruptionChecker.throwIfInterrupted()
 
-            if (!assumeChild && !searchCriteria.showAssignedToOthers && !instance.isAssignedToMe(myUser))
-                return FilterResult.Exclude
-
-            if (!searchCriteria.showDone && instance.done != null)
-                return FilterResult.Exclude
-
-            if (instance.instanceKey in searchCriteria.excludedInstanceKeys)
-                return FilterResult.Exclude
-
-            return instance.task.getMatchResult(searchCriteria.search).let {
+            return task.getMatchResult(searchCriteria.search).let {
                 it.getFilterResult() ?: run {
                     if (searchingChildrenOfQueryMatch) {
                         FilterResult.Include(false)
                     } else {
-                        if (
-                            instance.getChildInstances()
-                                .filter { it.isVisible(now, Instance.VisibilityOptions(assumeChildOfVisibleParent = true)) }
-                                .any { !childHierarchyMatches(it, true).doesntMatch }
-                        ) {
+                        val childTasks = if (onlyHierarchy) task.getHierarchyChildTasks() else task.getChildTasks()
+
+                        if (childTasks.any { !childHierarchyMatches(it, onlyHierarchy).doesntMatch })
                             FilterResult.Include(false)
-                        } else {
+                        else
                             FilterResult.Exclude
-                        }
                     }
                 }
             }
         }
 
-        map { it to childHierarchyMatches(it, assumeChild) }.filter { !it.second.doesntMatch }
+        override fun Sequence<Task>.filterSearch(onlyHierarchy: Boolean): Sequence<Pair<Task, FilterResult>> =
+            if (searchCriteria.search.isEmpty) {
+                map { it to FilterResult.NoSearch("e") }
+            } else {
+                // todo taskKey this could return a subtype of FilterCriteria, i.e. the subset where doesnMatch = false
+                map { it to childHierarchyMatches(it, onlyHierarchy) }.filter { !it.second.doesntMatch }
+            }
+
+        override fun Sequence<Task>.filterSearchCriteria(
+            myUser: MyUser,
+            now: ExactTimeStamp.Local,
+        ): Sequence<Pair<Task, FilterResult>> {
+            if (searchCriteria.isTaskEmpty) return map { it to FilterResult.NoSearch("b") }
+
+            val filtered1 = if (searchCriteria.showAssignedToOthers) {
+                this
+            } else {
+                filter { it.isAssignedToMe(myUser) }
+            }
+
+            val filtered2 = filtered1.filterSearch()
+
+            return if (searchCriteria.taskCriteria.showDeleted) {
+                filtered2
+            } else {
+                filtered2.filter { it.first.isVisible(now) }
+            }
+        }
+
+        override fun Sequence<Instance>.filterSearchCriteria(
+            now: ExactTimeStamp.Local,
+            myUser: MyUser,
+            assumeChild: Boolean,
+        ): Sequence<Pair<Instance, FilterResult>> = if (searchCriteria.isInstanceEmpty) {
+            this.map { it to FilterResult.NoSearch("i") }
+        } else {
+            fun childHierarchyMatches(instance: Instance, assumeChild: Boolean): FilterResult {
+                InterruptionChecker.throwIfInterrupted()
+
+                if (!assumeChild && !searchCriteria.showAssignedToOthers && !instance.isAssignedToMe(myUser))
+                    return FilterResult.Exclude
+
+                if (!searchCriteria.showDone && instance.done != null)
+                    return FilterResult.Exclude
+
+                if (instance.instanceKey in searchCriteria.excludedInstanceKeys)
+                    return FilterResult.Exclude
+
+                return instance.task.getMatchResult(searchCriteria.search).let {
+                    it.getFilterResult() ?: run {
+                        if (searchingChildrenOfQueryMatch) {
+                            FilterResult.Include(false)
+                        } else {
+                            if (
+                                instance.getChildInstances()
+                                    .filter {
+                                        it.isVisible(
+                                            now,
+                                            Instance.VisibilityOptions(assumeChildOfVisibleParent = true)
+                                        )
+                                    }
+                                    .any { !childHierarchyMatches(it, true).doesntMatch }
+                            ) {
+                                FilterResult.Include(false)
+                            } else {
+                                FilterResult.Exclude
+                            }
+                        }
+                    }
+                }
+            }
+
+            map { it to childHierarchyMatches(it, assumeChild) }.filter { !it.second.doesntMatch }
+        }
+
+        override fun getChildrenSearchContext(filterResult: FilterResult): SearchContext = when (filterResult) {
+            FilterResult.Exclude -> this
+            is FilterResult.NoSearch -> this
+            is FilterResult.Include -> if (filterResult.matchesSearch) {
+                when (searchCriteria.search) {
+                    is SearchCriteria.Search.Query -> new(searchCriteria, true)
+                    is SearchCriteria.Search.TaskKey -> new(searchCriteria.clearSearch(), false)
+                }
+            } else {
+                this
+            }
+        }
     }
 }

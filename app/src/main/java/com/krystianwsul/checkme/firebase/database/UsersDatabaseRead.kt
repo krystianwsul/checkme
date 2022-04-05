a38c694eae8b77fe899c200bc187b4f980671698
krystianwsul <krystianwsul@gmail.com>
2022-04-05 20:08:24 +0200

    IndicatorDatabaseRead: fix generics issue for reading FB

--- app/src/main/java/com/krystianwsul/checkme/firebase/database/UsersDatabaseRead.kt
+++ app/src/main/java/com/krystianwsul/checkme/firebase/database/UsersDatabaseRead.kt
@@ -1,10 +1,13 @@
 package com.krystianwsul.checkme.firebase.database
 
 import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.GenericTypeIndicator
 import com.krystianwsul.common.firebase.DatabaseWrapper
 import com.krystianwsul.common.firebase.json.users.UserWrapper
 
 class UsersDatabaseRead : IndicatorDatabaseRead<Map<String, UserWrapper>>() {
 
     override fun DatabaseReference.getQuery() = child(DatabaseWrapper.USERS_KEY).orderByKey()
+
+    override val genericTypeIndicator = object : GenericTypeIndicator<Map<String, UserWrapper>>() {}
 }

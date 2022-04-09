0cbe9d8baeaa1dfa34fb79ef99f2808a72f5f5d9
krystianwsul <krystianwsul@gmail.com>
2022-04-09 12:08:35 +0200

    DayViewModel: use observable delegate because of refreshes

--- app/src/main/java/com/krystianwsul/checkme/viewmodels/DayViewModel.kt
+++ app/src/main/java/com/krystianwsul/checkme/viewmodels/DayViewModel.kt
@@ -5,7 +5,6 @@ import com.krystianwsul.checkme.Preferences
 import com.krystianwsul.checkme.domainmodel.extensions.getGroupListData
 import com.krystianwsul.checkme.gui.instances.list.GroupListDataWrapper
 import com.krystianwsul.common.time.ExactTimeStamp
-import kotlin.properties.Delegates.notNull
 
 class DayViewModel : ViewModel() {
 
@@ -25,32 +24,31 @@ class DayViewModel : ViewModel() {
 
     class Entry(private val timeRange: Preferences.TimeRange, private val position: Int) {
 
-        private var initialized = false
-        private var showAssigned by notNull<Boolean>()
-
         private val domainListener = object : DomainListener<DayData>() {
 
             override val domainResultFetcher = DomainResultFetcher.DomainFactoryData {
-                it.getGroupListData(ExactTimeStamp.Local.now, position, timeRange, showAssigned)
+                it.getGroupListData(ExactTimeStamp.Local.now, position, timeRange, delegate.parameters.showAssigned)
             }
         }
+
+        private val delegate: ObservableDomainViewModel.Delegate<DayData, Parameters> =
+            ObservableDomainViewModel.Delegate(domainListener)
 
         val data get() = domainListener.data
         val dataId get() = domainListener.dataId
 
         fun start(showAssigned: Boolean) {
-            val force = initialized && (this.showAssigned != showAssigned)
+            delegate.parametersRelay.accept(Parameters(showAssigned))
 
-            initialized = true
-            this.showAssigned = showAssigned
-
-            domainListener.start(force)
+            delegate.start()
         }
 
-        fun refresh() = domainListener.start(true)
+        fun refresh() = delegate.refresh()
 
-        fun stop() = domainListener.stop()
+        fun stop() = delegate.dispose()
     }
 
     data class DayData(val groupListDataWrapper: GroupListDataWrapper) : DomainData()
+
+    private data class Parameters(val showAssigned: Boolean) : ObservableDomainViewModel.Parameters
 }

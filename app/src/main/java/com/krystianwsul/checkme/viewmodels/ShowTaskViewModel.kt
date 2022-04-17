b2cfd9898ea94e697bf350da01ef99686935a565
krystianwsul <krystianwsul@gmail.com>
2022-04-17 19:28:15 +0200

    ShowTaskViewModel: set DomainListener priority based on task read priority

--- app/src/main/java/com/krystianwsul/checkme/viewmodels/ShowTaskViewModel.kt
+++ app/src/main/java/com/krystianwsul/checkme/viewmodels/ShowTaskViewModel.kt
@@ -1,17 +1,34 @@
 package com.krystianwsul.checkme.viewmodels
 
 import com.jakewharton.rxrelay3.PublishRelay
+import com.krystianwsul.checkme.domainmodel.HasInstancesStore
 import com.krystianwsul.checkme.domainmodel.extensions.getShowTaskData
 import com.krystianwsul.checkme.gui.tasks.TaskListFragment
 import com.krystianwsul.common.criteria.SearchCriteria
 import com.krystianwsul.common.firebase.models.ImageState
 import com.krystianwsul.common.utils.TaskKey
+import com.mindorks.scheduler.Priority
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.kotlin.addTo
 
 class ShowTaskViewModel : ObservableDomainViewModel<ShowTaskViewModel.Data, ShowTaskViewModel.Parameters>() {
 
     override val domainListener = object : DomainListener<Data>() {
+
+        /*
+        Awful hack to prevent ShowTaskActivity from crashing on startup.  Definitely restructure into something where
+        DatabaseResultQueue can examine the currently registered DomainListeners, and determine appropriate load priority
+        based on that.
+         */
+        override val priority
+            get() = when (val taskKey = parameters.taskKey) {
+                is TaskKey.Project -> super.priority
+                is TaskKey.Root -> {
+                    val taskPriority = HasInstancesStore.getPriority(taskKey)
+
+                    Priority.values()[taskPriority.ordinal - 1]
+                }
+            }
 
         override val domainResultFetcher =
             DomainResultFetcher.DomainFactoryData { it.getShowTaskData(parameters.taskKey, parameters.searchCriteria) }

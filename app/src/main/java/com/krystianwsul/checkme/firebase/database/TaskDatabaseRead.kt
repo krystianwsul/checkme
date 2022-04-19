42a221e447db8fdfef41b289d2b25001c276ada6
krystianwsul <krystianwsul@gmail.com>
2022-04-19 18:16:22 +0200

    TaskDatabaseRead: cleanup

--- app/src/main/java/com/krystianwsul/checkme/firebase/database/TaskDatabaseRead.kt
+++ app/src/main/java/com/krystianwsul/checkme/firebase/database/TaskDatabaseRead.kt
@@ -10,14 +10,17 @@ class TaskDatabaseRead(private val taskKey: TaskKey.Root) : TypedDatabaseRead<Ro
 
     private var finalTaskPriority: HasInstancesStore.TaskPriority.Final? = null
 
-    override fun getPriority(): DatabaseReadPriority {
-        val taskPriority = finalTaskPriority ?: HasInstancesStore.getTaskPriority(taskKey).also {
-            if (it is HasInstancesStore.TaskPriority.Final)
-                finalTaskPriority = it
-        }
+    private fun getTaskPriority(): HasInstancesStore.TaskPriority {
+        finalTaskPriority?.let { return it }
 
-        return taskPriority.databaseReadPriority
+        val taskPriority = HasInstancesStore.getTaskPriority(taskKey)
+
+        taskPriority.let { it as? HasInstancesStore.TaskPriority.Final }?.let { finalTaskPriority = it }
+
+        return taskPriority
     }
+
+    override fun getPriority() = getTaskPriority().databaseReadPriority
 
     override val kClass = RootTaskJson::class
 

9645ec57b8fea3965d4fc67971d0f8213b34379f
krystianwsul <krystianwsul@gmail.com>
2022-05-04 19:14:11 +0200

    DayViewModel: wire up ProjectFilter

--- app/src/main/java/com/krystianwsul/checkme/viewmodels/DayViewModel.kt
+++ app/src/main/java/com/krystianwsul/checkme/viewmodels/DayViewModel.kt
@@ -1,9 +1,14 @@
 package com.krystianwsul.checkme.viewmodels
 
 import androidx.lifecycle.ViewModel
+import com.jakewharton.rxrelay3.BehaviorRelay
+import com.krystianwsul.checkme.Preferences
 import com.krystianwsul.checkme.domainmodel.extensions.getGroupListData
 import com.krystianwsul.checkme.gui.instances.list.GroupListDataWrapper
 import com.krystianwsul.common.time.ExactTimeStamp
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.kotlin.Observables
+import io.reactivex.rxjava3.kotlin.addTo
 
 class DayViewModel : ViewModel() {
 
@@ -35,18 +40,33 @@ class DayViewModel : ViewModel() {
         val data get() = domainListener.data
         val dataId get() = domainListener.dataId
 
+        private val showAssignedRelay = BehaviorRelay.create<Boolean>()
+
+        private val compositeDisposable = CompositeDisposable()
+
+        init {
+            Observables.combineLatest(showAssignedRelay, Preferences.projectFilterObservable)
+                .map { (showAssigned, projectFilter) -> Parameters(showAssigned, projectFilter) }
+                .subscribe(delegate.parametersRelay)
+                .addTo(compositeDisposable)
+        }
+
         fun start(showAssigned: Boolean) {
-            delegate.parametersRelay.accept(Parameters(showAssigned))
+            showAssignedRelay.accept(showAssigned)
 
             delegate.start()
         }
 
         fun refresh() = delegate.refresh()
 
-        fun stop() = delegate.dispose()
+        fun stop() {
+            delegate.dispose()
+            compositeDisposable.clear()
+        }
     }
 
     data class DayData(val groupListDataWrapper: GroupListDataWrapper) : DomainData()
 
-    private data class Parameters(val showAssigned: Boolean) : ObservableDomainViewModel.Parameters
+    private data class Parameters(val showAssigned: Boolean, val projectFilter: Preferences.ProjectFilter) :
+        ObservableDomainViewModel.Parameters
 }

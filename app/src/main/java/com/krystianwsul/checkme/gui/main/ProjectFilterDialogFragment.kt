a9f245b1002276ec6deef4412d201c35d8c4ea46
krystianwsul <krystianwsul@gmail.com>
2022-05-04 16:44:55 +0200

    ProjectFilterDialogFragment: use composableView for project list

--- app/src/main/java/com/krystianwsul/checkme/gui/main/ProjectFilterDialogFragment.kt
+++ app/src/main/java/com/krystianwsul/checkme/gui/main/ProjectFilterDialogFragment.kt
@@ -2,7 +2,16 @@ package com.krystianwsul.checkme.gui.main
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.material.RadioButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.text.buildAnnotatedString
+import com.google.android.material.composethemeadapter.MdcTheme
 import com.krystianwsul.checkme.databinding.FragmentProjectFilterDialogBinding
 import com.krystianwsul.checkme.gui.base.NoCollapseBottomSheetDialogFragment
 import com.krystianwsul.checkme.gui.utils.ResettableProperty
@@ -22,6 +31,33 @@ class ProjectFilterDialogFragment : NoCollapseBottomSheetDialogFragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) =
         FragmentProjectFilterDialogBinding.inflate(inflater, container, false).also { binding = it }.root
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val projects = (0..10).map { "project $it" }
+
+        binding.projectFilterDialogCompose.setContent {
+            MdcTheme { ProjectList(projects) { } }
+        }
+    }
+
+    @Composable
+    private fun ProjectList(projects: List<String>, onClick: (String) -> Unit) {
+        Column {
+            projects.forEach { project ->
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    RadioButton(selected = false, onClick = { onClick(project) })
+
+                    val annotatedString = buildAnnotatedString {
+                        append(project)
+                    }
+
+                    ClickableText(text = annotatedString, onClick = { onClick(project) })
+                }
+            }
+        }
+    }
 
     override fun onDestroyView() {
         bindingProperty.reset()
